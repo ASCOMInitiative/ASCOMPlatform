fa261288f47298eac4b897e0a0d5f8025093fafd
ChristopheGerbier <ChristopheGerbier@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2009-08-06 12:23:06 +0000

    Focuser Simulator :  - Renamed project as requested  - Referenced ASCOM.Utilities instead of ASCOM.HelpNET

--- trunk/Drivers and Simulators/Focus Simulator .NET/FocVide/FocuserHardware.cs
+++ trunk/Drivers and Simulators/Focus Simulator .NET/FocVide/FocuserHardware.cs
@@ -110,7 +110,8 @@ namespace ASCOM.FocVide
         public static void Halt()
         {
             MyLog(eLogKind.LogMove, "HALT requested");
-            if (!Properties.Settings.Default.sEnableHalt) { throw new MethodNotImplementedException("Halt"); }
+            //if (!Properties.Settings.Default.sEnableHalt) { throw new MethodNotImplementedException("Halt"); }
+            if (!Properties.Settings.Default.sEnableHalt) { throw new Exception("Device not capable"); }
             else { HaltRequested = true; }
         }
         #endregion
