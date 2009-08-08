feb6ab661f17c4c212fceb69976d45127bcb5095
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2009-08-08 00:34:32 +0000

    Added new assembly ASCOM.Setup.TemplateWizard which implements a GUI extension in Visual Studio's New Project dialog. Condensed the template projects into a single universal template for each supported language (currently C# and VB). The new template project is configured to activate the TemplateWizard assembly, which pops up a dialog to collect additional information from the user. This information is used to further customise the generated template Driver. Updated the x64 installer to install the ASCOM.Setup.TemplateWizard assembly in the GAC and also added the core components to the GAC.

--- DriverTemplates/CSharpBase/Test.js
+++ DriverTemplates/CSharpBase/Test.js
@@ -1,6 +1,6 @@
 var H = new ActiveXObject("DriverHelper.Chooser");
 WScript.Echo("Choose your new driver in the Chooser that will appear. Then click Properties to activate its SetupDialog");
-H.DeviceType = "$safeprojectname$";					// Make chooser for $safeprojectname$s
+H.DeviceType = "$deviceclass$";					// Make chooser for $deviceclass$ devices
 var F = new ActiveXObject(H.Choose(""));			// Create instance of selected Driver
 F = null;
 H = null;
