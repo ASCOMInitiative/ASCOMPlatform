e9ebb7230cf706e165d9685bd6a33490177ab4c4
PaulKanevsky <PaulKanevsky@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2009-08-22 21:33:11 +0000

    Added GeminiProperties class and frmStatus pop-up status form.

---
+++ trunk/Drivers and Simulators/Gemini Driver/GeminiTelescope/frmStatus.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ASCOM.GeminiTelescope
+{
+    public partial class frmStatus : Form
+    {
+        Timer tmrUpdate = new Timer();
+
+        public bool AutoHide = false;
+
+        public frmStatus()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmStatus_Load(object sender, EventArgs e)
+        {
+            this.geminiPropertiesBindingSource.Add(new GeminiProperties());
+            tmrUpdate.Interval = 1500;
+            tmrUpdate.Tick += new EventHandler(tmrUpdate_Tick);
+            tmrUpdate.Start();
+        }
+
+        void tmrUpdate_Tick(object sender, EventArgs e)
+        {
+            geminiPropertiesBindingSource.ResetBindings(false);
+            if (AutoHide)
+            {
+                if (Cursor.Position.X < this.Left || Cursor.Position.Y < this.Top)
+                    this.Close();
+            }
+        }
+
+        private void geminiPropertiesBindingSource_CurrentItemChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void frmStatus_Move(object sender, EventArgs e)
+        {
+            AutoHide = false;   //once the user moves this, it will no longer go away automatically
+        }
+
+    }
+}
