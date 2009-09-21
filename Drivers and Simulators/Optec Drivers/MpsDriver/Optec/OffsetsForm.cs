d29c795a20e91e3dadc56de066fa8560b3b9d1e4
JordanSchaenzle <JordanSchaenzle@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2009-09-21 14:39:47 +0000

    Started User Interface

---
+++ Drivers and Simulators/Optec Drivers/MpsDriver/Optec/OffsetsForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ASCOM.Optec
+{
+    public partial class OffsetsForm : Form
+    {
+        public OffsetsForm()
+        {
+            InitializeComponent();
+        }
+        public OffsetsForm(int currentPos)
+        {
+            InitializeComponent();
+            PortNumber_CB.SelectedIndex = currentPos;
+        }
+
+        private void OK_Btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+  
+    }
+}
--- Drivers and Simulators/Optec Drivers/MpsDriver/Optec/SetupDialogForm.cs
+++ Drivers and Simulators/Optec Drivers/MpsDriver/Optec/SetupDialogForm.cs
@@ -50,5 +50,49 @@ namespace ASCOM.Optec
             //MessageBox.Show(DeviceComm.ComState.ToString());
 
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    pictureBox1.Image = Properties.Resources.Rotator1;
+                    break;
+                case 1:
+                    pictureBox1.Image = Properties.Resources.Rotator2;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.Rotator3;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.Rotator4;
+                    break;
+            }
+            
+        }
+
+        private void LED_CheckedChanged(object sender, EventArgs e)
+        {
+            if (LEDOn_RB.Checked)
+            {
+                pictureBox2.Image = Properties.Resources.LEDOn;
+            }
+            if (LEDOff_RB.Checked)
+            {
+                pictureBox2.Image = Properties.Resources.LEDOff;
+            }
+        }
+
+        private void SetupDialogForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Offsets_Btn_Click(object sender, EventArgs e)
+        {
+            OffsetsForm OffsetSetup = new OffsetsForm(1);
+            OffsetSetup.StartPosition = FormStartPosition.CenterParent;
+            OffsetSetup.ShowDialog();
+        }
 	}
 }
