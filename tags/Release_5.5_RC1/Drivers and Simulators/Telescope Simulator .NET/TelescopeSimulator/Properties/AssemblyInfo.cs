c4eb986678b1109c885a51a707840adf0d06a7cb
PeterSimpson <PeterSimpson@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2009-09-22 06:25:22 +0000

    Platform updater 5.5 Release candidate 1

---
+++ tags/Release_5.5_RC1/Drivers and Simulators/Telescope Simulator .NET/TelescopeSimulator/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ASCOM TelescopeSimulator server")]
+[assembly: AssemblyDescription("ASCOM multi-interface server for TelescopeSimulator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © 2009, Robert Turner")]
+
+// Version information for Platform components is set at the solution level.
+
+[assembly: ComVisibleAttribute(false)]
