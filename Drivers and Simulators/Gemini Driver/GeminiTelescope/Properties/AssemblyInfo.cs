f983b6ed4075b70e5d6c67bd55cb29c835a20d5b
PaulKanevsky <PaulKanevsky@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2009-10-20 22:32:11 +0000

    Attempted fix for PulseGuide method Additional logging to determine Joystick capabilities

--- Drivers and Simulators/Gemini Driver/GeminiTelescope/Properties/AssemblyInfo.cs
+++ Drivers and Simulators/Gemini Driver/GeminiTelescope/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.31.0")]
-[assembly: AssemblyFileVersion("1.0.31.0")]
+[assembly: AssemblyVersion("1.0.33.0")]
+[assembly: AssemblyFileVersion("1.0.33.0")]
 
 [assembly: ComVisibleAttribute(false)]
