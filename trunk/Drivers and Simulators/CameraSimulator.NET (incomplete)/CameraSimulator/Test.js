84e0520049f484c63ce180c5274259c2e4574bb6
ChrisRowland <ChrisRowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-01-09 19:18:06 +0000

    Add masses of properties in the application and setup. Implement a lot of the basic functionality. Passes Conform with one error.

--- trunk/Drivers and Simulators/CameraSimulator.NET (incomplete)/CameraSimulator/Test.js
+++ trunk/Drivers and Simulators/CameraSimulator.NET (incomplete)/CameraSimulator/Test.js
@@ -1,6 +1,7 @@
 var H = new ActiveXObject("DriverHelper.Chooser");
 WScript.Echo("Choose your new driver in the Chooser that will appear. Then click Properties to activate its SetupDialog");
 H.DeviceType = "Camera";							// Make chooser for Camera
-var F = new ActiveXObject(H.Choose(""));			// Create instance of selected Driver
+var F = new ActiveXObject(H.Choose("")); 		// Create instance of selected Driver
+WScript.Echo("Description " + F.Description);
 F = null;
 H = null;
