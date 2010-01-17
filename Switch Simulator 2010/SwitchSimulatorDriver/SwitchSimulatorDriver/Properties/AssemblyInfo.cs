87cfea6369f7238c4a92ece1877457d6b622d7ef
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-01-17 23:55:46 +0000

    Rob's first test upload

---
+++ Switch Simulator 2010/SwitchSimulatorDriver/SwitchSimulatorDriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+// TODO - Add your authorship information here
+[assembly: AssemblyTitle("Switch Simulator Driver")]
+[assembly: AssemblyDescription("ASCOM Switch Driver for Switch Simulator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ASCOM Initiative")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bfa20802-0ed3-4e85-a13d-4ead82760ab1")]
+
