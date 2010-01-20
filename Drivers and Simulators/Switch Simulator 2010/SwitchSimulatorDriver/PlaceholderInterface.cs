b66473d1ec2c8d8c1b2d9bb1ee3c9bc18006c048
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-01-20 02:39:01 +0000

    Updated Switch Simulator Projects with newer templates

---
+++ Drivers and Simulators/Switch Simulator 2010/SwitchSimulatorDriver/PlaceholderInterface.cs
@@ -0,0 +1,8 @@
+﻿namespace ASCOM.SwitchSimulator
+{
+	interface ISwitch
+	{
+		// Dummy interface just to stop compile errors during development.
+		// This file is not needed and can be deleted.
+	}
+}
