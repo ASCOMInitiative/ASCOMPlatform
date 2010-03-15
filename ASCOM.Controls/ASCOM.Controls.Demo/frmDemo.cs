52c29b6b00bcb2964249a17fc0341cf9765c77e5
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-03-15 04:25:04 +0000

    Completed the Anunciator control along with the supporting AnunciatorPanel in ASCOM.Controls. Added some additional extension methods to support Anunciator. Added some additional unit tests for the same. Added an xUnit project file in the root folder, which makes it easier to wire-up xUnit an 'external tool'. This commit deliberately has 2 failing unit tests aimed at testing TeamCity features. They will be fixed in the next commit.

--- ASCOM.Controls/ASCOM.Controls.Demo/frmDemo.cs
+++ ASCOM.Controls/ASCOM.Controls.Demo/frmDemo.cs
@@ -18,13 +18,6 @@ namespace ASCOM.Controls.Demo
 			InitializeComponent();
 			}
 
-		private void ctlChooser_SelectionChanged(object sender, EventArgs e)
-			{
-			Trace.WriteLine(
-				String.Format("New device selection is: {0} | {1}", ctlTelescopeChooser.SelectedDevice.DeviceID, ctlTelescopeChooser.SelectedDevice.Description)
-				);
-			}
-
 		private void frmDemo_Load(object sender, EventArgs e)
 			{
 			Trace.WriteLine("Loading frmDemo");
@@ -38,28 +31,6 @@ namespace ASCOM.Controls.Demo
 		private void btnCancel_Click(object sender, EventArgs e)
 			{
 			this.Close();
-			}
-
-		private void ctlCameraChooser_SelectionChanged(object sender, EventArgs e)
-			{
-			Trace.WriteLine("Camera selection changed");
-			ledCameraChosen.Cadence = CadencePattern.BlinkSlow;
-			ledCameraChosen.Green = true;
-			ledCameraChosen.Red = false;
-			ledCameraConfigured.Red = true;
-			ledCameraConfigured.Green = false;
-			ledCameraConfigured.Cadence = CadencePattern.BlinkAlarm;
-			}
-
-		private void ctlTelescopeChooser_SelectionChanged(object sender, EventArgs e)
-			{
-			Trace.WriteLine("Telescope selection changed");
-			ledTelescopeChosen.Cadence = CadencePattern.BlinkSlow;
-			ledTelescopeChosen.Green = true;
-			ledTelescopeChosen.Red = false;
-			ledTelescopeConfigured.Red = true;
-			ledTelescopeConfigured.Green = false;
-			ledTelescopeConfigured.Cadence = CadencePattern.BlinkAlarm;
 			}
 		}
 	}
