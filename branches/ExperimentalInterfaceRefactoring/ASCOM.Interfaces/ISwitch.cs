1cd225cbb9f0aac4db14e6dbcdc529f43960f83c
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-05-31 19:53:04 +0000

    SetSwitch to add bool

--- branches/ExperimentalInterfaceRefactoring/ASCOM.Interfaces/ISwitch.cs
+++ branches/ExperimentalInterfaceRefactoring/ASCOM.Interfaces/ISwitch.cs
@@ -2,16 +2,28 @@
 using System.Collections;
 namespace ASCOM.Interfaces
 {
+    /// <summary>
+    /// Defines the properties and methods that are common to switch.
+    /// </summary> 
     public interface ISwitch : IDeviceControl, IAscomDriver
     {
+        /// <summary>
+        /// Dispose the late-bound interface, if needed. Will release it via COM
+        /// if it is a COM object, else if native .NET will just dereference it
+        /// for GC.
+        /// </summary>
         void Dispose();
+
         /// <summary>
         /// Yields a collection of ISwitchDevice objects.
         /// </summary>
         ArrayList Switches { get; }
+
         /// <summary>
-        /// True = on, False = off.
+        /// Sets a switch to on or off
         /// </summary>
-        void SetSwitch(string Name);
+        /// <param name="Name">Name=name of switch to set</param> 
+        /// <param name="State">True=On, False=Off</param> 
+        void SetSwitch(string Name, bool State);
     }
 }
