b366e3fa0638aaf40c524509aa43f885b78ed756
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-06-15 01:07:08 +0000

    Completing the AnnunciatorPanel and Annunciator controls. Ready for review.

---
+++ ASCOM.Controls/ASCOM.Controls/AnnunciatorPanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ASCOM.Controls
+{
+	/// <summary>
+	///   A panel control for grouping and arranging <see cref = "Annunciator" /> controls.
+	///   This control inherits most of its behaviour from the <see cref = "FlowLayoutPanel" />
+	///   base class, but provides some defaults that are appropriate for use with ASCOM.
+	/// </summary>
+	public sealed class AnnunciatorPanel : FlowLayoutPanel
+	{
+		/// <summary>
+		///   Initializes a new instance of the <see cref = "AnnunciatorPanel" /> class.
+		/// </summary>
+		public AnnunciatorPanel()
+		{
+			BackColor = Color.FromArgb(64, 0, 0);
+		}
+	}
+}
