92ec978ad4d3345386ccfb9d51d74293c8d7528b
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-08-23 21:07:37 +0000

    Added a simple test harness console application.

---
+++ trunk/Drivers and Simulators/Optec Drivers/PyxisLE/TestHarness/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ASCOM.Utilities;
+using ASCOM.DriverAccess;
+
+namespace TestHarness
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var progId = Rotator.Choose(string.Empty);
+			var driver = new Rotator(progId);
+			var thing = driver.CanReverse;					// Just to give a place to set a breakpoint.
+			driver.SetupDialog();
+			Console.WriteLine("Press any key to exit");
+			Console.ReadLine();
+		}
+	}
+}
