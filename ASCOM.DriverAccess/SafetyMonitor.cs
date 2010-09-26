4894556eebca82fcd6ee5a032dafe36abe7ff020
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-09-26 13:32:16 +0000

    Added initial draft of SafetyMonitor.cs class

--- ASCOM.DriverAccess/SafetyMonitor.cs
+++ ASCOM.DriverAccess/SafetyMonitor.cs
@@ -1,10 +1,10 @@
-//
-// 10-Jul-08	rbd		1.0.5 - Release COM on Dispose().
-//
+//-----------------------------------------------------------------------
+// <summary>Defines the SafetyMonitor class.</summary>
+//-----------------------------------------------------------------------
+// 25-Sept-10  	rem     6.0.0 - Initial draft of the class
+
 using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using ASCOM.Interface;
+using ASCOM.DeviceInterface;
 using ASCOM.Utilities;
 
 namespace ASCOM.DriverAccess
@@ -13,241 +13,50 @@ namespace ASCOM.DriverAccess
     /// <summary>
     /// Provides universal access to SafetyMonitor drivers
     /// </summary>
-    public class SafetyMonitor : ASCOM.Interface.ISafetyMonitor, IDisposable
+    public class SafetyMonitor : AscomDriver, ISafetyMonitor
     {
-        object objSafetyMonitorLateBound;
-        ASCOM.Interface.ISafetyMonitor ISafetyMonitor;
-        Type objTypeSafetyMonitor;
+        #region SafetyMonitor constructors
+
+        private readonly MemberFactory _memberFactory;
 
         /// <summary>
         /// Creates a SafetyMonitor object with the given Prog ID
         /// </summary>
-        /// <param name="SafetyMonitorID"></param>
-        public SafetyMonitor(string SafetyMonitorID)
+        /// <param name="safetyMonitorId"></param>
+        public SafetyMonitor(string safetyMonitorId)
+            : base(safetyMonitorId)
         {
-            // Get Type Information 
-            objTypeSafetyMonitor = Type.GetTypeFromProgID(SafetyMonitorID);
-
-            // Create an instance of the SafetyMonitor object
-            objSafetyMonitorLateBound = Activator.CreateInstance(objTypeSafetyMonitor);
-
-            // Try to see if this driver has an ASCOM.SafetyMonitor interface
-            try
-            {
-                ISafetyMonitor = (ASCOM.Interface.ISafetyMonitor)objSafetyMonitorLateBound;
-            }
-            catch (Exception)
-            {
-                ISafetyMonitor = null;
-            }
-
+            _memberFactory = MemberFactory;
         }
+        #endregion
 
+        #region Convenience Members
         /// <summary>
         /// Brings up the ASCOM Chooser Dialog to choose a SafetyMonitor
         /// </summary>
-        /// <param name="SafetyMonitorID">SafetyMonitor Prog ID for default or null for None</param>
+        /// <param name="safetyMonitorId">SafetyMonitor Prog ID for default or null for None</param>
         /// <returns>Prog ID for chosen SafetyMonitor or null for none</returns>
-        public static string Choose(string SafetyMonitorID)
+        public static string Choose(string safetyMonitorId)
         {
-            Chooser oChooser = new Chooser();
-            oChooser.DeviceType = "SafetyMonitor";			// Requires Helper 5.0.3 (May '07)
-            return oChooser.Choose(SafetyMonitorID);
+            var oChooser = new Chooser { DeviceType = "SafetyMonitor" };
+            return oChooser.Choose(safetyMonitorId);
         }
+
+        #endregion
 
         #region ISafetyMonitor Members
 
         /// <summary>
-        /// Confirms the ability to flag an  emergency shutdown
+        /// Tells is the SafetyMonitor is set to safe
         /// </summary>
-        public bool CanEmergencyShutdown
-        {
-            get
-            {
-                if (ISafetyMonitor != null)
-                    return ISafetyMonitor.CanEmergencyShutdown;
-                else
-                    return Convert.ToBoolean(objTypeSafetyMonitor.InvokeMember("CanEmergencyShutdown",
-                        BindingFlags.Default | BindingFlags.GetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { }));
-            }
-        }
-
-        /// <summary>
-        /// Flags the ability to return usability condition
-        /// </summary>
-        public bool CanIsGood
-        {
-            get
-            {
-                if (ISafetyMonitor != null)
-                    return ISafetyMonitor.CanIsGood;
-                else
-                    return Convert.ToBoolean(objTypeSafetyMonitor.InvokeMember("CanIsGood",
-                        BindingFlags.Default | BindingFlags.GetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { }));
-            }
-        }
-
-        /// <summary>
-        /// Set True to Connect to the SafetyMonitores; set False to terminate the Connection.
-        /// The current Connected status can also be read back as this property.
-        /// An exception will be raised if the Connected fails to change state for any reason.
-        /// </summary>
-        public bool Connected
-        {
-            get
-            {
-                if (ISafetyMonitor != null)
-                    return ISafetyMonitor.Connected;
-                else
-                    return Convert.ToBoolean(objTypeSafetyMonitor.InvokeMember("Connected",
-                        BindingFlags.Default | BindingFlags.GetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { }));
-            }
-            set
-            {
-                if (ISafetyMonitor != null)
-                    ISafetyMonitor.Connected = value;
-                else
-                    objTypeSafetyMonitor.InvokeMember("Connected",
-                        BindingFlags.Default | BindingFlags.SetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { value });
-            }
-        }
-
-        /// <summary>
-        /// Returns a description of the SafetyMonitor controller
-        /// </summary>
-        public string Description
-        {
-            get
-            {
-                if (ISafetyMonitor != null)
-                    return ISafetyMonitor.Description;
-                else
-                    return Convert.ToString(objTypeSafetyMonitor.InvokeMember("Description",
-                        BindingFlags.Default | BindingFlags.GetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { }));
-            }
-        }
-
-        /// <summary>
-        /// Returns the driver info
-        /// </summary>
-        public string DriverInfo
-        {
-            get
-            {
-                if (ISafetyMonitor != null)
-                    return ISafetyMonitor.DriverInfo;
-                else
-                    return Convert.ToString(objTypeSafetyMonitor.InvokeMember("DriverInfo",
-                        BindingFlags.Default | BindingFlags.GetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { }));
-            }
-        }
-
-        /// <summary>
-        /// Returns the driver version
-        /// </summary>
-        public string DriverVersion
-        {
-            get
-            {
-                if (ISafetyMonitor != null)
-                    return ISafetyMonitor.DriverVersion;
-                else
-                    return Convert.ToString(objTypeSafetyMonitor.InvokeMember("DriverVersion",
-                        BindingFlags.Default | BindingFlags.GetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { }));
-            }
-        }
-
-        /// <summary>
-        /// Returns the emergency shutdown state
-        /// </summary>
-        public bool EmergencyShutdown
-        {
-            get
-            {
-                if (ISafetyMonitor != null)
-                    return ISafetyMonitor.EmergencyShutdown;
-                else
-                    return Convert.ToBoolean(objTypeSafetyMonitor.InvokeMember("EmergencyShutdown",
-                        BindingFlags.Default | BindingFlags.GetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { }));
-            }
-        }
-
-        /// <summary>
-        /// Returns the usability conditions
-        /// </summary>
-        public bool IsGood
-        {
-            get
-            {
-                if (ISafetyMonitor != null)
-                    return ISafetyMonitor.IsGood;
-                else
-                    return Convert.ToBoolean(objTypeSafetyMonitor.InvokeMember("IsGood",
-                        BindingFlags.Default | BindingFlags.GetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { }));
-            }
-        }
-
-        /// <summary>
-        /// Returns the safety conditions
-        /// </summary>
+        /// <value></value>
         public bool IsSafe
         {
-            get
-            {
-                if (ISafetyMonitor != null)
-                    return ISafetyMonitor.IsSafe;
-                else
-                    return Convert.ToBoolean(objTypeSafetyMonitor.InvokeMember("IsSafe",
-                        BindingFlags.Default | BindingFlags.GetProperty,
-                        null, objSafetyMonitorLateBound, new object[] { }));
-            }
-        }
-
- 
-        ///<summary>
-        ///Launches a configuration dialog box for the driver.  The call will not return
-        ///until the user clicks OK or cancel manually.
-        ///</summary>
-        ///<exception cref=" System.Exception">Must throw an exception if Setup dialog is unavailable.</exception>
-        public void SetupDialog()
-        {
-            if (ISafetyMonitor != null)
-                ISafetyMonitor.SetupDialog();
-            else
-                objTypeSafetyMonitor.InvokeMember("SetupDialog",
-                    BindingFlags.Default | BindingFlags.InvokeMethod,
-                    null, objSafetyMonitorLateBound, new object[] { });
+            get { return (bool)_memberFactory.CallMember(1, "IsSafe", new Type[] { }, new object[] { }); }
         }
 
         #endregion
 
-        #region IDisposable Members
-
-        /// <summary>
-        /// Dispose the late-bound interface, if needed. Will release it via COM
-        /// if it is a COM object, else if native .NET will just dereference it
-        /// for GC.
-        /// </summary>
-        public void Dispose()
-        {
-            if (this.objSafetyMonitorLateBound != null)
-            {
-                try { Marshal.ReleaseComObject(objSafetyMonitorLateBound); }
-                catch (Exception) { }
-                objSafetyMonitorLateBound = null;
-            }
-        }
-
-        #endregion
     }
     #endregion
 }
