14b89f07393e215b40d87ce2dbd6d4f87ccd7b0d
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-10-07 00:09:53 +0000

    Cleaned up the C# AssemblyVersionInfo and SolutionInfo files and created VB equivalents.

---
+++ AssemblyVersionInfo.vb
@@ -0,0 +1,20 @@
+﻿' This file sets the assembly version for all generated assemblies.
+' All projects in the build should reference this file by including the following
+' in their project file:
+'
+'   <Compile Include="..\AssemblyVersionInfo.vb">
+'     <Link>Properties\AssemblyVersionInfo.vb</Link>
+'   </Compile>
+'   <Compile Include="..\SolutionInfo.vb">
+'     <Link>Properties\SolutionInfo.vb</Link>
+'   </Compile>
+' 
+' Version information is isolated in a seperate file to facilitate integration with the build server.
+' The build server will replace this entire file with a machine-generated one. Therefore,
+' this file should contain ONLY the global version number and no other settings.
+' 
+' For local builds, the major and minor versions are fixed and the
+' revision and build numbers are set automatically by Visual Studio.
+
+<Assembly: AssemblyVersion("6.0.*")> 
+<Assembly: AssemblyFileVersionAttribute("6.0")> 
