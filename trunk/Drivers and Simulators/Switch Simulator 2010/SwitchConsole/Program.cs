1bd1bf52abdeb0d8d55da1c6003c7569e4a42a97
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-10-10 16:03:28 +0000

    SMALL UPDATES testing this pre-build stuff

--- trunk/Drivers and Simulators/Switch Simulator 2010/SwitchConsole/Program.cs
+++ trunk/Drivers and Simulators/Switch Simulator 2010/SwitchConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Linq;
+using SafetyMonitor = ASCOM.Simulator.SafetyMonitor;
 using Switch = ASCOM.Simulator.Switch;
+using ASCOM.Utilities;
 
 
 namespace SwitchConsole
@@ -10,21 +11,25 @@ namespace SwitchConsole
         static void Main()
         {
 
-            var obj = new Switch();
-            // Activator.CreateInstance(Type.GetTypeFromProgID("ASCOM.Simulator.Switch")));
-            var list = obj.Switches;
-            foreach (string[,] swithItems in list.Cast<string[,]>().Where(swithItems => swithItems != null))
-            {
-                Console.WriteLine(swithItems[0,0] + @" = " + swithItems[0,1]);
-            }
-            Console.WriteLine(obj.DriverInfo);
-            Console.WriteLine(obj.Name);
-            Console.WriteLine(obj.InterfaceVersion);
-            Console.WriteLine(obj.LastResult);
+            //var obj = new SafetyMonitor();
+            // //Activator.CreateInstance(Type.GetTypeFromProgID("ASCOM.Simulator.Switch")));
+            //var list = obj.Switches;
+            //foreach (string[,] swithItems in list.Cast<string[,]>().Where(swithItems => swithItems != null))
+            //{
+            //    Console.WriteLine(swithItems[0, 0] + @" = " + swithItems[0, 1]);
+            //}
+            //Console.WriteLine(obj.DriverInfo);
+            //Console.WriteLine(obj.Name);
+            //Console.WriteLine(obj.InterfaceVersion);
+            //Console.WriteLine(obj.LastResult);
 
+
+            //var p = new Profile();
+            //p.Register();
 
             var chooser = new ASCOM.Utilities.Chooser {DeviceType = "Switch"};
             chooser.Choose();
+
 
         }
     }
