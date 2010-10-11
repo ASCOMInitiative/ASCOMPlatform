17a0635577e928022c52acbde7c468a20f1b815d
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-10-11 02:47:48 +0000

    Augmented the DeviceIdAttribute class with an optional named parameter, DeviceName. It is anticipated that this will be useful in some deployment situations, where an installer custom action might need to perform registration with the ASCOM Profile but is unable to call code within the driver itself to do so. This attribute allows the installer custom action to discover the necessary data without running any code in the driver.

--- ASCOM.Attributes/DeviceIdAttribute.cs
+++ ASCOM.Attributes/DeviceIdAttribute.cs
@@ -5,14 +5,30 @@ using System.Text;
 namespace ASCOM
 {
 	/// <summary>
-	/// An attribute for specifying an associated ASCOM DeviceID (aka COM ProgID).
-	/// This is intended primarily for use with the ASCOM.SettingsProvider class.
-	/// This attribute is placed on the driver's <c>Properties.Settings</c> class, which propagates
-	/// down to each of the settings properties. When the setting is passed to the
-	/// ASCOM.SettingsProvider class at runtime, the settings provider looks for this attribute
-	/// to determine which settings hive to save the value in when it is passed to 
-	/// <see cref="T:ASCOM.Utilities.Profile"/>.
+	/// An attribute for declaratively associating an assembly, class or property with an ASCOM device.
 	/// </summary>
+	/// <remarks>
+    /// This attribute is intended for use int two main situations:
+    /// <list type="numbered">
+    /// <item>Settings management and integration with Visual Studio designers</item>
+    /// <description>
+    /// When this attribute is placed on the driver's <c>Properties.Settings</c> class, it  propagates
+    /// down to each of the settings properties. When the setting is passed to the 
+    /// <c>ASCOM.SettingsProvider</c> class at runtime, the settings provider looks for this attribute
+    /// to determine which settings hive to save the value in when it is passed to 
+    /// <see cref="T:ASCOM.Utilities.Profile"/>.
+    /// </description>
+    /// <item>Deployment</item>
+    /// <description>
+    /// The values in this attribute could be used by an installer custom action to perform
+    /// ASCOM registration during setup. Historically this has been handled programmatically,
+    /// but there are trends towards a more declarative approach to deployment (for example
+    /// WiX, Windows Installer Xml). It is expected that such an installer may need to obtain
+    /// registration data by reflecting on the assemblies being installed. Placing this attribute
+    /// at the assembly level will assist in this situation.
+    /// </description>
+    /// </list>
+	/// </remarks>
 	[global::System.AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly | AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
 	public sealed class DeviceIdAttribute : Attribute
 	{
@@ -26,8 +42,14 @@ namespace ASCOM
 		}
 
 		/// <summary>
-		/// Gets the ASCOM DeviceID that the attribute is associated with.
+		/// Gets the ASCOM DeviceID, also known as the COM ProgID.
 		/// </summary>
 		public string DeviceId { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the display name of the device. This would be the short display name, as displayed in the ASCOM Chooser.
+        /// </summary>
+        /// <value>The name of the device.</value>
+        public string DeviceName { get; set; }
 	}
 }
