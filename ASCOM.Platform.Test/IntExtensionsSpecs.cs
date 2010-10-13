dbb075fceda211a450817143a2a0313fef4693a4
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-10-13 02:21:06 +0000

    Augmented the DeviceId attribute with an optional named parameter, 'DeviceName'. Converted all unit tests to use Machine.Specifications BDD framework. Created namespace documentation for ASCOM.Internal

---
+++ ASCOM.Platform.Test/IntExtensionsSpecs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using ASCOM.Internal;
+using Machine.Specifications;
+
+namespace ASCOM.Platform.Test
+{
+
+    public class With_uint_register
+    {
+        protected static uint register;
+        protected static Exception ex;
+    }
+    public class With_int_register
+    {
+        protected static uint register;
+        protected static Exception ex;
+    }
+    [Subject(typeof(IntExtensions), "uint register")]
+    public class When_UIntBit_LsbSet : With_uint_register
+    {
+        Establish context = () => register = 0x00000001;
+        Because of = () => ex = Catch.Exception(() => register.Bit(32));
+        It should_be_true_for_bit_0 = () => register.Bit(0).ShouldBeTrue();
+        It should_be_false_for_bit_1 = () => register.Bit(1).ShouldBeFalse();
+        It should_be_false_for_bit_31 = () => register.Bit(31).ShouldBeFalse();
+        It should_throw_exception_for_bit_32 = () => ex.ShouldBeOfType<ArgumentOutOfRangeException>();
+    }
+    [Subject(typeof(IntExtensions), "uint register")]
+    public class When_UIntBit_MsbSet : With_uint_register
+    {
+        Establish context = () => register = 0x80000000U;
+        Because of = () => ex = Catch.Exception(() => register.Bit(32));
+        It should_be_false_for_bit_0 = () => register.Bit(0).ShouldBeFalse();
+        It should_be_false_for_bit_1 = () => register.Bit(1).ShouldBeFalse();
+        It should_be_true_for_bit_31 = () => register.Bit(31).ShouldBeTrue();
+        It should_throw_exception_for_bit_32 = () => ex.ShouldBeOfType<ArgumentOutOfRangeException>();
+    }
+    [Subject(typeof(IntExtensions), "int register")]
+    public class When_IntBit_LsbSet : With_int_register
+    {
+        Establish context = () => register = 0x00000001;
+        Because of = () => ex = Catch.Exception(() => register.Bit(32));
+        It should_be_true_for_bit_0 = () => register.Bit(0).ShouldBeTrue();
+        It should_be_false_for_bit_1 = () => register.Bit(1).ShouldBeFalse();
+        It should_be_false_for_bit_31 = () => register.Bit(31).ShouldBeFalse();
+        It should_throw_exception_for_bit_32 = () => ex.ShouldBeOfType<ArgumentOutOfRangeException>();
+    }
+    [Subject(typeof(IntExtensions), "int register")]
+    public class When_IntBit_MsbSet : With_int_register
+    {
+        Establish context = () => register = 0x80000000U;
+        Because of = () => ex = Catch.Exception(() => register.Bit(32));
+        It should_be_false_for_bit_0 = () => register.Bit(0).ShouldBeFalse();
+        It should_be_false_for_bit_1 = () => register.Bit(1).ShouldBeFalse();
+        It should_be_true_for_bit_31 = () => register.Bit(31).ShouldBeTrue();
+        It should_throw_exception_for_bit_32 = () => ex.ShouldBeOfType<ArgumentOutOfRangeException>();
+    }
+
+}
