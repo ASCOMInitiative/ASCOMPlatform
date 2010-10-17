f5ea3577cc9feb5a6e67479e1ace2f0fafd7089b
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-10-17 22:31:23 +0000

    updates for focuser simulator

--- trunk/Drivers and Simulators/Focus Simulator 2010/FocuserSimulatorDriver/SetupDialogForm.Designer.cs
+++ trunk/Drivers and Simulators/Focus Simulator 2010/FocuserSimulatorDriver/SetupDialogForm.Designer.cs
@@ -52,22 +52,25 @@
             // 
             // button1
             // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.button1.Location = new System.Drawing.Point(62, 37);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(37, 25);
             this.button1.TabIndex = 1;
             this.button1.Text = "In";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
             // 
             // button2
             // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.button2.Location = new System.Drawing.Point(119, 38);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(39, 24);
             this.button2.TabIndex = 2;
             this.button2.Text = "Out";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Button2Click);
             // 
             // label1
             // 
@@ -89,7 +92,6 @@
             this.label2.Size = new System.Drawing.Size(48, 18);
             this.label2.TabIndex = 4;
             this.label2.Text = "00000";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -114,15 +116,20 @@
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
             this.button3.Location = new System.Drawing.Point(21, 142);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(59, 32);
             this.button3.TabIndex = 7;
             this.button3.Text = "Setup";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3Click);
             // 
             // button4
             // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.button4.Location = new System.Drawing.Point(97, 142);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(59, 32);
@@ -140,11 +147,13 @@
             this.checkBox1.TabIndex = 9;
             this.checkBox1.Text = "Tempature Compensation";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1CheckedChanged);
             // 
             // SetupDialogForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(175, 197);
@@ -158,7 +167,11 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "SetupDialogForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Focuser Simulator";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -175,9 +188,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
 
     }
 }
--- trunk/Drivers and Simulators/Focus Simulator 2010/FocuserSimulatorDriver/SetupDialogForm.cs
+++ trunk/Drivers and Simulators/Focus Simulator 2010/FocuserSimulatorDriver/SetupDialogForm.cs
@@ -1,30 +1,102 @@
 ﻿using System;
-using System.Drawing;
 using System.Windows.Forms;
-using System.Reflection;
-using System.IO;
-using ASCOM.DeviceInterface;
 
 namespace ASCOM.Simulator
 {
     public partial class SetupDialogForm : Form
     {
-        private static readonly IFocuser Focuser = new Focuser();
-        Assembly _assembly;
+        // Sets up an instance of focuser driver for use in this class
+        private readonly Focuser _focuser = new Focuser();
 
+        // sets up a new timer to use
+        static readonly Timer MyTimer = new Timer();
+
+        // This is the method to run when the class is constructed
         public SetupDialogForm()
         {
             InitializeComponent();
+            UpdatePosition(0);
+            UpdateTempature(0);
+
+            checkBox1.Enabled = _focuser.TempCompAvailable;
+            if (checkBox1.Checked)
+            {MyTimer.Start();}
+            else
+            {
+                MyTimer.Stop();
+            }
+
+            /* Adds the event and the event handler for the method that will 
+          process the timer event to the timer. */
+            MyTimer.Tick += TimerEventProcessor;
+
+            // Sets the timer interval to 1 seconds.
+            MyTimer.Interval = 1000;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        // This is the method to run when the timer is raised.
+        private void TimerEventProcessor(Object myObject, EventArgs myEventArgs)
         {
+            MyTimer.Enabled = true;
 
+            //todo  add in what happens when the temp comp is turned on
+            UpdateTempature(-.1);
+            
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        // This is the method to run when the tempature needs updated, also updates the screen
+        private void UpdateTempature(double offSet)
         {
+            _focuser.Temperature = _focuser.Temperature + offSet;
+            label4.Text = _focuser.TempProbe ? _focuser.Temperature.ToString() : 0.ToString();
+        }
 
+        // This is the method to run when the position needs updated, also updates the screen
+        private void UpdatePosition(int offSet )
+        {
+            if (offSet != 0)
+            {
+                _focuser.Move(offSet);
+            }
+            label2.Text = _focuser.Position.ToString();
+        }
+
+        // This is the method to run when the In button is clicked
+        private void Button1Click(object sender, EventArgs e)
+        {
+                UpdatePosition(-250);
+        }
+
+        // This is the method to run when the Out button is clicked
+        private void Button2Click(object sender, EventArgs e)
+        {
+                UpdatePosition(250);
+        }
+
+        // This is the method to run when the Settings button is clicked
+        private void Button3Click(object sender, EventArgs e)
+        {
+            var settingsForm = new SettingsForm();
+            Hide();
+            Close();
+            settingsForm.ShowDialog();
+        }
+
+        // This is the method to run when the tempature compesator is turned on or off
+        private void CheckBox1CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                MyTimer.Start();
+            }
+            else
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                MyTimer.Stop();
+            }
         }
     }
 }
