991d868022a006c6f8c707cf163eb31e8c22db8d
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-10-23 22:15:47 +0000

    Added  Simulator testing project

---
+++ trunk/Drivers and Simulators/SimulatorTestingProject/Simulator Testing Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SafetyMonitor = ASCOM.Simulator.SafetyMonitor;
+using Switch = ASCOM.Simulator.Switch;
+using Focuser = ASCOM.Simulator.Focuser;
+using ASCOM.Utilities;
+
+namespace Simulator_Testing_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var chooser = new ASCOM.Utilities.Chooser { DeviceType = "Switch" };
+            chooser.Choose();
+        }
+    }
+}
