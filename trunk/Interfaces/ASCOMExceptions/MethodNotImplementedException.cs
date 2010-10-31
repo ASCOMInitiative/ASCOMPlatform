1ca488629619fea136b9edc8f9d458e026fb0d12
ChrisRowland <ChrisRowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-10-31 09:55:44 +0000

    Update to satisfy Code Analysis: Changed the base Exception type to System.Exception made the error codes readonly Added Exceptions as suggested Use CultureInfo.InvariantCulture

--- trunk/Interfaces/ASCOMExceptions/MethodNotImplementedException.cs
+++ trunk/Interfaces/ASCOMExceptions/MethodNotImplementedException.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Runtime.Serialization;
+using System.Globalization;
 
 namespace ASCOM
 {
@@ -18,34 +19,57 @@ namespace ASCOM
 	/// for each defined method is to throw the ASCOM.MethodNotImplementedException. Note
 	/// that no default constructor is supplied. Throwing this requires the the method name. 
 	/// </summary>
+    [Serializable]
 	public class MethodNotImplementedException : NotImplementedException
 	{
-		private const string csMessage = "Method {0}";
-		private string m_strMethod = "Unknown";
+        [NonSerialized]
+        private const string csMessage = "Method {0}";
+        [NonSerialized]
+        private string method = "Unknown";
 
 		/// <summary>
 		/// Create a new exception object and identify the specified driver method as the source.
 		/// </summary>
-		/// <param name="strMethod">The name of the driver method that caused the exception.</param>
-		public MethodNotImplementedException(string strMethod)
-			: base(String.Format(csMessage, strMethod))
+		/// <param name="method">The name of the driver method that caused the exception.</param>
+		public MethodNotImplementedException(string method)
+			: base(String.Format(CultureInfo.InvariantCulture,csMessage, method))
 		{
-			this.m_strMethod = strMethod;
+			this.method = method;
 		}
 		/// <summary>
 		/// Create a new exception object and identify the specified driver method as the source,
 		/// and include an inner exception object containing a caught exception.
 		/// </summary>
-		/// <param name="strMethod">The name of the driver method that caused the exception</param>
+		/// <param name="method">The name of the driver method that caused the exception</param>
 		/// <param name="inner">The caught exception</param>
-		public MethodNotImplementedException(string strMethod, System.Exception inner)
-			: base(String.Format(csMessage, strMethod), inner)
+		public MethodNotImplementedException(string method, System.Exception inner)
+			: base(String.Format(CultureInfo.InvariantCulture, csMessage, method), inner)
 		{
-			this.m_strMethod = strMethod;
+			this.method = method;
 		}
+
+        /// <summary>
+        /// For Code Analysis, please don't use
+        /// </summary>
+        public MethodNotImplementedException()
+            : base("Unknown  Method")
+        {
+        }
+
 		/// <summary>
 		/// The method that is not implemented
 		/// </summary>
-		public string Method { get { return m_strMethod; } }
+		public string Method { get { return method; } }
+
+        /// <summary>
+        /// Added to keep Code Analysis happy
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected MethodNotImplementedException(SerializationInfo info, 
+         StreamingContext context) : base(info, context)
+        {
+        }
+
 	}
 }
--- trunk/Interfaces/ASCOMExceptions/ParkedException.cs
+++ trunk/Interfaces/ASCOMExceptions/ParkedException.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace ASCOM
 {
@@ -8,6 +9,7 @@ namespace ASCOM
 	/// This exception should be used to indicate that movement (or other invalid operation) was attempted
 	/// while the device was in a parked state.
 	/// </summary>
+    [Serializable]
 	public class ParkedException : DriverException
 	{
 		/// <summary>
@@ -27,5 +29,34 @@ namespace ASCOM
 			: base("Operation not valid while the device is parked", ErrorCodes.InvalidWhileParked, inner)
 		{
 		}
+
+        /// <summary>
+        /// Keeps Code Analysis happy
+        /// </summary>
+        /// <param name="message"></param>
+        public ParkedException(string message)
+            : base(message, ErrorCodes.InvalidWhileParked)
+        {
+        }
+
+        /// <summary>
+        /// Keeps Code Analysis happy
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public ParkedException(string message, Exception inner)
+            : base(message, ErrorCodes.InvalidWhileParked, inner)
+        {
+        }
+         
+        /// <summary>
+        /// Added to keep Code Analysis happy
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected ParkedException(SerializationInfo info, 
+         StreamingContext context) : base(info, context)
+        {
+        }
 	}
 }
