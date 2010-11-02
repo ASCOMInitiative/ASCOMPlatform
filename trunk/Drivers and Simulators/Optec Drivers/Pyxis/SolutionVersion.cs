98d4a12bca14772dfe01066f771a02dc01c2ebbf
JordanSchaenzle <JordanSchaenzle@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-11-02 16:37:02 +0000

    Created Project for new Pyxis Driver

---
+++ trunk/Drivers and Simulators/Optec Drivers/Pyxis/SolutionVersion.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+// Set the solution version here and make sure to change it in the installer as well.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
