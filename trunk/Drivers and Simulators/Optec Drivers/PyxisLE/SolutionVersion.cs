8be8ab22761454231157d8ab2c6a4b450edc85b1
JordanSchaenzle <JordanSchaenzle@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-11-03 18:01:04 +0000

    Changed solution version number for PyxisLE programs for next version.

--- trunk/Drivers and Simulators/Optec Drivers/PyxisLE/SolutionVersion.cs
+++ trunk/Drivers and Simulators/Optec Drivers/PyxisLE/SolutionVersion.cs
@@ -2,5 +2,5 @@
 
 // Set the version for all of the projects in the solution here...
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
