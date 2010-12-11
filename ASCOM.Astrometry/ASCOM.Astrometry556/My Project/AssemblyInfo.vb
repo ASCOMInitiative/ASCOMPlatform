8134864a936f5e11be9792536762a8c09de07c21
PeterSimpson <PeterSimpson@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-12-11 18:03:50 +0000

    Up-versioned Platform 5.5 replacement Utilities component from 5.5.3 to 5.5.6

--- ASCOM.Astrometry/ASCOM.Astrometry556/My Project/AssemblyInfo.vb
+++ ASCOM.Astrometry/ASCOM.Astrometry556/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("5.5.6.0")> 
-<Assembly: AssemblyFileVersion("5.5.6.0")> 
+<Assembly: AssemblyFileVersion("5.5.60.0")> 
