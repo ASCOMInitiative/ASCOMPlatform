63b498eb6a6f8641a1092ad9aeac7c75b5f2bea9
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2010-12-31 00:30:56 +0000

    At Chris' request, rolling back changes from revisions 1574-75. Committing some outstanding changes to some unit tests.

--- trunk/ASCOM.Platform.UnitTest/FileInfoExtensionsSpecs.cs
+++ trunk/ASCOM.Platform.UnitTest/FileInfoExtensionsSpecs.cs
@@ -1,6 +1,39 @@
-﻿using ASCOM.Internal;
-using Machine.Specifications;
+﻿// --------------------------------------------------------------------------------
+// <summary
+// MSpec Specifications for ASCOM.Internal.Extensions.FileInfoExtensions
+// </summary>
+// 
+// <copyright company="TiGra Astronomy">
+// 	Copyright © 2009-2010 Timothy P. Long, .
+// 	http://www.tigranetworks.co.uk/Astronomy
+// </copyright>
+// 
+// <license>
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </license>
+// 
+// Original Author:		[TPL] Timothy P. Long <Tim@tigranetworks.co.uk>
+
 using System.IO;
+using ASCOM.Internal;
+using Machine.Specifications;
 
 namespace ASCOM.Platform.UnitTest
 {
@@ -23,7 +56,7 @@ namespace ASCOM.Platform.UnitTest
     }
 
     // Telescope.blah.cs => should return true
-    [Subject(typeof(FileInfoExtensions))]
+    [Subject(typeof (FileInfoExtensions))]
     public class When_testing_for_telescope_specific_file : With_empty_fileinfo
     {
         Establish context = () => target = new FileInfo("Telescope.blah.cs");
@@ -31,7 +64,7 @@ namespace ASCOM.Platform.UnitTest
     }
 
     // Telescope.cs => should return false
-    [Subject(typeof(FileInfoExtensions))]
+    [Subject(typeof (FileInfoExtensions))]
     public class When_testing_for_telescope_nonspecific_file : With_empty_fileinfo
     {
         Establish context = () => target = new FileInfo("Telescope.cs");
@@ -39,7 +72,7 @@ namespace ASCOM.Platform.UnitTest
     }
 
     // blah.blah.cs => should return false
-    [Subject(typeof(FileInfoExtensions))]
+    [Subject(typeof (FileInfoExtensions))]
     public class When_testing_for_nonspecific_file_with_three_segments : With_empty_fileinfo
     {
         Establish context = () => target = new FileInfo("blah.blah.cs");
@@ -47,13 +80,10 @@ namespace ASCOM.Platform.UnitTest
     }
 
     // blah.cs => should return false
-    [Subject(typeof(FileInfoExtensions))]
+    [Subject(typeof (FileInfoExtensions))]
     public class When_testing_for_nonspecific_file_with_two_segments : With_empty_fileinfo
     {
         Establish context = () => target = new FileInfo("blah.cs");
         It should_return_true = () => target.IsDeviceSpecific().ShouldBeFalse();
     }
-
-
-
 }
--- trunk/ASCOM.Platform.UnitTest/LEDIndicatorSpecs.cs
+++ trunk/ASCOM.Platform.UnitTest/LEDIndicatorSpecs.cs
@@ -1,4 +1,37 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------
+// <summary
+// MSpec Specifications for ASCOM.Controls.LedIndicator control
+// </summary>
+// 
+// <copyright company="TiGra Astronomy">
+// 	Copyright © 2009-2010 Timothy P. Long, .
+// 	http://www.tigranetworks.co.uk/Astronomy
+// </copyright>
+// 
+// <license>
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </license>
+// 
+// Original Author:		[TPL] Timothy P. Long <Tim@tigranetworks.co.uk>
+
+using System;
 using ASCOM.Controls;
 using Machine.Specifications;
 
@@ -7,60 +40,61 @@ namespace ASCOM.Platform.UnitTest
     [Subject(typeof (LedIndicator), "Control Disposed")]
     public class when_led_control_is_disposed : with_clean_cadencemanager
     {
-        private Because of = () =>
+        Because of = () =>
             {
-                var control = new LedIndicator();
+                LedIndicator control = new LedIndicator();
                 control.Enabled = true; // This ensures the control is active and registered with CadenceManager
                 control.Dispose();
             };
 
-        private It should_unregister_from_cadence_manager = () => CadenceManager.UpdateList.Count.ShouldEqual(0);
+        It should_unregister_from_cadence_manager = () => CadenceManager.UpdateList.Count.ShouldEqual(0);
     }
 
     [Subject(typeof (LedIndicator), "Construction")]
     public class when_led_control_is_created : with_clean_cadencemanager
     {
-        private Because of = () =>
+        Because of = () =>
             {
-                var control = new LedIndicator();
+                LedIndicator control = new LedIndicator();
                 control.Enabled = true; // This ensures the control is active and registered with CadenceManager
             };
 
-        private It should_register_with_cadence_manager = () => CadenceManager.UpdateList.Count.ShouldEqual(1);
+        It should_register_with_cadence_manager = () => CadenceManager.UpdateList.Count.ShouldEqual(1);
     }
 
-    [Subject(typeof(LedIndicator), "Disposal")]
+    [Subject(typeof (LedIndicator), "Disposal")]
     public class when_led_control_is_double_disposed
     {
         protected static LedIndicator led;
         protected static Exception ex;
 
-        private Establish context = () => { led = new LedIndicator(); };
+        Establish context = () => { led = new LedIndicator(); };
 
-        private Because of = () => ex = Catch.Exception(() =>
-        {
-            led.Dispose();
-            led.Dispose();
-        }
-                                            );
-        private It should_not_throw = () => ex.ShouldBeNull();
+        Because of = () => ex = Catch.Exception(() =>
+            {
+                led.Dispose();
+                led.Dispose();
+            }
+                                    );
+
+        It should_not_throw = () => ex.ShouldBeNull();
     }
 
-    [Subject(typeof(LedIndicator), "Disposal")]
+    [Subject(typeof (LedIndicator), "Disposal")]
     public class when_led_control_is_updated_after_disposal : with_clean_cadencemanager
     {
         protected static LedIndicator led;
         protected static Exception ex;
 
-        private Because of = () => ex = Catch.Exception(() =>
-        {
-            led = new LedIndicator();
-            led.Dispose();
-            led.CadenceUpdate(true);
-            led.CadenceUpdate(false);
-        }
-                                            );
+        Because of = () => ex = Catch.Exception(() =>
+            {
+                led = new LedIndicator();
+                led.Dispose();
+                led.CadenceUpdate(true);
+                led.CadenceUpdate(false);
+            }
+                                    );
 
-        private It should_throw = () => ex.ShouldBeOfType<ObjectDisposedException>();
+        It should_throw = () => ex.ShouldBeOfType<ObjectDisposedException>();
     }
 }
