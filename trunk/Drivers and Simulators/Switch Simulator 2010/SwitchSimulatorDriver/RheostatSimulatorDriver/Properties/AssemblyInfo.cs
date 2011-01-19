e682ceb7a4fb14283a117d31c6f1be3e89ef12f8
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-01-19 22:35:30 +0000

    added Rheostat project

---
+++ trunk/Drivers and Simulators/Switch Simulator 2010/SwitchSimulatorDriver/RheostatSimulatorDriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+// TODO - Add your authorship information here
+
+[assembly: AssemblyTitle("ASCOM.Simulator.Rheostat")]
+[assembly: AssemblyDescription("ASCOM Rheostat Simulator Driver")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © 2010 The ASCOM Initiative")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("DBDDE9E6-34FD-4DDD-99C7-A1AF9C63A4F8")]
+
+// AssemblyFileVersion is set globally (do not add an AssemblyFileVersion here).
+
+[assembly: AssemblyVersion("6.0.0.0")]
