30ed525b0b4444fb2d061d12b1449e527aa560d7
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-01-20 02:00:46 +0000

    removed the meter library for the moment

--- trunk/Drivers and Simulators/Switch Simulator 2010/RheostatSimulatorDriver/SetupDialogForm.Designer.cs
+++ trunk/Drivers and Simulators/Switch Simulator 2010/RheostatSimulatorDriver/SetupDialogForm.Designer.cs
@@ -29,63 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.vuMeter1 = new VU_MeterLibrary.VuMeter();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // vuMeter1
-            // 
-            this.vuMeter1.AnalogMeter = true;
-            this.vuMeter1.BackColor = System.Drawing.Color.Black;
-            this.vuMeter1.DialBackground = System.Drawing.Color.White;
-            this.vuMeter1.DialTextNegative = System.Drawing.Color.Red;
-            this.vuMeter1.DialTextPositive = System.Drawing.Color.Black;
-            this.vuMeter1.DialTextZero = System.Drawing.Color.DarkGreen;
-            this.vuMeter1.Led1ColorOff = System.Drawing.Color.DarkGreen;
-            this.vuMeter1.Led1ColorOn = System.Drawing.Color.LimeGreen;
-            this.vuMeter1.Led1Count = 6;
-            this.vuMeter1.Led2ColorOff = System.Drawing.Color.Olive;
-            this.vuMeter1.Led2ColorOn = System.Drawing.Color.Yellow;
-            this.vuMeter1.Led2Count = 6;
-            this.vuMeter1.Led3ColorOff = System.Drawing.Color.Maroon;
-            this.vuMeter1.Led3ColorOn = System.Drawing.Color.Red;
-            this.vuMeter1.Led3Count = 4;
-            this.vuMeter1.LedSize = new System.Drawing.Size(1, 10);
-            this.vuMeter1.LedSpace = 3;
-            this.vuMeter1.Level = 0;
-            this.vuMeter1.LevelMax = 65535;
-            this.vuMeter1.Location = new System.Drawing.Point(50, 84);
-            this.vuMeter1.MeterScale = VU_MeterLibrary.MeterScale.Log10;
-            this.vuMeter1.Name = "vuMeter1";
-            this.vuMeter1.NeedleColor = System.Drawing.Color.Black;
-            this.vuMeter1.PeakHold = true;
-            this.vuMeter1.Peakms = 1000;
-            this.vuMeter1.PeakNeedleColor = System.Drawing.Color.Black;
-            this.vuMeter1.ShowDialOnly = false;
-            this.vuMeter1.ShowLedPeak = false;
-            this.vuMeter1.ShowTextInDial = false;
-            this.vuMeter1.Size = new System.Drawing.Size(147, 117);
-            this.vuMeter1.TabIndex = 0;
-            this.vuMeter1.TextInDial = new string[] {
-        "0",
-        "20",
-        "40",
-        "60",
-        "80",
-        "100"};
-            this.vuMeter1.UseLedLight = true;
-            this.vuMeter1.VerticalBar = false;
-            this.vuMeter1.VuText = "Heater 1";
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(73, 170);
-            this.trackBar1.Maximum = 100;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(104, 45);
-            this.trackBar1.TabIndex = 1;
             // 
             // timer1
             // 
@@ -99,20 +44,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(317, 272);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.vuMeter1);
             this.Name = "SetupDialogForm";
             this.Text = "SetupDialogForm";
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private VU_MeterLibrary.VuMeter vuMeter1;
-        private System.Windows.Forms.TrackBar trackBar1;
+
         private System.Windows.Forms.Timer timer1;
     }
 }
