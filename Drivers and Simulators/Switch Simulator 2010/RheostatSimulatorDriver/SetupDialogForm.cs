332e4d11369cc3629a7434d353d5244bb6115330
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-01-20 01:35:48 +0000

    adding Rheostat project

---
+++ Drivers and Simulators/Switch Simulator 2010/RheostatSimulatorDriver/SetupDialogForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using ASCOM.DeviceInterface;
+
+namespace ASCOM.Simulator
+{
+    public partial class SetupDialogForm : Form
+    {
+        public SetupDialogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1Tick(object sender, System.EventArgs e)
+        {
+            vuMeter1.Level = trackBar1.Value;
+        }
+    }
+}
