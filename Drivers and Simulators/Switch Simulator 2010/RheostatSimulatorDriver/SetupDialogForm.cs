f88a0c9ca3bc758d0e76a3270c62ab04cd4f165a
RobMorgan <RobMorgan@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-02-10 00:27:06 +0000

    ability for the user to set the upper and lower limits of the switch

--- Drivers and Simulators/Switch Simulator 2010/RheostatSimulatorDriver/SetupDialogForm.cs
+++ Drivers and Simulators/Switch Simulator 2010/RheostatSimulatorDriver/SetupDialogForm.cs
@@ -32,12 +32,11 @@ namespace ASCOM.Simulator
             aGauge1.Value = trackBar1.Value;
             label1.Text = _s1.Name;
 
-            aGauge2.MinValue = Convert.ToSingle(_s2.State[0]);
-            aGauge2.MaxValue = Convert.ToSingle(_s2.State[1]);
-            trackBar2.Value = Convert.ToInt16(_s2.State[2]);
-            aGauge2.Value = trackBar2.Value;
-            label2.Text = _s2.Name;
-            
+            SetupGuage1();
+            SetupGuage2();
+
+            label6.Text = null;
+            label11.Text = null;
             label3.Text = Switches.Name + @" v" + Switches.DriverVersion;
         }
 
@@ -58,6 +57,7 @@ namespace ASCOM.Simulator
 
         private void Guage1MouseUp(object sender, MouseEventArgs e)
         {
+            label11.Text = null;
             Switches.SetSwitch(_s1.Name, _s1.State);
             _s1 = (IRheostat)Switches.GetSwitch(_s1.Name);
             aGauge1.Value = Convert.ToSingle(_s1.State[2]);
@@ -65,16 +65,69 @@ namespace ASCOM.Simulator
 
         private void Guage2MouseUp(object sender, MouseEventArgs e)
         {
+            label6.Text = null;
             Switches.SetSwitch(_s2.Name, _s2.State);
             _s2 = (IRheostat)Switches.GetSwitch(_s2.Name);
-            aGauge2.Value = Convert.ToSingle(_s2.State[2]);
+            SetupGuage2();
+        }
+
+        private void SetupGuage2()
+        {
+
+            label2.Text = _s2.Name;
+            var guage2Min = Convert.ToSingle(_s2.State[0]);
+            var guage2Max = Convert.ToSingle(_s2.State[1]);
+            var guage2Value = Convert.ToInt16(_s2.State[2]);
+
+            aGauge2.MinValue = guage2Min;
+            aGauge2.MaxValue = guage2Max;
+            aGauge2.RangeStartValue = Convert.ToSingle(guage2Max - (guage2Max * .10));
+            aGauge2.Value = guage2Value;
+            aGauge2.RangeEndValue = guage2Max;
+
+            trackBar2.Minimum = Convert.ToInt16(guage2Min);
+            trackBar2.Maximum = Convert.ToInt16(guage2Max);
+            trackBar2.Value = guage2Value;
+
+            comboBox1.Text = guage2Min.ToString();
+            comboBox2.Text = guage2Max.ToString();
+
+            aGauge2.RangeStartValue = Convert.ToSingle(guage2Max - (guage2Max * .10));
+            aGauge2.RangeEndValue = guage2Max;
+
+        }
+
+        private void SetupGuage1()
+        {
+
+            label1.Text = _s1.Name;
+            var guage1Min = Convert.ToSingle(_s1.State[0]);
+            var guage1Max = Convert.ToSingle(_s1.State[1]);
+            var guage1Value = Convert.ToInt16(_s1.State[2]);
+
+            aGauge1.MinValue = guage1Min;
+            aGauge1.MaxValue = guage1Max;
+            aGauge1.RangeStartValue = Convert.ToSingle(guage1Max - (guage1Max * .10));
+            aGauge1.Value = guage1Value;
+            aGauge1.RangeEndValue = guage1Max;
+
+            trackBar1.Minimum = Convert.ToInt16(guage1Min);
+            trackBar1.Maximum = Convert.ToInt16(guage1Max);
+            trackBar1.Value = guage1Value;
+
+            comboBox3.Text = guage1Min.ToString();
+            comboBox4.Text = guage1Max.ToString();
+
+            aGauge1.RangeStartValue = Convert.ToSingle(guage1Max - (guage1Max * .10));
+            aGauge1.RangeEndValue = guage1Max;
+
         }
   
-        private static void BrowseToAscom(object sender, MouseEventArgs e)
+        private void BrowseToAscom(object sender, MouseEventArgs e)
         {
                         try
             {
-                Process.Start("http://ascom-standards.org/");
+                Process.Start(@"http://ascom-standards.org/");
             }
             catch (Win32Exception noBrowser)
             {
@@ -87,6 +140,44 @@ namespace ASCOM.Simulator
             }
         }
 
-    #endregion
+        private void Button2Click(object sender, EventArgs e)
+        {
+            _s2.State[0] = comboBox1.Text;
+            _s2.State[1] = comboBox2.Text;
+            _s2.State[2] = comboBox1.Text;
+
+            if (Convert.ToInt16(comboBox1.Text) < Convert.ToInt16(comboBox2.Text))
+            {
+                Switches.SetSwitch(_s2.Name, _s2.State);
+                _s2 = (IRheostat) Switches.GetSwitch(_s2.Name);
+                SetupGuage2();
+                label6.Text = @"Switch settings saved";
+            }
+            else
+            {
+                label6.Text = @"Min must be less than Max";
+            }
+        }
+
+        private void Button3Click(object sender, EventArgs e)
+        {
+            _s1.State[0] = comboBox3.Text;
+            _s1.State[1] = comboBox4.Text;
+            _s1.State[2] = comboBox3.Text;
+
+            if (Convert.ToInt16(comboBox3.Text) < Convert.ToInt16(comboBox4.Text))
+            {
+                Switches.SetSwitch(_s1.Name, _s1.State);
+                _s1 = (IRheostat)Switches.GetSwitch(_s1.Name);
+                SetupGuage1();
+                label11.Text = @"Switch settings saved";
+            }
+            else
+            {
+                label11.Text = @"Min must be less than Max";
+            }
+        }
+
+        #endregion
     }
 }
