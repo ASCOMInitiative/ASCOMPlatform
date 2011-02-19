a2ffe4d8f88f2ad453eaf7b4d244267d1deb4e7e
PeterSimpson <PeterSimpson@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-02-19 23:59:47 +0000

    Help - Examples - correct code that indicated Util.DMS should be used to covert an RA expressed in hours.

--- trunk/ASCOM Platform Examples/Examples/TransformExamples.vb
+++ trunk/ASCOM Platform Examples/Examples/TransformExamples.vb
@@ -14,11 +14,11 @@
 
         'Set Arcturus J2000 co-ordinates and read off the corresponding Topocentric co-ordinates 
         T.SetJ2000(U.HMSToHours("14:15:38.951"), U.DMSToDegrees("19:10:38.06"))
-        MsgBox("RA Topo: " & U.DegreesToHMS(T.RATopocentric, ":", ":", "", 3) & " DEC Topo: " & U.DegreesToDMS(T.DECTopocentric, ":", ":", "", 3))
+        MsgBox("RA Topo: " & U.HoursToHMS(T.RATopocentric, ":", ":", "", 3) & " DEC Topo: " & U.DegreesToDMS(T.DECTopocentric, ":", ":", "", 3))
 
         'Set arbitary topocentric co-ordinates and read off the corresponding J2000 co-ordinates (site parameters remain the same)
         T.SetTopocentric(11.0, 25.0)
-        MsgBox("RA J2000: " & U.DegreesToHMS(T.RAJ2000, ":", ":", "", 3) & " DEC J2000: " & U.DegreesToDMS(T.DECJ2000, ":", ":", "", 3))
+        MsgBox("RA J2000: " & U.HoursToHMS(T.RAJ2000, ":", ":", "", 3) & " DEC J2000: " & U.DegreesToDMS(T.DecJ2000, ":", ":", "", 3))
 
         'Clean up components
         T.Dispose()
