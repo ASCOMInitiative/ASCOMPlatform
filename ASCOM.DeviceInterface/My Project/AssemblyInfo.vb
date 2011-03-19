5956df8163ff31caa79f911f457d3a89b35b3ade
PeterSimpson <PeterSimpson@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-03-19 17:34:09 +0000

    Focus Simulator - Enable handbox form to appear in the .NET version and fix behaviour. Add a goto location text field and action button. DeviceInterface and SafetyMonitor - register for COM attributes from DeviceInterface and safetymonitor projects to remove local build issues.

--- ASCOM.DeviceInterface/My Project/AssemblyInfo.vb
+++ ASCOM.DeviceInterface/My Project/AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("Copyright © ASCOM 2010")> 
 <Assembly: AssemblyTrademark("")> 
 
-<Assembly: ComVisible(True)> 
+<Assembly: ComVisible(False)> 
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("5849AF17-3590-42ff-900E-7544D45C9825")> 
