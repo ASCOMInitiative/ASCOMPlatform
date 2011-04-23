86b58ed3af058ca97b1c26887c4071bf0101e0b5
PeterSimpson <PeterSimpson@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-04-23 10:56:46 +0000

    Installer - Removes Old VS Templates before VSI installer runs (Jira ASCOM-153).Installer will now automatically overwrite older files rather than prompting.

--- trunk/Releases/ASCOM 6/EraseTemplates/EraseTemplates/Properties/AssemblyInfo.cs
+++ trunk/Releases/ASCOM 6/EraseTemplates/EraseTemplates/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UninstallApp")]
-[assembly: AssemblyDescription("Uninstalls previous Platforms 4, 5 and 5.5")]
+[assembly: AssemblyTitle("EraseTemplates")]
+[assembly: AssemblyDescription("Erases old Visual Studio templates installed by the VSI Installer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ASCOM Initiative")]
-[assembly: AssemblyProduct("UninstallApp")]
+[assembly: AssemblyProduct("EraseTemplates")]
 [assembly: AssemblyCopyright("Copyright © ASCOM 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b51f66fe-1a27-4a79-8549-8791dcc1986e")]
+[assembly: Guid("e91d0ec6-5dbb-471a-9119-4ae7102c34f2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("6.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("6.0.0.0")]
