8feb81a4735e464d5146f74f2204d92aba6ebf70
ChrisRowland <ChrisRowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-05-15 11:41:15 +0000

    ASCOM-222 fixed by: Removing "N0" from ToString, this seems to stop the thousands separator from being displayed. changing Convert.ToInt32 to int.Parse(string, NumberFormat.Number... because this will parse a string with a thousands separator.

--- Drivers and Simulators/CameraSimulator.NET/CameraSimulator/SetupDialogForm.cs
+++ Drivers and Simulators/CameraSimulator.NET/CameraSimulator/SetupDialogForm.cs
@@ -11,7 +11,7 @@ namespace ASCOM.Simulator
 	[ComVisible(false)]					// Form not registered for COM!
 	public partial class SetupDialogForm : Form
 	{
-        private const string STR_N0 = "N0";
+        //private const string STR_N0 = "N0";
         private const string STR_N2 = "N2";
         private Camera camera;
 
@@ -56,22 +56,22 @@ namespace ASCOM.Simulator
         internal void InitProperties(Camera theCamera)
         {
             this.checkBoxInterfaceVersion.Checked = (theCamera.interfaceVersion == 2);
-            this.textBoxPixelSizeX.Text = theCamera.pixelSizeX.ToString(STR_N2, CultureInfo.InvariantCulture);
-            this.textBoxPixelSizeY.Text = theCamera.pixelSizeY.ToString(STR_N2, CultureInfo.InvariantCulture);
+            this.textBoxPixelSizeX.Text = theCamera.pixelSizeX.ToString(STR_N2, CultureInfo.CurrentCulture);
+            this.textBoxPixelSizeY.Text = theCamera.pixelSizeY.ToString(STR_N2, CultureInfo.CurrentCulture);
             //this.textBoxFullWellCapacity.Text = camera.fullWellCapacity.ToString();
-            this.textBoxMaxADU.Text = theCamera.maxADU.ToString(CultureInfo.InvariantCulture);
-            this.textBoxElectronsPerADU.Text = theCamera.electronsPerADU.ToString(STR_N2, CultureInfo.InvariantCulture);
+            this.textBoxMaxADU.Text = theCamera.maxADU.ToString(CultureInfo.CurrentCulture);
+            this.textBoxElectronsPerADU.Text = theCamera.electronsPerADU.ToString(STR_N2, CultureInfo.CurrentCulture);
 
-            this.textBoxCameraXSize.Text = theCamera.cameraXSize.ToString(STR_N0, CultureInfo.InvariantCulture);
-            this.textBoxCameraYSize.Text = theCamera.cameraYSize.ToString(STR_N0, CultureInfo.InvariantCulture);
+            this.textBoxCameraXSize.Text = theCamera.cameraXSize.ToString(CultureInfo.CurrentCulture);
+            this.textBoxCameraYSize.Text = theCamera.cameraYSize.ToString(CultureInfo.CurrentCulture);
             this.checkBoxCanAsymmetricBin.Checked = theCamera.canAsymmetricBin;
-            this.textBoxMaxBinX.Text = theCamera.maxBinX.ToString(CultureInfo.InvariantCulture);
-            this.textBoxMaxBinY.Text = theCamera.maxBinY.ToString(CultureInfo.InvariantCulture);
+            this.textBoxMaxBinX.Text = theCamera.maxBinX.ToString(CultureInfo.CurrentCulture);
+            this.textBoxMaxBinY.Text = theCamera.maxBinY.ToString(CultureInfo.CurrentCulture);
             this.checkBoxHasShutter.Checked = theCamera.hasShutter;
             this.textBoxSensorName.Text = theCamera.sensorName;
             this.comboBoxSensorType.SelectedIndex = (int)theCamera.sensorType;
-            this.textBoxBayerOffsetX.Text = theCamera.bayerOffsetX.ToString(CultureInfo.InvariantCulture);
-            this.textBoxBayerOffsetY.Text = theCamera.bayerOffsetY.ToString(CultureInfo.InvariantCulture);
+            this.textBoxBayerOffsetX.Text = theCamera.bayerOffsetX.ToString(CultureInfo.CurrentCulture);
+            this.textBoxBayerOffsetY.Text = theCamera.bayerOffsetY.ToString(CultureInfo.CurrentCulture);
 
             this.checkBoxHasCooler.Checked = theCamera.hasCooler;
             this.checkBoxCanSetCCDTemperature.Checked = theCamera.canSetCcdTemperature;
@@ -79,8 +79,8 @@ namespace ASCOM.Simulator
 
             this.checkBoxCanAbortExposure.Checked = theCamera.canAbortExposure;
             this.checkBoxCanStopExposure.Checked = theCamera.canStopExposure;
-            this.textBoxMaxExposure.Text = theCamera.exposureMax.ToString(CultureInfo.InvariantCulture);
-            this.textBoxMinExposure.Text = theCamera.exposureMin.ToString(CultureInfo.InvariantCulture);
+            this.textBoxMaxExposure.Text = theCamera.exposureMax.ToString(CultureInfo.CurrentCulture);
+            this.textBoxMinExposure.Text = theCamera.exposureMin.ToString(CultureInfo.CurrentCulture);
 
             if (theCamera.gains != null && theCamera.gains.Count > 0)
             {
@@ -94,8 +94,8 @@ namespace ASCOM.Simulator
             {
                 radioButtonNoGain.Checked = true;
             }
-            this.textBoxGainMin.Text = theCamera.gainMin.ToString(CultureInfo.InvariantCulture);
-            this.textBoxGainMax.Text = theCamera.gainMax.ToString(CultureInfo.InvariantCulture);
+            this.textBoxGainMin.Text = theCamera.gainMin.ToString(CultureInfo.CurrentCulture);
+            this.textBoxGainMax.Text = theCamera.gainMax.ToString(CultureInfo.CurrentCulture);
             this.checkBoxApplyNoise.Checked = theCamera.applyNoise;
 
             this.checkBoxCanPulseGuide.Checked = theCamera.canPulseGuide;
@@ -105,22 +105,22 @@ namespace ASCOM.Simulator
 
         private void SaveProperties()
         {
-            camera.pixelSizeX = Convert.ToDouble(this.textBoxPixelSizeX.Text, CultureInfo.InvariantCulture);
-            camera.pixelSizeY = Convert.ToDouble(this.textBoxPixelSizeY.Text, CultureInfo.InvariantCulture);
+            camera.pixelSizeX = double.Parse(this.textBoxPixelSizeX.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
+            camera.pixelSizeY = double.Parse(this.textBoxPixelSizeY.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
             //camera.fullWellCapacity = Convert.ToDouble(this.textBoxFullWellCapacity.Text, CultureInfo.InvariantCulture);
-            camera.maxADU = Convert.ToInt32(this.textBoxMaxADU.Text, CultureInfo.InvariantCulture);
-            camera.electronsPerADU = Convert.ToDouble(this.textBoxElectronsPerADU.Text, CultureInfo.InvariantCulture);
+            camera.maxADU = int.Parse(this.textBoxMaxADU.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
+            camera.electronsPerADU = double.Parse(this.textBoxElectronsPerADU.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
 
-            camera.cameraXSize = Convert.ToInt32(this.textBoxCameraXSize.Text, CultureInfo.InvariantCulture);
-            camera.cameraYSize = Convert.ToInt32(this.textBoxCameraYSize.Text, CultureInfo.InvariantCulture);
+            camera.cameraXSize = int.Parse(this.textBoxCameraXSize.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
+            camera.cameraYSize = int.Parse(this.textBoxCameraYSize.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
             camera.canAsymmetricBin = this.checkBoxCanAsymmetricBin.Checked;
-            camera.maxBinX = Convert.ToInt16(this.textBoxMaxBinX.Text, CultureInfo.InvariantCulture);
-            camera.maxBinY = Convert.ToInt16(this.textBoxMaxBinY.Text, CultureInfo.InvariantCulture);
+            camera.maxBinX = short.Parse(this.textBoxMaxBinX.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
+            camera.maxBinY = short.Parse(this.textBoxMaxBinY.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
             camera.hasShutter = this.checkBoxHasShutter.Checked;
             camera.sensorName = this.textBoxSensorName.Text;
             camera.sensorType = (SensorType)this.comboBoxSensorType.SelectedIndex;
-            camera.bayerOffsetX = Convert.ToInt16(this.textBoxBayerOffsetX.Text, CultureInfo.InvariantCulture);
-            camera.bayerOffsetY = Convert.ToInt16(this.textBoxBayerOffsetY.Text, CultureInfo.InvariantCulture);
+            camera.bayerOffsetX = short.Parse(this.textBoxBayerOffsetX.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
+            camera.bayerOffsetY = short.Parse(this.textBoxBayerOffsetY.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
 
             camera.hasCooler = this.checkBoxHasCooler.Checked;
             camera.canSetCcdTemperature = this.checkBoxCanSetCCDTemperature.Checked;
@@ -128,8 +128,8 @@ namespace ASCOM.Simulator
 
             camera.canAbortExposure = this.checkBoxCanAbortExposure.Checked;
             camera.canStopExposure = this.checkBoxCanStopExposure.Checked;
-            camera.exposureMin = Convert.ToDouble(this.textBoxMinExposure.Text, CultureInfo.InvariantCulture);
-            camera.exposureMax = Convert.ToDouble(this.textBoxMaxExposure.Text, CultureInfo.InvariantCulture);
+            camera.exposureMin = double.Parse(this.textBoxMinExposure.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
+            camera.exposureMax = double.Parse(this.textBoxMaxExposure.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
             camera.applyNoise = this.checkBoxApplyNoise.Checked;
 
             camera.canPulseGuide = this.checkBoxCanPulseGuide.Checked;
@@ -148,8 +148,8 @@ namespace ASCOM.Simulator
             if (this.radioButtonUseMinAndMax.Checked)
             {
                 camera.gains = null;
-                camera.gainMin = Convert.ToInt16(textBoxGainMin.Text, CultureInfo.InvariantCulture);
-                camera.gainMax = Convert.ToInt16(textBoxGainMax.Text, CultureInfo.InvariantCulture);
+                camera.gainMin = short.Parse(textBoxGainMin.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
+                camera.gainMax = short.Parse(textBoxGainMax.Text, NumberStyles.Number, CultureInfo.CurrentCulture);
             }
             camera.interfaceVersion = (short)(checkBoxInterfaceVersion.Checked ? 2 : 1);
         }
