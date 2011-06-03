173ed095050e539800f878ef8d97835338abd426
TeamCity <TeamCity@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-06-03 01:32:36 +0000

    Labeled automatically by TeamCity

---
+++ tags/Build_6.0.6036.1909/ASCOM.Utilities/ASCOM.Utilities/AllUsersFileSystemProvider.vb
@@ -0,0 +1,140 @@
+﻿'Implements the filestore mechanic to store ASCOM profiles
+
+'This implementation stores files in the All Users profile area that is accessible to everyone and so 
+'creates a "per machine" store rather than a "per user" store.
+
+Imports System.IO
+Imports ASCOM.Utilities.Interfaces
+Imports System.Security.AccessControl
+Imports System.Security.Principal
+
+Friend Class AllUsersFileSystemProvider
+
+    Implements IFileStoreProvider
+    Private Const ASCOM_DIRECTORY As String = "\ASCOM"
+    Private Const PROFILE_DIRECTORY As String = ASCOM_DIRECTORY & "\Profile" 'Root directory within the supplied file system space
+
+    Private BaseFolder, ASCOMFolder As String
+
+#Region "New"
+    Friend Sub New()
+        'Find the location of the All Users profile
+        ASCOMFolder = System.Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) & ASCOM_DIRECTORY
+        BaseFolder = System.Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) & PROFILE_DIRECTORY
+    End Sub
+#End Region
+
+#Region "IFileStore Implementation"
+    Friend Sub SetSecurityACLs(ByVal p_tl As TraceLogger) Implements IFileStoreProvider.SetSecurityACLs
+        Dim dInfo As New DirectoryInfo(ASCOMFolder) 'Apply to the ASCOM folder itself
+        Dim dSecurity As DirectorySecurity
+
+        'PWGS 5.5.2.0 Fix for users security group not being globally usable
+        'Build a temp domainSID using the Null SID passed in as a SDDL string. The constructor will 
+        'accept the traditional notation or the SDDL notation interchangeably.
+        Dim DomainSid As New SecurityIdentifier("S-1-0-0")
+        'Create a security Identifier for the BuiltinUsers Group to be passed to the new accessrule
+        Dim Ident As New SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, DomainSid)
+
+        p_tl.LogMessage("  SetSecurityACLs", "Retrieving access control")
+        dSecurity = dInfo.GetAccessControl
+
+        p_tl.LogMessage("  SetSecurityACLs", "Adding full control access rule")
+        dSecurity.AddAccessRule(New FileSystemAccessRule(Ident, FileSystemRights.FullControl, InheritanceFlags.ContainerInherit Or InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow))
+
+        p_tl.LogMessage("  SetSecurityACLs", "Setting access control")
+        dInfo.SetAccessControl(dSecurity)
+
+        p_tl.LogMessage("  SetSecurityACLs", "Successfully set security ACL!")
+    End Sub
+
+    Friend ReadOnly Property Exists(ByVal p_FileName As String) As Boolean Implements IFileStoreProvider.Exists
+        'Tests whether a file exists and returns a boolean value
+        Get
+            Try
+                If File.Exists(CreatePath(p_FileName)) Then
+                    Return True 'This was successful 
+                Else
+                    Return False 'No exception but no file returned so the file does not exist
+                End If
+            Catch ex As Exception
+                MsgBox("Exists " & ex.ToString)
+                Return False 'Exception so file doesn't exist
+            End Try
+        End Get
+    End Property
+
+    Friend Sub CreateDirectory(ByVal p_SubKeyName As String, ByVal p_TL As TraceLogger) Implements IFileStoreProvider.CreateDirectory
+        'Creates a directory in the supplied path (p_SubKeyName)
+        Try
+            p_TL.LogMessage("  CreateDirectory", "Creating directory for: """ & p_SubKeyName & """")
+            Directory.CreateDirectory(CreatePath(p_SubKeyName))
+            p_TL.LogMessage("  CreateDirectory", "Created directory OK")
+        Catch ex As Exception
+            p_TL.LogMessage("FileSystem.CreateDirectory", "Exception: " & ex.ToString)
+            MsgBox("CreateDirectory Exception: " & ex.ToString)
+        End Try
+    End Sub
+
+    Friend Sub DeleteDirectory(ByVal p_SubKeyName As String) Implements IFileStoreProvider.DeleteDirectory
+        'Deletes a directory specified by p_SubKeyName
+        Directory.Delete(CreatePath(p_SubKeyName), True)
+    End Sub
+
+    Friend Sub EraseFileStore() Implements IFileStoreProvider.EraseFileStore
+        Dim Response As MsgBoxResult
+        Response = MsgBox("Are you sure you wish to erase the Utilities profile store?", MsgBoxStyle.OkCancel Or MsgBoxStyle.Critical, "ASCOM.Utilities")
+        If Response = MsgBoxResult.Ok Then
+            Try : Directory.Delete(BaseFolder, True) : Catch : End Try
+        End If
+    End Sub
+
+    Friend ReadOnly Property GetDirectoryNames(ByVal p_SubKeyName As String) As String() Implements IFileStoreProvider.GetDirectoryNames
+        'Enumerates the sub directories within a directory specified by p_SubKeyName
+        'Returns a string array of directory names
+        Get
+            Dim FullDirs() As String, ct As Integer, RelDirs() As String
+            FullDirs = Directory.GetDirectories(CreatePath(p_SubKeyName))
+            ct = 0
+            For Each FullDir As String In FullDirs
+                RelDirs = Split(FullDir, "\")
+                FullDirs(ct) = RelDirs(RelDirs.Length - 1)
+                ct += 1
+            Next
+            Return FullDirs
+        End Get
+    End Property
+
+    Friend ReadOnly Property FullPath(ByVal p_FileName As String) As String Implements IFileStoreProvider.FullPath
+        Get
+            Return CreatePath(p_FileName)
+        End Get
+    End Property
+
+    Friend ReadOnly Property BasePath() As String Implements IFileStoreProvider.BasePath
+        Get
+            Return BaseFolder
+        End Get
+    End Property
+
+    Friend Sub Rename(ByVal p_CurrentName As String, ByVal p_NewName As String) Implements IFileStoreProvider.Rename
+        File.Delete(CreatePath(p_NewName)) 'Make sure the target file doesn't exist
+        File.Move(CreatePath(p_CurrentName), CreatePath(p_NewName))
+    End Sub
+
+    Friend Sub RenameDirectory(ByVal CurrentName As String, ByVal NewName As String) Implements IFileStoreProvider.RenameDirectory
+        Try : Directory.Delete(CreatePath(NewName), True) : Catch : End Try 'Remove driectory if it already exists
+        Dim DirInfo As New DirectoryInfo(CreatePath(CurrentName))
+        DirInfo.MoveTo(CreatePath(NewName))
+    End Sub
+
+#End Region
+
+#Region "Support Code"
+    Private Function CreatePath(ByVal p_FileName As String) As String
+        If Left(p_FileName, 1) <> "\" Then p_FileName = "\" & p_FileName
+        Return BaseFolder & p_FileName
+    End Function
+#End Region
+
+End Class
---
+++ tags/Build_6.0.6036.1909/Drivers and Simulators/Optec Drivers/OptecTCF Driver/OptecTCF-S ASCOM Driver/Forms/TempCompTest_Form.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace ASCOM.OptecTCF_S
+{
+    /*
+    partial class TempCompTest_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TempCompTest_Form));
+            this.label1 = new System.Windows.Forms.Label();
+            this.duration_NUD = new System.Windows.Forms.NumericUpDown();
+            this.Start_Btn = new System.Windows.Forms.Button();
+            this.Stop_Btn = new System.Windows.Forms.Button();
+            this.Close_Btn = new System.Windows.Forms.Button();
+            this.Clear_Btn = new System.Windows.Forms.Button();
+            this.Output_LB = new System.Windows.Forms.ListBox();
+            this.TestTimer = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.duration_NUD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the duration of the test (Seconds):";
+            // 
+            // duration_NUD
+            // 
+            this.duration_NUD.Location = new System.Drawing.Point(238, 8);
+            this.duration_NUD.Maximum = new decimal(new int[] {
+            60000,
+            0,
+            0,
+            0});
+            this.duration_NUD.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.duration_NUD.Name = "duration_NUD";
+            this.duration_NUD.Size = new System.Drawing.Size(64, 20);
+            this.duration_NUD.TabIndex = 1;
+            this.duration_NUD.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // Start_Btn
+            // 
+            this.Start_Btn.Location = new System.Drawing.Point(15, 27);
+            this.Start_Btn.Name = "Start_Btn";
+            this.Start_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Start_Btn.TabIndex = 2;
+            this.Start_Btn.Text = "Start Test";
+            this.Start_Btn.UseVisualStyleBackColor = true;
+            this.Start_Btn.Click += new System.EventHandler(this.Start_Btn_Click);
+            // 
+            // Stop_Btn
+            // 
+            this.Stop_Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Stop_Btn.Enabled = false;
+            this.Stop_Btn.Location = new System.Drawing.Point(15, 223);
+            this.Stop_Btn.Name = "Stop_Btn";
+            this.Stop_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Stop_Btn.TabIndex = 3;
+            this.Stop_Btn.Text = "Stop Test";
+            this.Stop_Btn.UseVisualStyleBackColor = true;
+            this.Stop_Btn.Click += new System.EventHandler(this.Stop_Btn_Click);
+            // 
+            // Close_Btn
+            // 
+            this.Close_Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Close_Btn.Location = new System.Drawing.Point(227, 223);
+            this.Close_Btn.Name = "Close_Btn";
+            this.Close_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Close_Btn.TabIndex = 4;
+            this.Close_Btn.Text = "Close";
+            this.Close_Btn.UseVisualStyleBackColor = true;
+            this.Close_Btn.Click += new System.EventHandler(this.Close_Btn_Click);
+            // 
+            // Clear_Btn
+            // 
+            this.Clear_Btn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.Clear_Btn.Location = new System.Drawing.Point(121, 223);
+            this.Clear_Btn.MinimumSize = new System.Drawing.Size(0, 20);
+            this.Clear_Btn.Name = "Clear_Btn";
+            this.Clear_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Clear_Btn.TabIndex = 5;
+            this.Clear_Btn.Text = "Clear Data";
+            this.Clear_Btn.UseVisualStyleBackColor = true;
+            this.Clear_Btn.Click += new System.EventHandler(this.clearData);
+            // 
+            // Output_LB
+            // 
+            this.Output_LB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.Output_LB.FormattingEnabled = true;
+            this.Output_LB.Location = new System.Drawing.Point(15, 56);
+            this.Output_LB.Name = "Output_LB";
+            this.Output_LB.Size = new System.Drawing.Size(287, 160);
+            this.Output_LB.TabIndex = 6;
+            // 
+            // TestTimer
+            // 
+            this.TestTimer.Tick += new System.EventHandler(this.TestTimer_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(105, 31);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(91, 16);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
+            // 
+            // TempCompTest_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 258);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Output_LB);
+            this.Controls.Add(this.Clear_Btn);
+            this.Controls.Add(this.Close_Btn);
+            this.Controls.Add(this.Stop_Btn);
+            this.Controls.Add(this.Start_Btn);
+            this.Controls.Add(this.duration_NUD);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(16, 290);
+            this.Name = "TempCompTest_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Temperature Compensation Test";
+            this.Load += new System.EventHandler(this.TempCompTest_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.duration_NUD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown duration_NUD;
+        private System.Windows.Forms.Button Start_Btn;
+        private System.Windows.Forms.Button Stop_Btn;
+        private System.Windows.Forms.Button Close_Btn;
+        private System.Windows.Forms.Button Clear_Btn;
+        private System.Windows.Forms.ListBox Output_LB;
+        private System.Windows.Forms.Timer TestTimer;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+
+     */
+}
