b4244d7587917850d2a682b41c55c4ebff95ceb1
TeamCity <TeamCity@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-06-09 07:43:47 +0000

    Labeled automatically by TeamCity

---
+++ tags/Build_6.0.6039.1923/ASCOM.SettingsProvider/ASCOM.SettingsProviderSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASCOM.SettingsProviderSample
+	{
+	class Program
+		{
+		static void Main(string[] args)
+			{
+			Console.WriteLine("Setting1 value = {0}", Properties.Settings.Default.Setting1);
+			Console.ReadLine();
+			}
+		}
+	}
