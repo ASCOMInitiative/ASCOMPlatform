546ad6467dcb1025fe6d5f1a5b8b6e121743959f
TeamCity <TeamCity@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-06-18 00:19:19 +0000

    Labeled automatically by TeamCity

---
+++ tags/Build_6.0.6051.1943/ASCOM.PowerShell.Cmdlets/ASCOMPowerShellCmdlets/AscomUtil.cs
@@ -0,0 +1,30 @@
+﻿using System.Management.Automation;
+using ASCOM.Utilities;
+
+namespace ASCOM.PowerShell.Cmdlets
+{
+    [Cmdlet(VerbsCommon.New, "Util")]
+    public class AscomUtil : Cmdlet
+    {
+
+        #region private fields
+
+        private Util m_util;
+
+        #endregion private fields
+
+        #region parameters
+
+        #endregion parameters
+
+        #region protected overrides
+
+        protected override void ProcessRecord()
+        {
+            m_util = new Util();
+            WriteObject(m_util);
+        }
+
+        #endregion protected overrides
+    }
+}
