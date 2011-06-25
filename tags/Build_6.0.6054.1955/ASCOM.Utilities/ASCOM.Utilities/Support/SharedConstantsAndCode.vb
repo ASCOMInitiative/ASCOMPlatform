a6d9768945291dc26abc18a86ab37afb8933edc4
TeamCity <TeamCity@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-06-25 14:13:16 +0000

    Labeled automatically by TeamCity

---
+++ tags/Build_6.0.6054.1955/ASCOM.Utilities/ASCOM.Utilities/Support/SharedConstantsAndCode.vb
@@ -0,0 +1,176 @@
+﻿'These items are shared between the Utilities and ASCOM.Astrometry assemblies
+
+Imports System.Reflection
+Imports Microsoft.Win32
+Imports ASCOM.Utilities
+
+Module SharedConstants
+    Friend Const TRACE_TRANSFORM As String = "Trace Transform", TRACE_TRANSFORM_DEFAULT As Boolean = False
+    Friend Const REGISTRY_CONFORM_FOLDER As String = "Software\ASCOM\Utilities"
+End Module
+
+#Region "Registry Utility Code"
+Module RegistryCommonCode
+    Friend Function GetBool(ByVal p_Name As String, ByVal p_DefaultValue As Boolean) As Boolean
+        Dim l_Value As Boolean
+        Dim m_HKCU, m_SettingsKey As RegistryKey
+
+        m_HKCU = Registry.CurrentUser
+        m_HKCU.CreateSubKey(REGISTRY_CONFORM_FOLDER)
+        m_SettingsKey = m_HKCU.OpenSubKey(REGISTRY_CONFORM_FOLDER, True)
+
+        Try
+            If m_SettingsKey.GetValueKind(p_Name) = RegistryValueKind.String Then ' Value does exist
+                l_Value = CBool(m_SettingsKey.GetValue(p_Name))
+            End If
+        Catch ex As System.IO.IOException 'Value doesn't exist so create it
+            SetName(p_Name, p_DefaultValue.ToString)
+            l_Value = p_DefaultValue
+        Catch ex As Exception
+            'LogMsg("GetBool", GlobalVarsAndCode.MessageLevel.msgError, "Unexpected exception: " & ex.ToString)
+            l_Value = p_DefaultValue
+        End Try
+        m_SettingsKey.Flush() 'Clean up registry keys
+        m_SettingsKey.Close()
+        m_SettingsKey = Nothing
+        m_HKCU.Flush()
+        m_HKCU.Close()
+        m_HKCU = Nothing
+
+        Return l_Value
+    End Function
+    Friend Function GetString(ByVal p_Name As String, ByVal p_DefaultValue As String) As String
+        Dim l_Value As String = ""
+        Dim m_HKCU, m_SettingsKey As RegistryKey
+
+        m_HKCU = Registry.CurrentUser
+        m_HKCU.CreateSubKey(REGISTRY_CONFORM_FOLDER)
+        m_SettingsKey = m_HKCU.OpenSubKey(REGISTRY_CONFORM_FOLDER, True)
+
+        Try
+            If m_SettingsKey.GetValueKind(p_Name) = RegistryValueKind.String Then ' Value does exist
+                l_Value = m_SettingsKey.GetValue(p_Name).ToString
+            End If
+        Catch ex As System.IO.IOException 'Value doesn't exist so create it
+            SetName(p_Name, p_DefaultValue.ToString)
+            l_Value = p_DefaultValue
+        Catch ex As Exception
+            'LogMsg("GetString", GlobalVarsAndCode.MessageLevel.msgError, "Unexpected exception: " & ex.ToString)
+            l_Value = p_DefaultValue
+        End Try
+        m_SettingsKey.Flush() 'Clean up registry keys
+        m_SettingsKey.Close()
+        m_SettingsKey = Nothing
+        m_HKCU.Flush()
+        m_HKCU.Close()
+        m_HKCU = Nothing
+
+        Return l_Value
+    End Function
+    Friend Function GetDouble(ByVal p_Key As RegistryKey, ByVal p_Name As String, ByVal p_DefaultValue As Double) As Double
+        Dim l_Value As Double
+        Dim m_HKCU, m_SettingsKey As RegistryKey
+
+        m_HKCU = Registry.CurrentUser
+        m_HKCU.CreateSubKey(REGISTRY_CONFORM_FOLDER)
+        m_SettingsKey = m_HKCU.OpenSubKey(REGISTRY_CONFORM_FOLDER, True)
+
+        'LogMsg("GetDouble", GlobalVarsAndCode.MessageLevel.msgDebug, p_Name.ToString & " " & p_DefaultValue.ToString)
+        Try
+            If p_Key.GetValueKind(p_Name) = RegistryValueKind.String Then ' Value does exist
+                l_Value = CDbl(p_Key.GetValue(p_Name))
+            End If
+        Catch ex As System.IO.IOException 'Value doesn't exist so create it
+            SetName(p_Name, p_DefaultValue.ToString)
+            l_Value = p_DefaultValue
+        Catch ex As Exception
+            'LogMsg("GetDouble", GlobalVarsAndCode.MessageLevel.msgError, "Unexpected exception: " & ex.ToString)
+            l_Value = p_DefaultValue
+        End Try
+        m_SettingsKey.Flush() 'Clean up registry keys
+        m_SettingsKey.Close()
+        m_SettingsKey = Nothing
+        m_HKCU.Flush()
+        m_HKCU.Close()
+        m_HKCU = Nothing
+
+        Return l_Value
+    End Function
+    Friend Function GetDate(ByVal p_Name As String, ByVal p_DefaultValue As Date) As Date
+        Dim l_Value As Date
+        Dim m_HKCU, m_SettingsKey As RegistryKey
+
+        m_HKCU = Registry.CurrentUser
+        m_HKCU.CreateSubKey(REGISTRY_CONFORM_FOLDER)
+        m_SettingsKey = m_HKCU.OpenSubKey(REGISTRY_CONFORM_FOLDER, True)
+
+        Try
+            If m_SettingsKey.GetValueKind(p_Name) = RegistryValueKind.String Then ' Value does exist
+                l_Value = CDate(m_SettingsKey.GetValue(p_Name))
+            End If
+        Catch ex As System.IO.IOException 'Value doesn't exist so create it
+            SetName(p_Name, p_DefaultValue.ToString)
+            l_Value = p_DefaultValue
+        Catch ex As Exception
+            'LogMsg("GetDate", GlobalVarsAndCode.MessageLevel.msgError, "Unexpected exception: " & ex.ToString)
+            l_Value = p_DefaultValue
+        End Try
+        m_SettingsKey.Flush() 'Clean up registry keys
+        m_SettingsKey.Close()
+        m_SettingsKey = Nothing
+        m_HKCU.Flush()
+        m_HKCU.Close()
+        m_HKCU = Nothing
+
+        Return l_Value
+    End Function
+    Friend Sub SetName(ByVal p_Name As String, ByVal p_Value As String)
+        Dim m_HKCU, m_SettingsKey As RegistryKey
+
+        m_HKCU = Registry.CurrentUser
+        m_HKCU.CreateSubKey(REGISTRY_CONFORM_FOLDER)
+        m_SettingsKey = m_HKCU.OpenSubKey(REGISTRY_CONFORM_FOLDER, True)
+
+        m_SettingsKey.SetValue(p_Name, p_Value.ToString, RegistryValueKind.String)
+        m_SettingsKey.Flush() 'Clean up registry keys
+        m_SettingsKey.Close()
+        m_SettingsKey = Nothing
+        m_HKCU.Flush()
+        m_HKCU.Close()
+        m_HKCU = Nothing
+
+    End Sub
+End Module
+#End Region
+
+#Region "Version Code"
+
+Module VersionCode
+    Friend Sub RunningVersions(ByVal TL As TraceLogger)
+        Dim AssemblyNames() As AssemblyName
+        TL.LogMessage("Versions", "Utilities version: " & Assembly.GetExecutingAssembly.GetName.Version.ToString)
+        TL.LogMessage("Versions", "CLR version: " & System.Environment.Version.ToString)
+        AssemblyNames = Assembly.GetExecutingAssembly.GetReferencedAssemblies
+
+        'Get Operating system information
+        Dim OS As System.OperatingSystem = System.Environment.OSVersion
+        TL.LogMessage("Versions", "OS Version " & OS.Platform & " Service Pack: " & OS.ServicePack & " Full: " & OS.VersionString)
+        'Get file system information
+        Dim MachineName As String = System.Environment.MachineName
+        Dim ProcCount As Integer = System.Environment.ProcessorCount
+        Dim SysDir As String = System.Environment.SystemDirectory
+        Dim WorkSet As Long = System.Environment.WorkingSet
+        TL.LogMessage("Versions", "Machine name: " & MachineName & " Number of processors: " & ProcCount & " System directory: " & SysDir & " Working set size: " & WorkSet & " bytes")
+
+        'Get fully qualified paths to particular directories in a non OS specific way
+        'There are many more options in the SpecialFolders Enum than are shown here!
+        TL.LogMessage("Versions", "Application Data: " & System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData))
+        TL.LogMessage("Versions", "Common Files: " & System.Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles))
+        TL.LogMessage("Versions", "My Documents: " & System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments))
+        TL.LogMessage("Versions", "Program Files: " & System.Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles))
+        TL.LogMessage("Versions", "System: " & System.Environment.GetFolderPath(Environment.SpecialFolder.System))
+        TL.LogMessage("Versions", "Current: " & System.Environment.CurrentDirectory)
+    End Sub
+
+End Module
+#End Region
