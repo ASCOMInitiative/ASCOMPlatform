e3977c0c8cebdc4271d85ddab09d0baa2f5f960f
ChrisRowland <ChrisRowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-08-12 16:45:57 +0000

    ASCOM-272 Change the ReadMe.htm file to be more suited to operation on 64 bit systems, using a test application instead of a script file. Add the settings provider code. NEEDS REVIEW!

--- DriverTemplates/ASCOM 6 Templates/src/ASCOM Camera Driver Template CS/Test.js
+++ DriverTemplates/ASCOM 6 Templates/src/ASCOM Camera Driver Template CS/Test.js
@@ -1,6 +1,6 @@
 var H = new ActiveXObject("ASCOM.Utilities.Chooser");
 WScript.Echo("Choose your new driver in the Chooser that will appear. Then click Properties to activate its SetupDialog");
 H.DeviceType = "Camera";							// Make chooser for Camera
-var F = new ActiveXObject(H.Choose("")); 		// Create instance of selected Driver
+var F = new ActiveXObject(H.Choose("ASCOM.$safeprojectname$.Camera")); 		// Create instance of selected Driver
 F = null;
 H = null;
