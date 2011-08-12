78da4b0a48893c419d6b0d5fb3c0cf48075999c6
ChrisRowland <ChrisRowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-08-12 16:49:26 +0000

    add settings.cs

---
+++ DriverTemplates/ASCOM 6 Templates/src/ASCOM Camera Driver Template CS/Settings.cs
@@ -0,0 +1,13 @@
+﻿// Settings class for the $safeprojectname$ camera driver
+// there is no need to edit this file
+
+using System.Configuration;
+
+namespace ASCOM.$safeprojectname$
+{
+    [DeviceId("ASCOM.$safeprojectname$.Camera", DeviceName = "$safeprojectname$ Camera")]
+    [SettingsProvider(typeof(ASCOM.SettingsProvider))]
+    internal partial class Settings
+    {
+    }
+}
