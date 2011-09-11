da0d417d20ddb229361ffd0c2a4d82c53030497e
ChrisRowland <ChrisRowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-09-11 09:39:17 +0000

    Driver Test Forms Application Template CSharp added to the template sources

---
+++ trunk/DriverTemplates/ASCOM 6 Templates/src/Driver Test Forms Application Template CSharp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TEMPLATEDEVICENAME
+{
+    public partial class Form1 : Form
+    {
+
+        private TEMPLATEDEVICECLASS driver;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetUIState();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (IsConnected)
+                driver.Connected = false;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void buttonChoose_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DriverId = TEMPLATEDEVICECLASS.Choose(Properties.Settings.Default.DriverId);
+            SetUIState();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (IsConnected)
+            {
+                driver.Connected = false;
+            }
+            else
+            {
+                driver = new TEMPLATEDEVICECLASS(Properties.Settings.Default.DriverId);
+                driver.Connected = true;
+            }
+            SetUIState();
+        }
+
+        private void SetUIState()
+        {
+            buttonConnect.Enabled = !string.IsNullOrEmpty(Properties.Settings.Default.DriverId);
+            buttonChoose.Enabled = !IsConnected;
+            buttonConnect.Text = IsConnected ? "Disconnect" : "Connect";
+        }
+
+        private bool IsConnected
+        {
+            get
+            {
+                return ((this.driver != null) && (driver.Connected == true));
+            }
+        }
+
+    }
+}
