6d18f6420de67599b99c6f3029607b700756dd7b
ChrisRowland <ChrisRowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-09-11 08:44:05 +0000

    Make template compatible with the ASCOM setup wizard.

--- DriverTemplates/ASCOM 6 Templates/src/ASCOM Test Application Template CS/Program.cs
+++ DriverTemplates/ASCOM 6 Templates/src/ASCOM Test Application Template CS/Program.cs
@@ -1,64 +1,42 @@
-﻿using System;
+﻿// This implements a console application that can be used to test an ASCOM driver
+//
+
+// This is used to define code in the template that is specific to one class implementation
+// unused code can be deleted and this definition removed.
+#define TEMPLATEDEVICECLASS
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace $safeprojectname$
+namespace TEMPLATEDEVICENAME
 {
     class Program
     {
         static void Main(string[] args)
         {
             // Uncomment the code that's required
+            // choose the device
+            string id = ASCOM.DriverAccess.Dome.Choose("");
+            if (string.IsNullOrEmpty(id))
+                return;
+            // create this device
+            ASCOM.DriverAccess.Dome device = new ASCOM.DriverAccess.Dome(id);
 
-            //string id = ASCOM.DriverAccess.Camera.Choose("");
-            //ASCOM.DriverAccess.Camera camera = new ASCOM.DriverAccess.Camera(id);
-            //Console.WriteLine("name " + camera.Name);
-            //Console.WriteLine("description " + camera.Description);
-            //Console.WriteLine("DriverInfo " + camera.DriverInfo);
-            //Console.WriteLine("driverVersion " + camera.DriverVersion);
+            // this can be replaced by this code, it avoids the chooser and creates the driver class directly.
+            //ASCOM.DriverAccess.Telescope device = new ASCOM.DriverAccess.Telescope("ASCOM.TEMPLATEDEVICENAME.TEMPLATEDEVICECLASS");
 
-            //string id = ASCOM.DriverAccess.Dome.Choose("");
-            //ASCOM.DriverAccess.Dome dome = new ASCOM.DriverAccess.Dome(id);
-            //Console.WriteLine("name " + dome.Name);
-            //Console.WriteLine("description " + dome.Description);
-            //Console.WriteLine("DriverInfo " + dome.DriverInfo);
-            //Console.WriteLine("driverVersion " + dome.DriverVersion);
 
-            //string id = ASCOM.DriverAccess.FilterWheel.Choose("");
-            //ASCOM.DriverAccess.FilterWheel filterWheel = new ASCOM.DriverAccess.FilterWheel(id);
-            //Console.WriteLine("name " + filterWheel.Name);
-            //Console.WriteLine("description " + filterWheel.Description);
-            //Console.WriteLine("DriverInfo " + filterWheel.DriverInfo);
-            //Console.WriteLine("driverVersion " + filterWheel.DriverVersion);
+            // now run some tests, adding code to your driver so that the tests will pass.
+            // these first tests are common to all drivers.
+            Console.WriteLine("name " + device.Name);
+            Console.WriteLine("description " + device.Description);
+            Console.WriteLine("DriverInfo " + device.DriverInfo);
+            Console.WriteLine("driverVersion " + device.DriverVersion);
 
-            //string id = ASCOM.DriverAccess.Focuser.Choose("");
-            //ASCOM.DriverAccess.Focuser focuser = new ASCOM.DriverAccess.Focuser(id);
-            //Console.WriteLine("name " + focuser.Name);
-            //Console.WriteLine("description " + focuser.Description);
-            //Console.WriteLine("DriverInfo " + focuser.DriverInfo);
-            //Console.WriteLine("driverVersion " + focuser.DriverVersion);
+            // TODO add more code to test the driver.
 
-            //string id = ASCOM.DriverAccess.Rotator.Choose("");
-            //ASCOM.DriverAccess.Rotator rotator = new ASCOM.DriverAccess.Rotator(id);
-            //Console.WriteLine("name " + rotator.Name);
-            //Console.WriteLine("description " + rotator.Description);
-            //Console.WriteLine("DriverInfo " + rotator.DriverInfo);
-            //Console.WriteLine("driverVersion " + rotator.DriverVersion);
-
-            //string id = ASCOM.DriverAccess.SafetyMonitor.Choose("");
-            //ASCOM.DriverAccess.SafetyMonitor safetyMonitor = new ASCOM.DriverAccess.SafetyMonitor(id);
-            //Console.WriteLine("name " + safetyMonitor.Name);
-            //Console.WriteLine("description " + safetyMonitor.Description);
-            //Console.WriteLine("DriverInfo " + safetyMonitor.DriverInfo);
-            //Console.WriteLine("driverVersion " + safetyMonitor.DriverVersion);
-
-            //string id = ASCOM.DriverAccess.Telescope.Choose("");
-            //ASCOM.DriverAccess.Telescope telescope = new ASCOM.DriverAccess.Telescope(id);
-            //Console.WriteLine("name " + telescope.Name);
-            //Console.WriteLine("description " + telescope.Description);
-            //Console.WriteLine("DriverInfo " + telescope.DriverInfo);
-            //Console.WriteLine("driverVersion " + telescope.DriverVersion);
 
             Console.WriteLine("Press Enter to finish");
             Console.ReadLine();
