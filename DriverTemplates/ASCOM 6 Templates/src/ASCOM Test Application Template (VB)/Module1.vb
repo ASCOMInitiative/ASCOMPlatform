860dc1e1737730b91ac6862a63742316be637454
ChrisRowland <ChrisRowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-09-11 10:16:41 +0000

    ASCOM Test Application Template (VB) added to template sources.

---
+++ DriverTemplates/ASCOM 6 Templates/src/ASCOM Test Application Template (VB)/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim id As String = TEMPLATEDEVICECLASS.Choose("")
+
+        Dim driver As TEMPLATEDEVICECLASS = New TEMPLATEDEVICECLASS(id)
+
+        Console.WriteLine("Name " & driver.Name)
+        Console.WriteLine("description " + driver.Description)
+        Console.WriteLine("DriverInfo " + driver.DriverInfo)
+        Console.WriteLine("driverVersion " + driver.DriverVersion)
+
+        ' TODO add more code to test the driver.
+
+
+        Console.WriteLine("Press Enter to finish")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
