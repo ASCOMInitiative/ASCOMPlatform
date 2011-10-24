4e78f51ecf75bde2da5eae09e432a3aa15c54975
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-10-24 08:59:11 +0000

    ASCOM-304 cleaned up the DriverTemplates folder hierarchy. Removed all the pre-platform-6 'fluff' and renamed 'ASCOM 6 Templates' -> 'TemplateSources' (removed artificial/redundant versioning). Verified that this does not affect the build or the InstallAware package (the 'ASCOM 6 Driver Templates.vsi' file is manually committed and has not been moved).

--- DriverTemplates/Driver Template CSharp/PlaceholderInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ASCOM.TEMPLATEDEVICENAME
-{
-
-	interface ITEMPLATEDEVICEINTERFACE
-	{
-		// Dummy interface just to stop compile errors during development.
-		// This file is not needed and is deleted by the setup wizard.
-	}
-}
