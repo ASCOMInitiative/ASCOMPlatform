5122294375b0f818903cfe4dea7a7ad0fbd97fd3
TimLong <TimLong@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2011-11-04 12:56:28 +0000

    Creating a branch to experiment with web-based documentation

---
+++ branches/TPL-WebDocExperiment/Driver Helper/VB.Net Test/VB.Net Test/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VB.Net Test")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("VB.Net Test")> 
+<Assembly: AssemblyCopyright("Copyright ©  2007")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f21984e0-ee4f-41a1-8582-057ae4e36d96")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ branches/TPL-WebDocExperiment/NOVAS/Usno.Novas.TestConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Usno;
+
+namespace Usno.NovasTestConsoleApp
+	{
+	class Program
+		{
+		static void Main(string[] args)
+			{
+			double tjd = 2454970.5563889;
+			DateTime expected = new DateTime(2009, 5, 19, 01, 21, 12, 10, DateTimeKind.Utc);
+			DateTime actual;
+			actual = Novas.cal_date(tjd);
+			Console.WriteLine("Julian date {0} is equivalent to Gregorian date {1}#{2} UT", tjd, actual, actual.Millisecond);
+			Console.ReadLine();
+			}
+		}
+	}
