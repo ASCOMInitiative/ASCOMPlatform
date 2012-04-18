7c36f26f5b83d1c2368fdfc46377f9d1f121b780
chris-rowland <chris-rowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2012-04-18 07:20:21 +0000

    inadvertent commit, trying to revert the repository

--- trunk/Drivers and Simulators/SimulatorTestingProject/Simulator Testing Project/Program.cs
+++ trunk/Drivers and Simulators/SimulatorTestingProject/Simulator Testing Project/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            object[] parameter = new object[1]; 
+
+            ASCOM.DriverAccess.Telescope scope = new ASCOM.DriverAccess.Telescope("ASCOM.Simulator.Telescope");
+            scope.Connected = true;
+            System.Console.WriteLine("Position " + scope.RightAscension.ToString() + ", " + scope.Declination.ToString());
+
+            System.Console.ReadLine();
+            //object[] parameter = new object[1]; 
+
+            object s = System.Activator.GetObject(System.Type.GetTypeFromProgID("ASCOM.Simulator.Telescope"),string.Empty);
 
             //Type a = Type.GetTypeFromProgID("ASCOM.Simulator.RheostatSwitch");
             ////Create instance of excel 
@@ -14,8 +22,8 @@
             ////Set the Visible property 
             //a.InvokeMember("Visible", BindingFlags.SetProperty, null, b, parameter);
 
-            var chooser = new ASCOM.Utilities.Chooser { DeviceType = "Switch" };
-            chooser.Choose();
+            //var chooser = new ASCOM.Utilities.Chooser { DeviceType = "Switch" };
+            //chooser.Choose();
         }
     }
 }
