9b37f5a57c86740ab7ee5816f80404bd2e0e5714
simpsonpwg <simpsonpwg@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2013-09-14 06:42:52 +0000

    Install - Fix issues a few people have with non-canonical ACLs on their ASCOM Profile key. Remove some redundant comment text in GlobalCode.vb

--- trunk/ASCOM.Utilities/ASCOM.Utilities/GlobalCode.vb
+++ trunk/ASCOM.Utilities/ASCOM.Utilities/GlobalCode.vb
@@ -1262,18 +1262,9 @@ Friend Class PEReader
                 stream = Nothing
             End If
 
-            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
-            ' TODO: set large fields to null.
         End If
         Me.disposedValue = True
     End Sub
-
-    ' TODO: override Finalize() only if Dispose(ByVal disposing As Boolean) above has code to free unmanaged resources.
-    'Protected Overrides Sub Finalize()
-    '    ' Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
-    '    Dispose(False)
-    '    MyBase.Finalize()
-    'End Sub
 
     ' This code added by Visual Basic to correctly implement the disposable pattern.
     Public Sub Dispose() Implements IDisposable.Dispose
