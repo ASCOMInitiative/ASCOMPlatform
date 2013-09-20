4e9adc0f774ca392330ddec8e5226c584757f4b4
chris-rowland <chris-rowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2013-09-20 09:35:08 +0000

    Add preliminary ISwitchV2 interface and ISwitch driver access components. Add preliminary SwitchDrivers to the driver templates.

---
+++ trunk/ASCOM.DeviceInterface/ISwitchV2.vb
@@ -0,0 +1,285 @@
+﻿Imports System.Runtime.InteropServices
+'-----------------------------------------------------------------------
+' <summary>Defines the ISwitchV2 Interface</summary>
+'-----------------------------------------------------------------------
+''' <summary>
+''' Defines the ISwitchV2 Interface
+''' </summary>
+<Guid("71A6CA6B-A86B-4EBB-8DA3-6D91705177A3"), ComVisible(True), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)> _
+Public Interface ISwitchV2
+
+#Region "Common Methods"
+    'IAscomDriver Methods
+
+    ''' <summary>
+    ''' Set True to connect to the device. Set False to disconnect from the device.
+    ''' You can also read the property to check whether it is connected.
+    ''' </summary>
+    ''' <value><c>true</c> if connected; otherwise, <c>false</c>.</value>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Must be implemented</b></p>Do not use a NotConnectedException here, that exception is for use in other methods that require a connection in order to succeed.</remarks>
+    Property Connected() As Boolean
+
+    ''' <summary>
+    ''' Returns a description of the device, such as manufacturer and modelnumber. Any ASCII characters may be used. 
+    ''' </summary>
+    ''' <value>The description.</value>
+    ''' <exception cref="NotConnectedException">If the device is not connected and this information is only available when connected.</exception>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Must be implemented</b></p> </remarks>
+    ReadOnly Property Description() As String
+
+    ''' <summary>
+    ''' Descriptive and version information about this ASCOM driver.
+    ''' </summary>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks>
+    ''' <p style="color:red"><b>Must be implemented</b></p> This string may contain line endings and may be hundreds to thousands of characters long.
+    ''' It is intended to display detailed information on the ASCOM driver, including version and copyright data.
+    ''' See the <see cref="Description" /> property for information on the device itself.
+    ''' To get the driver version in a parseable string, use the <see cref="DriverVersion" /> property.
+    ''' </remarks>
+    ReadOnly Property DriverInfo() As String
+
+    ''' <summary>
+    ''' A string containing only the major and minor version of the driver.
+    ''' </summary>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Must be implemented</b></p> This must be in the form "n.n".
+    ''' It should not to be confused with the <see cref="InterfaceVersion" /> property, which is the version of this specification supported by the 
+    ''' driver.
+    ''' </remarks>
+    ReadOnly Property DriverVersion() As String
+
+    ''' <summary>
+    ''' The interface version number that this device supports. Should return 2 for this interface version.
+    ''' </summary>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Must be implemented</b></p> Clients can detect legacy V1 drivers by trying to read ths property.
+    ''' If the driver raises an error, it is a V1 driver. V1 did not specify this property. A driver may also return a value of 1. 
+    ''' In other words, a raised error or a return value of 1 indicates that the driver is a V1 driver.
+    ''' </remarks>
+    ReadOnly Property InterfaceVersion() As Short
+
+    ''' <summary>
+    ''' The short name of the driver, for display purposes
+    ''' </summary>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Must be implemented</b></p> </remarks>
+    ReadOnly Property Name() As String
+
+    ''' <summary>
+    ''' Launches a configuration dialog box for the driver.  The call will not return
+    ''' until the user clicks OK or cancel manually.
+    ''' </summary>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Must be implemented</b></p> </remarks>
+    Sub SetupDialog()
+
+    'DeviceControl Methods
+
+    ''' <summary>
+    ''' Invokes the specified device-specific action.
+    ''' </summary>
+    ''' <param name="ActionName">
+    ''' A well known name agreed by interested parties that represents the action to be carried out. 
+    ''' </param>
+    ''' <param name="ActionParameters">List of required parameters or an <see cref="String.Empty">Empty String</see> if none are required.
+    ''' </param>
+    ''' <returns>A string response. The meaning of returned strings is set by the driver author.</returns>
+    ''' <exception cref="ASCOM.MethodNotImplementedException">Throws this exception if no actions are suported.</exception>
+    ''' <exception cref="ASCOM.ActionNotImplementedException">It is intended that the SupportedActions method will inform clients 
+    ''' of driver capabilities, but the driver must still throw an ASCOM.ActionNotImplemented exception if it is asked to 
+    ''' perform an action that it does not support.</exception>
+    ''' <exception cref="NotConnectedException">If the driver is not connected.</exception>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <example>Suppose filter wheels start to appear with automatic wheel changers; new actions could 
+    ''' be “FilterWheel:QueryWheels” and “FilterWheel:SelectWheel”. The former returning a 
+    ''' formatted list of wheel names and the second taking a wheel name and making the change, returning appropriate 
+    ''' values to indicate success or failure.
+    ''' </example>
+    ''' <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> 
+    ''' This method is intended for use in all current and future device types and to avoid name clashes, management of action names 
+    ''' is important from day 1. A two-part naming convention will be adopted - <b>DeviceType:UniqueActionName</b> where:
+    ''' <list type="bullet">
+    ''' <item><description>DeviceType is the same value as would be used by <see cref="ASCOM.Utilities.Chooser.DeviceType"/> e.g. Telescope, Camera, Switch etc.</description></item>
+    ''' <item><description>UniqueActionName is a single word, or multiple words joined by underscore characters, that sensibly describes the action to be performed.</description></item>
+    ''' </list>
+    ''' <para>
+    ''' It is recommended that UniqueActionNames should be a maximum of 16 characters for legibility.
+    ''' Should the same function and UniqueActionName be supported by more than one type of device, the reserved DeviceType of 
+    ''' “General” will be used. Action names will be case insensitive, so FilterWheel:SelectWheel, filterwheel:selectwheel 
+    ''' and FILTERWHEEL:SELECTWHEEL will all refer to the same action.</para>
+    ''' <para>The names of all supported actions must bre returned in the <see cref="SupportedActions"/> property.</para>
+    ''' </remarks>
+    Function Action(ByVal ActionName As String, ByVal ActionParameters As String) As String
+
+    ''' <summary>
+    ''' Returns the list of action names supported by this driver.
+    ''' </summary>
+    ''' <value>An ArrayList of strings (SafeArray collection) containing the names of supported actions.</value>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Must be implemented</b></p> This method must return an empty arraylist if no actions are supported. Please do not throw a 
+    ''' <see cref="ASCOM.PropertyNotImplementedException" />.
+    ''' <para>This is an aid to client authors and testers who would otherwise have to repeatedly poll the driver to determine its capabilities. 
+    ''' Returned action names may be in mixed case to enhance presentation but  will be recognised case insensitively in 
+    ''' the <see cref="Action">Action</see> method.</para>
+    '''<para>An array list collection has been selected as the vehicle for  action names in order to make it easier for clients to
+    ''' determine whether a particular action is supported. This is easily done through the Contains method. Since the
+    ''' collection is also ennumerable it is easy to use constructs such as For Each ... to operate on members without having to be concerned 
+    ''' about hom many members are in the collection. </para>
+    ''' <para>Collections have been used in the Telescope specification for a number of years and are known to be compatible with COM. Within .NET
+    ''' the ArrayList is the correct implementation to use as the .NET Generic methods are not compatible with COM.</para>
+    ''' </remarks>
+    ReadOnly Property SupportedActions() As ArrayList
+
+    ''' <summary>
+    ''' Transmits an arbitrary string to the device and does not wait for a response.
+    ''' Optionally, protocol framing characters may be added to the string before transmission.
+    ''' </summary>
+    ''' <param name="Command">The literal command string to be transmitted.</param>
+    ''' <param name="Raw">
+    ''' if set to <c>true</c> the string is transmitted 'as-is'.
+    ''' If set to <c>false</c> then protocol framing characters may be added prior to transmission.
+    ''' </param>
+    ''' <exception cref="MethodNotImplementedException">If the method is not implemented</exception>
+    ''' <exception cref="NotConnectedException">If the driver is not connected.</exception>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> </remarks>
+    Sub CommandBlind(ByVal Command As String, Optional ByVal Raw As Boolean = False)
+
+    ''' <summary>
+    ''' Transmits an arbitrary string to the device and waits for a boolean response.
+    ''' Optionally, protocol framing characters may be added to the string before transmission.
+    ''' </summary>
+    ''' <param name="Command">The literal command string to be transmitted.</param>
+    ''' <param name="Raw">
+    ''' if set to <c>true</c> the string is transmitted 'as-is'.
+    ''' If set to <c>false</c> then protocol framing characters may be added prior to transmission.
+    ''' </param>
+    ''' <returns>
+    ''' Returns the interpreted boolean response received from the device.
+    ''' </returns>
+    ''' <exception cref="MethodNotImplementedException">If the method is not implemented</exception>
+    ''' <exception cref="NotConnectedException">If the driver is not connected.</exception>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> </remarks>
+    Function CommandBool(ByVal Command As String, Optional ByVal Raw As Boolean = False) As Boolean
+
+    ''' <summary>
+    ''' Transmits an arbitrary string to the device and waits for a string response.
+    ''' Optionally, protocol framing characters may be added to the string before transmission.
+    ''' </summary>
+    ''' <param name="Command">The literal command string to be transmitted.</param>
+    ''' <param name="Raw">
+    ''' if set to <c>true</c> the string is transmitted 'as-is'.
+    ''' If set to <c>false</c> then protocol framing characters may be added prior to transmission.
+    ''' </param>
+    ''' <returns>
+    ''' Returns the string response received from the device.
+    ''' </returns>
+    ''' <exception cref="MethodNotImplementedException">If the method is not implemented</exception>
+    ''' <exception cref="NotConnectedException">If the driver is not connected.</exception>
+    ''' <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+    ''' <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> </remarks>
+    Function CommandString(ByVal Command As String, Optional ByVal Raw As Boolean = False) As String
+
+    ''' <summary>
+    ''' Dispose the late-bound interface, if needed. Will release it via COM
+    ''' if it is a COM object, else if native .NET will just dereference it
+    ''' for GC.
+    ''' </summary>
+    Sub Dispose()
+
+#End Region
+
+#Region "Device Methods"
+    ''' <summary>
+    ''' The number of switches managed by this driver. Switch numbers are 0 to
+    ''' MaxSwitch - 1
+    ''' </summary>
+    ReadOnly Property MaxSwitch As Short
+
+    ''' <summary>
+    ''' Return the name of switch n. This method is mandatory.
+    ''' </summary>
+    ''' <param name="id">The switch number to return</param>
+    ''' <returns>The name of the switch</returns>
+    Function GetSwitchName(id As Short) As String
+
+    ''' <summary>
+    ''' Sets a switch name to a specified value.  If the switch name cannot
+    ''' be set by the application this must return the MethodNetImplementedException.
+    ''' </summary>
+    ''' <param name="id">The number of the switch whose name is to be set</param>
+    ''' <param name="name">The name of the switch</param>
+    Sub SetSwitchName(id As Short, name As String)
+
+#Region "boolean members"
+    ''' <summary>
+    ''' Return the state of switch n as a boolean
+    ''' an analogue switch will return true if the value is closer to the maximum than the minimum, otherwise false
+    ''' </summary>
+    ''' <param name="id">The switch number to return</param>
+    ''' <returns>True or false</returns>
+    Function GetSwitch(id As Short) As Boolean
+
+    ''' <summary>
+    ''' Sets a switch to the specified state, true or false.
+    ''' If the switch cannot be set then throws a MethodNotImplementedException.
+    ''' Setting an analogue switch to true will set it to its maximim value and
+    ''' setting it to false will set it to its minimum value.
+    ''' </summary>
+    ''' <param name="ID">The number of the switch to set</param>
+    ''' <param name="State">The required switch state</param>
+    Sub SetSwitch(id As Short, state As Boolean)
+#End Region
+
+#Region "Analogue members"
+    ''' <summary>
+    ''' returns the maximum analogue value for this switch
+    ''' boolean switches must return 1.0
+    ''' </summary>
+    ''' <param name="id"></param>
+    ''' <returns></returns>
+    Function MaxSwitchValue(id As Short) As Double
+
+    ''' <summary>
+    ''' returns the minimum analogue value for this switch
+    ''' boolean switches must return 0.0
+    ''' </summary>
+    ''' <param name="id"></param>
+    ''' <returns></returns>
+    Function MinSwitchValue(id As Short) As Double
+
+    ''' <summary>
+    ''' returns the step size that this switch supports. This gives the difference between
+    ''' successive values of the switch.
+    ''' The number of values is ((MaxSwitchValue - MinSwitchValue) / SwitchStep) + 1
+    ''' boolean switches must return 1.0, giving two states.
+    ''' </summary>
+    ''' <param name="id"></param>
+    ''' <returns></returns>
+    Function SwitchStep(id As Short) As Double
+
+    ''' <summary>
+    ''' returns the analogue switch value for switch id
+    ''' boolean switches will return 1.0 or 0.0
+    ''' </summary>
+    ''' <param name="id"></param>
+    ''' <returns></returns>
+    Function GetSwitchValue(id As Short) As Double
+
+    ''' <summary>
+    ''' set the analogue value for this switch.
+    ''' If the switch cannot be set then throws a MethodNotImplementedException.
+    ''' If the value is not between the maximum and minimum then throws an InvalidValueException
+    ''' boolean switches will be set to true if the value is closer to the maximum than the minimum.
+    ''' </summary>
+    ''' <param name="id"></param>
+    ''' <param name="value"></param>
+    Sub SetSwitchValue(id As Short, value As Double)
+
+#End Region
+#End Region
+End Interface
---
+++ trunk/DriverTemplates/TemplateSources/src/ASCOM Driver Template (CS)/DeviceSwitch.cs
@@ -0,0 +1,189 @@
+﻿// All lines from line 1 to the device interface implementation region will be discarded by the project wizard when the template is used
+// Required code must lie within the device implementation region
+// The //ENDOFINSERTEDFILE tag must be the last but one line in this file
+
+using ASCOM;
+using ASCOM.Utilities;
+
+class DeviceSwitch
+{
+    private TraceLogger tl = new TraceLogger();
+
+    #region ISwitchV2 Implementation
+
+    private  short numSwitch;
+
+    /// <summary>
+    /// The number of switches managed by this driver
+    /// </summary>
+    public short MaxSwitch
+    {
+        get 
+        { 
+            tl.LogMessage("MaxSwitch Get", numSwitch.ToString());
+            return this.numSwitch; 
+        }
+    }
+
+    /// <summary>
+    /// Return the name of switch n
+    /// </summary>
+    /// <param name="id">The switch number to return</param>
+    /// <returns>
+    /// The name of the switch
+    /// </returns>
+    public string GetSwitchName(short id)
+    {
+        Validate("GetSwitchName", id);
+        throw new MethodNotImplementedException("GetSwitchName");
+    }
+
+    /// <summary>
+    /// Sets a switch name to a specified value
+    /// </summary>
+    /// <param name="id">The number of the switch whose name is to be set</param>
+    /// <param name="name">The name of the switch</param>
+    public void SetSwitchName(short id, string name)
+    {
+        Validate("SetSwitchName", id);
+        throw new MethodNotImplementedException("SetSwitchName");
+    }
+
+    #region boolean switch members
+
+    /// <summary>
+    /// Return the state of switch n
+    /// an analogue switch will return true if the value is closer to the maximum than the minimum, otherwise false
+    /// </summary>
+    /// <param name="id">The switch number to return</param>
+    /// <returns>
+    /// True or false
+    /// </returns>
+    public bool GetSwitch(short id)
+    {
+        Validate("GetSwitch", id);
+        throw new MethodNotImplementedException("GetSwitch");
+    }
+
+    /// <summary>
+    /// Sets a switch to the specified state
+    /// If the switch cannot be set then throws a MethodNotImplementedException.
+    /// Setting an analogue switch to true will set it to its maximim value and
+    /// setting it to false will set it to its minimum value.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="state"></param>
+    public void SetSwitch(short id, bool state)
+    {
+        Validate("SetSwitch", id);
+        throw new MethodNotImplementedException("SetSwitch");
+    }
+
+    #endregion
+
+    #region analogue members
+
+    /// <summary>
+    /// returns the maximum value for this switch
+    /// boolean switches must return 1.0
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public double MaxSwitchValue(short id)
+    {
+        Validate("MaxSwitchValue", id);
+        throw new MethodNotImplementedException("MaxSwitchValue");
+    }
+
+    /// <summary>
+    /// returns the minimum value for this switch
+    /// boolean switches must return 0.0
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public double MinSwitchValue(short id)
+    {
+        Validate("MinSwitchValue", id);
+        throw new MethodNotImplementedException("MinSwitchValue");
+    }
+
+    /// <summary>
+    /// returns the step size that this switch supports. This gives the difference between
+    /// successive values of the switch.
+    /// The number of values is ((MaxSwitchValue - MinSwitchValue) / SwitchStep) + 1
+    /// boolean switches must return 1.0, giving two states.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public double SwitchStep(short id)
+    {
+        Validate("SwitchStep", id);
+        throw new MethodNotImplementedException("SwitchStep");
+    }
+
+    /// <summary>
+    /// returns the analogue switch value for switch id
+    /// boolean switches will return 1.0 or 0.0
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public double GetSwitchValue(short id)
+    {
+        Validate("GetSwitchValue", id);
+        throw new MethodNotImplementedException("GetSwitchValue");
+    }
+
+    /// <summary>
+    /// set the analogue value for this switch.
+    /// If the switch cannot be set then throws a MethodNotImplementedException.
+    /// If the value is not between the maximum and minimum then throws an InvalidValueException
+    /// boolean switches will be set to true if the value is closer to the maximum than the minimum.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="value"></param>
+    public void SetSwitchValue(short id, double value)
+    {
+        Validate("SetSwitchValue", id, value);
+        throw new MethodNotImplementedException("SetSwitchValue");
+    }
+
+    #endregion
+    #endregion
+
+    #region private methods
+
+    /// <summary>
+    /// Checks that the switch id is in range and throws an InvalidValueException if it isn't
+    /// </summary>
+    /// <param name="message">The message.</param>
+    /// <param name="id">The id.</param>
+    private void Validate(string message,short id)
+    {
+        if (id < 0 || id >= numSwitch)
+        {
+            tl.LogMessage(message, string.Format("Switch {0} not available, range is 0 to {1}", id, numSwitch -1));
+            throw new InvalidValueException(message, id.ToString(), string.Format("0 to {0}", numSwitch - 1));
+        }
+    }
+
+    /// <summary>
+    /// Checks that the switch id and value are in range and throws an
+    /// InvalidValueException if they are not.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    /// <param name="id">The id.</param>
+    /// <param name="value">The value.</param>
+    private void Validate(string message, short id, double value)
+    {
+        Validate(message, id);
+        if (value < MinSwitchValue(id) || value > MaxSwitchValue(id))
+        {
+ 	        throw new InvalidValueException(message, value.ToString(), string.Format("Switch({0}) range {1} to {2}", id, MinSwitchValue(id), MaxSwitchValue(id)));
+        }
+    }
+
+
+    #endregion
+
+    //ENDOFINSERTEDFILE
+}
