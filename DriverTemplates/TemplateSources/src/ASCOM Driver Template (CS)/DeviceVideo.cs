0995ea2cefd247167358d208e8305aaf459e384a
simpsonpwg <simpsonpwg@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2013-10-19 15:17:43 +0000

    Template Wizard - Added video and video with base class template pro-forma's to test whether this works.

---
+++ DriverTemplates/TemplateSources/src/ASCOM Driver Template (CS)/DeviceVideo.cs
@@ -0,0 +1,21 @@
+﻿// All lines from line 1 to the device interface implementation region will be discarded by the project wizard when the template is used
+// Required code must lie within the device implementation region
+// The //ENDOFINSERTEDFILE tag must be the last but one line in this file
+
+using ASCOM;
+using ASCOM.Utilities;
+
+class DeviceVideoUsingBaseClass
+{
+    private TraceLogger tl = new TraceLogger();
+
+    #region Video Implementation
+    // Video with base class implementation
+    #endregion
+
+    #region private methods
+    // Video with base class Private Methods
+    #endregion
+
+    //ENDOFINSERTEDFILE
+}
---
+++ DriverTemplates/TemplateSources/src/ASCOM Driver Template (CS)/DeviceVideoUsingBaseClass.cs
@@ -0,0 +1,21 @@
+﻿// All lines from line 1 to the device interface implementation region will be discarded by the project wizard when the template is used
+// Required code must lie within the device implementation region
+// The //ENDOFINSERTEDFILE tag must be the last but one line in this file
+
+using ASCOM;
+using ASCOM.Utilities;
+
+class DeviceVideo
+{
+    private TraceLogger tl = new TraceLogger();
+
+    #region Video Implementation
+    // Video Implementation
+    #endregion
+
+    #region private methods
+    // Video Private Methods
+    #endregion
+
+    //ENDOFINSERTEDFILE
+}
