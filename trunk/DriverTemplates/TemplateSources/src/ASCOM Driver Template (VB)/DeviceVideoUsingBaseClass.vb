7518ec0b9a81dc5aa573b78135f202212135482d
simpsonpwg <simpsonpwg@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2013-10-19 21:20:52 +0000

    Templates - Add VB templates for Video and VideoUsingBaseClass.

---
+++ trunk/DriverTemplates/TemplateSources/src/ASCOM Driver Template (VB)/DeviceVideoUsingBaseClass.vb
@@ -0,0 +1,20 @@
+﻿' All lines from line 1 to the device interface implementation region will be discarded by the project wizard when the template is used
+' Required code must lie within the device implementation region
+' The //ENDOFINSERTEDFILE tag must be the last but one line in this file
+
+Imports ASCOM.DeviceInterface
+Imports System.Collections
+Imports ASCOM
+Imports ASCOM.Utilities
+
+Class DeviceVideoUsingBaseClass
+    'Implements IVideo
+    Private m_util As New Util()
+    Private TL As New TraceLogger()
+
+#Region "IVideo Implementation"
+    ' DeviceVideoUsingBaseClass implementation
+#End Region
+
+    '//ENDOFINSERTEDFILE
+End Class
