52aaceaacedbde9188632612ef42ad49ad8bb324
simpsonpwg <simpsonpwg@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2013-10-19 10:32:50 +0000

    InstallTemplates - Update to make this trawl the registry and My Documents for VS template directories in which to install the template files.

--- trunk/Releases/ASCOM 6/InstallTemplates/InstallTemplates/Program.cs
+++ trunk/Releases/ASCOM 6/InstallTemplates/InstallTemplates/Program.cs
@@ -16,13 +16,9 @@ namespace InstallTemplates
 
         static int Main(string[] args)
         {
-            string VS2005TemplateDir = "";
-            string VS2008TemplateDir = "";
-            string VS2010TemplateDir = "";
 
             string TemplateSourceDirectory = "";
-
-            RegistryKey VSKey;
+            Dictionary<string, string> vsTemplateDirectoryList = new Dictionary<string, string>();
 
             try
             {
@@ -34,54 +30,19 @@ namespace InstallTemplates
                 TemplateSourceDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + @"\ASCOM\Platform 6 Developer Components\Templates";
                 LogMessage("InstallTemplates", "Template Source Directory: " + TemplateSourceDirectory);
 
-                VSKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\VisualStudio\8.0", false); //VS2005 path location
-                if (VSKey != null)
-                {
-                    VS2005TemplateDir = VSKey.GetValue("UserProjectTemplatesLocation", "").ToString();
-                }
-                if (VS2005TemplateDir == "") // Check whether it has been installed into the default location but not run yet!
-                {
-                    LogMessage("InstallTemplates", "VS2005 template directory not found, searching folder: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2005\Templates\ProjectTemplates");
-                    if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2005\Templates\ProjectTemplates"))
-                    {
-                        VS2005TemplateDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2005\Templates\ProjectTemplates";
-                        LogMessage("InstallTemplates", "VS2005 template directory found: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2005\Templates\ProjectTemplates");
-                    }
-                }
+                // Search registry for template directories
+                vsTemplateDirectoryList = AddTemplateDirectories(vsTemplateDirectoryList, FindTemplateDirectoriesInRegistry(Registry.CurrentUser, @"Software\Microsoft\VisualStudio"));
+                vsTemplateDirectoryList = AddTemplateDirectories(vsTemplateDirectoryList, FindTemplateDirectoriesInRegistry(Registry.CurrentUser, @"Software\Microsoft\WDExpress"));
 
-                VSKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\VisualStudio\9.0", false); //VS2008 path location
-                if (VSKey != null)
-                {
-                    VS2008TemplateDir = VSKey.GetValue("UserProjectTemplatesLocation", "").ToString();
-                }
-                if (VS2008TemplateDir == "") // Check whether it has been installed into the default location but not run yet!
-                {
-                    LogMessage("InstallTemplates", "VS2008 template directory not found, searching folder: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2008\Templates\ProjectTemplates");
-                    if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2008\Templates\ProjectTemplates"))
-                    {
-                        VS2008TemplateDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2008\Templates\ProjectTemplates";
-                        LogMessage("InstallTemplates", "VS2008 template directory found: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2008\Templates\ProjectTemplates");
-                    }
-                }
+                // Search file system for template directories
+                vsTemplateDirectoryList = AddTemplateDirectories(vsTemplateDirectoryList, FindTemplateDirectoriesInFileSystem(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"Visual Studio*"));
 
-                VSKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\VisualStudio\10.0", false); //VS2010 path location
-                if (VSKey != null)
+                LogMessage("Main", " ");
+                foreach (KeyValuePair<string, string> templateDir in vsTemplateDirectoryList)
                 {
-                    VS2010TemplateDir = VSKey.GetValue("UserProjectTemplatesLocation", "").ToString();
+                    LogMessage("Main", "Installing templates in directory: " + templateDir.Key.ToString());
+                    InstallTemplates(templateDir.Key.ToString(), TemplateSourceDirectory);
                 }
-                if (VS2010TemplateDir == "") // Check whether it has been installed into the default location but not run yet!
-                {
-                    LogMessage("InstallTemplates", "VS2010 template directory not found, searching folder: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2010\Templates\ProjectTemplates");
-                    if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2010\Templates\ProjectTemplates"))
-                    {
-                        VS2010TemplateDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2010\Templates\ProjectTemplates";
-                        LogMessage("InstallTemplates", "VS2010 template directory found: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Visual Studio 2010\Templates\ProjectTemplates");
-                    }
-                }
-
-                InstallTemplates("Visual Studio 2005", VS2005TemplateDir,TemplateSourceDirectory);
-                InstallTemplates("Visual Studio 2008", VS2008TemplateDir, TemplateSourceDirectory);
-                InstallTemplates("Visual Studio 2010", VS2010TemplateDir,TemplateSourceDirectory);
 
             }
             catch (Exception ex)
@@ -96,8 +57,91 @@ namespace InstallTemplates
 
             return ReturnCode;
         }
+
+        static private Dictionary<string, string> AddTemplateDirectories(Dictionary<string, string> overallList, List<string> additionList)
+        {
+            foreach (string templateDirectory in additionList)
+            {
+                if (!overallList.ContainsKey(templateDirectory)) overallList.Add(templateDirectory, "");
+            }
+
+            return overallList;
+        }
+
+        static private List<string> FindTemplateDirectoriesInFileSystem(string searchDir, string searchPattern)
+        {
+            List<string> foundTempateDirectories = new List<string>();
+            string[] vsDirectories = new string[0];
+            const string projectTemplateDirectory = @"\Templates\ProjectTemplates";
+            LogMessage("FindInFileSystem", "Searching directory: " + searchDir + " for: " + searchPattern);
+
+            try
+            {
+                vsDirectories = Directory.GetDirectories(searchDir, searchPattern);
+            }
+            catch { }
+
+            if (vsDirectories.Length > 0)
+            {
+
+                foreach (string vsDirectory in vsDirectories)
+                {
+                    //LogMessage("FindTemplateDirectoriesInFileSystem", "Found: " + vsDirectory);
+
+                    if (Directory.Exists(vsDirectory + projectTemplateDirectory))
+                    {
+                        LogMessage("FindInFileSystem", "    Found Template Directory: " + vsDirectory + projectTemplateDirectory);
+                        foundTempateDirectories.Add(vsDirectory + projectTemplateDirectory);
+                    }
+                }
+            }
+            else
+            {
+                LogMessage("FindInFileSystem", "    VS directories not present");
+            }
+            return foundTempateDirectories;
+        }
+
+        static private List<string> FindTemplateDirectoriesInRegistry(RegistryKey rKey, string searchKey)
+        {
+            RegistryKey VSKey = Registry.CurrentUser;
+            List<string> foundTempateDirectories = new List<string>();
+
+            LogMessage("FindInRegistry", "Searching key: " + rKey.ToString() + searchKey);
+
+            try
+            {
+                VSKey = rKey.OpenSubKey(searchKey, false); //VS BAse Location
+            }
+            catch { }
+
+            if (VSKey != null)
+            {
+                string[] vsVersions = VSKey.GetSubKeyNames();
+
+                foreach (string vsversion in vsVersions)
+                {
+                    //LogMessage("FindTemplateDirectoriesInRegistry", "Found: " + vsversion);
+                    string keyname = searchKey + @"\" + vsversion;
+                    //LogMessage("FindTemplateDirectoriesInRegistry", "Searching for Key: " + keyname);
+                    VSKey = Registry.CurrentUser.OpenSubKey(keyname);
+                    string templateDir = (string)VSKey.GetValue(@"UserProjectTemplatesLocation");
+                    if (!string.IsNullOrEmpty(templateDir))
+                    {
+                        LogMessage("FindInRegistry", "    Found Template Directory: " + templateDir);
+                        foundTempateDirectories.Add(templateDir);
+                    }
+                }
+            }
+            else
+            {
+                LogMessage("FindInRegistry", "    Registry key not present");
+            }
+            return foundTempateDirectories;
+        }
+
         //log messages and send to screen when appropriate
-        public static void LogMessage(string section, string logMessage)
+        static public void LogMessage(string section, string logMessage)
         {
             Console.WriteLine(logMessage);
             TL.LogMessageCrLf(section, logMessage); // The CrLf version is used in order properly to format exception messages
@@ -105,7 +149,7 @@ namespace InstallTemplates
         }
 
         //log error messages and send to screen when appropriate
-        public static void LogError(string section, string logMessage)
+        static public void LogError(string section, string logMessage)
         {
             Console.WriteLine(logMessage);
             TL.LogMessageCrLf(section, logMessage); // The CrLf version is used in order properly to format exception messages
@@ -118,95 +162,55 @@ namespace InstallTemplates
         /// <param name="Name">Descriptive name of the Visual Studio release</param>
         /// <param name="TemplateBasePath">Base path to its location in the HKCU</param>
         /// <returns></returns>
-        static int InstallTemplates(string Name, string TemplateBasePath, string TemplateSourceDirectory)
+        static int InstallTemplates(string TemplateBasePath, string TemplateSourceDirectory)
         {
             string Platform5VB = TemplateBasePath + @"\Visual Basic\"; // Set up expected paths
             string Platform5CSharp = TemplateBasePath + @"\Visual C#\";
             string Platform6VB = TemplateBasePath + @"\Visual Basic\ASCOM6\";
             string Platform6CSharp = TemplateBasePath + @"\Visual C#\ASCOM6\";
 
-            if (TemplateBasePath == "")
+            LogMessage("InstallTemplates", "VB Path (Platform 5): " + Platform5VB);
+            LogMessage("InstallTemplates", "C# Path (Platform 5): " + Platform5CSharp);
+            LogMessage("InstallTemplates", "VB Path (Platform 6): " + Platform6VB);
+            LogMessage("InstallTemplates", "C# Path (Platform 6): " + Platform6CSharp);
+
+            FileDelete(Platform5CSharp, "ASCOM Camera Driver (C#).zip"); //Platform 5 C#
+            FileDelete(Platform5CSharp, "ASCOM Dome Driver (C#).zip");
+            FileDelete(Platform5CSharp, "ASCOM FilterWheel Driver (C#).zip");
+            FileDelete(Platform5CSharp, "ASCOM Focuser Driver (C#).zip");
+            FileDelete(Platform5CSharp, "ASCOM Rotator Driver (C#).zip");
+            FileDelete(Platform5CSharp, "ASCOM Switch Driver (C#).zip");
+            FileDelete(Platform5CSharp, "ASCOM Telescope Driver (C#).zip");
+            FileDelete(Platform5CSharp, "ASCOM Local Server (singleton).zip");
+
+            FileDelete(Platform5VB, "ASCOM Camera Driver (VB).zip"); // Platform 5 VB
+            FileDelete(Platform5VB, "ASCOM Dome Driver (VB).zip");
+            FileDelete(Platform5VB, "ASCOM FilterWheel Driver (VB).zip");
+            FileDelete(Platform5VB, "ASCOM Focuser Driver (VB).zip");
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB).zip");
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(2).zip"); //Remove copies introduced by a bad content file in Platform 5 Templates SP1
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(3).zip");
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(4).zip");
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(5).zip");
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(6).zip");
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(7).zip");
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(8).zip");
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(9).zip");
+            FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(10).zip");
+            FileDelete(Platform5VB, "ASCOM Switch Driver (VB).zip");
+            FileDelete(Platform5VB, "ASCOM Telescope Driver (VB).zip");
+
+            foreach (string item in Directory.GetFiles(TemplateSourceDirectory, "*.zip"))
             {
-                LogMessage("InstallTemplates", Name + " is not installed");
-            }
-            else
-            {
-                LogMessage("InstallTemplates", Name + " VB Path: " + Platform5VB);
-                LogMessage("InstallTemplates", Name + " C# Path: " + Platform5CSharp);
-
-                FileDelete(Platform5CSharp, "ASCOM Camera Driver (C#).zip"); //Platform 5 C#
-                FileDelete(Platform5CSharp, "ASCOM Dome Driver (C#).zip");
-                FileDelete(Platform5CSharp, "ASCOM FilterWheel Driver (C#).zip");
-                FileDelete(Platform5CSharp, "ASCOM Focuser Driver (C#).zip");
-                FileDelete(Platform5CSharp, "ASCOM Rotator Driver (C#).zip");
-                FileDelete(Platform5CSharp, "ASCOM Switch Driver (C#).zip");
-                FileDelete(Platform5CSharp, "ASCOM Telescope Driver (C#).zip");
-                FileDelete(Platform5CSharp, "ASCOM Local Server (singleton).zip");
-
-                FileDelete(Platform5VB, "ASCOM Camera Driver (VB).zip"); // Platform 5 VB
-                FileDelete(Platform5VB, "ASCOM Dome Driver (VB).zip");
-                FileDelete(Platform5VB, "ASCOM FilterWheel Driver (VB).zip");
-                FileDelete(Platform5VB, "ASCOM Focuser Driver (VB).zip");
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB).zip");
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(2).zip"); //Remove copies introduced by a bad content file in Platform 5 Templates SP1
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(3).zip");
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(4).zip");
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(5).zip");
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(6).zip");
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(7).zip");
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(8).zip");
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(9).zip");
-                FileDelete(Platform5VB, "ASCOM Rotator Driver (VB)(10).zip");
-                FileDelete(Platform5VB, "ASCOM Switch Driver (VB).zip");
-                FileDelete(Platform5VB, "ASCOM Telescope Driver (VB).zip");
-
-                //FileDelete(Platform6CSharp, "ASCOM Camera Driver Template CS.zip"); //Platform 6 C#
-                //FileDelete(Platform6CSharp, "ASCOM Dome Driver Template CS.zip");
-                //FileDelete(Platform6CSharp, "ASCOM FilterWheel Driver Template CS.zip");
-                //FileDelete(Platform6CSharp, "ASCOM Focuser Driver Template CS.zip");
-                //FileDelete(Platform6CSharp, "ASCOM Rotator Driver Template CS.zip");
-                //FileDelete(Platform6CSharp, "ASCOM SafetyMonitor Driver Template CS.zip");
-                //FileDelete(Platform6CSharp, "ASCOM Switch Driver Template CS.zip");
-                //FileDelete(Platform6CSharp, "ASCOM Telescope Driver Template CS.zip");
-                //FileDelete(Platform6CSharp, "ASCOM LocalServer Template CS.zip");
-
-                //FileDelete(Platform6VB, "ASCOM Camera Driver Template VB.zip"); // Platform 6 VB
-                //FileDelete(Platform6VB, "ASCOM Dome Driver Template VB.zip");
-                //FileDelete(Platform6VB, "ASCOM FilterWheel Driver Template VB.zip");
-                //FileDelete(Platform6VB, "ASCOM Focuser Driver Template VB.zip");
-                //FileDelete(Platform6VB, "ASCOM Rotator Driver Template VB.zip");
-                //FileDelete(Platform6VB, "ASCOM SafetyMonitor Driver Template VB.zip");
-                //FileDelete(Platform6VB, "ASCOM Switch Driver Template VB.zip");
-                //FileDelete(Platform6VB, "ASCOM Telescope Driver Template VB.zip");
-
-                // erase all files in the ASCOM6 part of the project templates
-                if (Directory.Exists(Platform6CSharp))
+                if (item.ToUpper().Contains("CS")) // CSharp item
                 {
-                    foreach (var item in Directory.GetFiles(Platform6CSharp, "*.zip"))
-                    {
-                        FileDelete(Platform6CSharp, Path.GetFileName(item));
-                    }
+                    File.Copy(item, Platform6CSharp + "\\" + Path.GetFileName(item), true);
+                    LogMessage("InstallTemplates", "Copying C# template: " + item + " as: " + Platform6CSharp + Path.GetFileName(item));
                 }
-                if (Directory.Exists(Platform6VB))
+                if (item.ToUpper().Contains("VB")) // VBitem
                 {
-                    foreach (var item in Directory.GetFiles(Platform6VB, "*.zip"))
-                    {
-                        FileDelete(Platform6VB, Path.GetFileName(item));
-                    }
-                }
-
-                foreach (string item in Directory.GetFiles(TemplateSourceDirectory, "*.zip"))
-                {
-                    if (item.ToUpper().Contains("CS")) // CSharp item
-                    {
-                        File.Copy(item, Platform6CSharp + "\\" + Path.GetFileName(item), true);
-                        LogMessage("InstallTemplates", "Copying C# template: " + item + " as: " + Platform6CSharp + "\\" + Path.GetFileName(item));
-                    }
-                    if (item.ToUpper().Contains("VB")) // VBitem
-                    {
-                        File.Copy(item, Platform6VB + "\\" + Path.GetFileName(item), true);
-                        LogMessage("InstallTemplates", "Copying VB template: " + item + " as: " + Platform6CSharp + "\\" + Path.GetFileName(item));
-                    }
+                    File.Copy(item, Platform6VB + "\\" + Path.GetFileName(item), true);
+                    LogMessage("InstallTemplates", "Copying VB template: " + item + " as: " + Platform6CSharp + Path.GetFileName(item));
                 }
             }
 
