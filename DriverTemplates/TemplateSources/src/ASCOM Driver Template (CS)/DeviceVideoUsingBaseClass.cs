323c59216ed058618eed882cea8bafbc06364926
simpsonpwg <simpsonpwg@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2013-10-23 21:23:23 +0000

    Video Template - First set of changes to template.

--- DriverTemplates/TemplateSources/src/ASCOM Driver Template (CS)/DeviceVideoUsingBaseClass.cs
+++ DriverTemplates/TemplateSources/src/ASCOM Driver Template (CS)/DeviceVideoUsingBaseClass.cs
@@ -4,13 +4,210 @@
 
 using ASCOM;
 using ASCOM.Utilities;
+using System.Diagnostics;
 
 class DeviceVideoUsingBaseClass
 {
     private TraceLogger tl = new TraceLogger();
 
     #region IVideo Implementation
-    // DeviceVideoUsingBaseClass Implementation
+    
+		/// <exception cref="T:ASCOM.NotConnectedException">Must throw exception if data unavailable.</exception>
+		/// <exception cref="T:ASCOM.PropertyNotImplementedException">Must throw exception if camera supports only one integration rate (exposure) that cannot be changed.</exception>		
+		public System.Collections.ArrayList SupportedIntegrationRates
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("SupportedIntegrationRates", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		///	<exception cref="T:ASCOM.InvalidValueException">Must throw an exception if not valid.</exception>
+		///	<exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if the camera supports only one integration rate (exposure) that cannot be changed.</exception>
+		public int IntegrationRate
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("IntegrationRate", false);
+			}
+
+			[DebuggerStepThrough]
+			set
+			{
+				throw new PropertyNotImplementedException("IntegrationRate", true);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		/// <exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gainmin is not supported</exception>
+		public string SensorName
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("SensorName", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw exception if the value is not known</exception>
+		public int CameraXSize
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("CameraXSize", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw exception if the value is not known</exception>
+		/// <exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gainmin is not supported</exception>
+		public int CameraYSize
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("CameraYSize", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw exception if data unavailable.</exception>
+		/// <exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gainmin is not supported</exception>
+		public double PixelSizeX
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("PixelSizeX", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw exception if data unavailable.</exception>
+		/// <exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gainmin is not supported</exception>
+		public double PixelSizeY
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("PixelSizeY", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		///	<exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gainmax is not supported</exception>
+		public short GainMax
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("GainMax", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		///	<exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gainmin is not supported</exception>
+		public short GainMin
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("GainMin", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		///	<exception cref="T:ASCOM.InvalidValueException">Must throw an exception if not valid.</exception>
+		///	<exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gain is not supported</exception>
+		public short Gain
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("Gain", false);
+			}
+
+			[DebuggerStepThrough]
+			set
+			{
+				throw new PropertyNotImplementedException("Gain", true);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		///	<exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if Gains is not supported</exception>
+		public System.Collections.ArrayList Gains
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("Gains", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		///	<exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gainmax is not supported</exception>
+		public short GammaMax
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("GainMax", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		///	<exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gainmin is not supported</exception>
+		public short GammaMin
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("GainMin", false);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		///	<exception cref="T:ASCOM.InvalidValueException">Must throw an exception if not valid.</exception>
+		///	<exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gamma is not supported</exception>
+		public short Gamma
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("Gamma", false);
+			}
+
+			[DebuggerStepThrough]
+			set
+			{
+				throw new PropertyNotImplementedException("Gamma", true);
+			}
+		}
+
+		///	<exception cref="T:ASCOM.NotConnectedException">Must throw an exception if the information is not available. (Some drivers may require an 
+		///	active <see cref="P:ASCOM.DeviceInterface.IVideo.Connected">connection</see> in order to retrieve necessary information from the camera.)</exception>
+		///	<exception cref="T:ASCOM.PropertyNotImplementedException">Must throw an exception if gainmin is not supported</exception>
+		public System.Collections.ArrayList Gammas
+		{
+			[DebuggerStepThrough]
+			get
+			{
+				throw new PropertyNotImplementedException("Gammas", false);
+			}
+		}
+
+
     #endregion
 
     //ENDOFINSERTEDFILE
