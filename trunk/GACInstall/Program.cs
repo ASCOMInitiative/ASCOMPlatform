bc10cd91761a128e354268285c2d010c0b50f73d
simpsonpwg <simpsonpwg@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2013-11-16 18:20:15 +0000

    Switch simulator - convert to Framework 4 client so this doesn't trigger a full installation on out of the box Win 7 systems. GACInstall - convert to using supported framework method to install / uninstall GAC assemblies. Installer - Add cheking for pre-requeisties dialogue when searching for VC10 librariesbecause this operation can take some time. Installer - Add time to each log message and further log messages Installer - Remove TLB file left over from Video simulator so that uninstall is completely clean

--- trunk/GACInstall/Program.cs
+++ trunk/GACInstall/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.EnterpriseServices.Internal;
 //// Artinsoft
 //// Author: Mauricio Rojas orellabac@gmail.com mrojas@artinsoft.com
 //// This program uses the undocumented GAC API to perform a simple install of an assembly
@@ -9,6 +9,14 @@ using System.Text;
 //// Revised by Peter Simpson to add help
 ////                          to uninstall as well as install
 ////                          to provide textual description of uninstall outcome
+
+//// Updated to use the mangaged API System.EnterpriseServices.Internal.Publish() by Peter Simpson 16th October 2013
+//// This gets rid of the unpublished FusionLib and GAC access assembly of Maurico Rojas and brings us back to 
+//// managed APIs at last! Hooray!
+
+//// Return code: 0 OK
+////              1 Unable to find file
+////              2 An exception happened
 
 namespace ASCOM.Internal.GACInstall
 {
@@ -21,104 +29,112 @@ namespace ASCOM.Internal.GACInstall
             bool install = true; // Initialise variables
             string filename = "";
             bool help = false;
+            bool versions = false;
+
             if (args.Length == 0) help = true; // No arguments will print a help message
-            if (args.Length == 1) // One argument could be /H or a filename
+            if (args.Length == 1) // One argument could be /H /V or a filename
             {
-                if (string.Compare(args[0],"/H",true) == 0) help = true; // Test for help switch
-                if (string.Compare(args[0],"-H",true) == 0) help = true;
+                if (string.Compare(args[0], "/H", true) == 0) help = true; // Test for help switch
+                if (string.Compare(args[0], "-H", true) == 0) help = true;
+                if (string.Compare(args[0], "/V", true) == 0) versions = true; // Test for help switch
+                if (string.Compare(args[0], "-V", true) == 0) versions = true;
                 filename = args[0];
             }
+
             if (args.Length == 2)
             {
-                if (string.Compare(args[0],"/H",true) == 0) help = true; // Test for help switch
-                if (string.Compare(args[0],"-H",true) == 0) help = true; 
-                if (string.Compare(args[1],"/H",true) == 0) help = true; 
-                if (string.Compare(args[1],"-H",true) == 0) help = true; 
-            
-                if ((string.Compare(args[0],"/U",true) == 0) || (string.Compare(args[0],"-U",true) == 0))
+                if (string.Compare(args[0], "/H", true) == 0) help = true; // Test for help switch
+                if (string.Compare(args[0], "-H", true) == 0) help = true;
+                if (string.Compare(args[1], "/H", true) == 0) help = true;
+                if (string.Compare(args[1], "-H", true) == 0) help = true;
+
+                if ((string.Compare(args[0], "/U", true) == 0) || (string.Compare(args[0], "-U", true) == 0))
                 { // Found uninstall switch in first argument
-                    install = false; 
-                    filename=args[1]; // Filename must be second argument
+                    install = false;
+                    filename = args[1]; // Filename must be second argument
                 }
                 else
                 {
-                    filename=args[0]; // Filename must be first argument
-                    if ((string.Compare(args[1],"/U",true) == 0) || (string.Compare(args[1],"-U",true) == 0))
+                    filename = args[0]; // Filename must be first argument
+                    if ((string.Compare(args[1], "/U", true) == 0) || (string.Compare(args[1], "-U", true) == 0))
                     {
                         install = false; // Uninstall switch found as second argument
                     }
                 }
             }
-             
-            // help, install and filename now contain the supplied parameters
+
+            // help, versions, install and filename now contain the supplied parameters
             if (help)
             {
                 // Help requested so print message
                 Console.WriteLine("GACInstall adds and removes assemblies from the cache");
-                Console.WriteLine(""); 
+                Console.WriteLine("");
                 Console.WriteLine("Usage: GACInstall AssemblyFileName");
                 Console.WriteLine("       GACInstall /U AssemblyName");
                 return 0;
             }
 
-            // Create an FUSION_INSTALL_REFERENCE struct and fill it with data
-            // We use opaque scheme here
-            FUSION_INSTALL_REFERENCE[] installReference = new FUSION_INSTALL_REFERENCE[1];
-            installReference[0].dwFlags = 0;
-            installReference[0].guidScheme = AssemblyCache.FUSION_REFCOUNT_OPAQUE_STRING_GUID;
-            installReference[0].szIdentifier = "GACInstall";
-            installReference[0].szNonCannonicalData= "Installed by Peter Simpson's GACInstall program";
-            installReference[0].cbSize = 40;
+            if (versions)
+            {
+                // versions requested so list the required information
+                Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().FullName);
+                Console.WriteLine(System.IO.Directory.Exists("fusion.dll"));
+                Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+                Console.WriteLine("Full path to fusion.dll: " + System.IO.Path.GetFullPath("fusion.dll") + "#");
+                Console.WriteLine("Path: " + System.Environment.GetEnvironmentVariable("PATH"));
+                return 0;
+            }
 
-            // Get an IAssemblyCache interface
-            IAssemblyCache pCache = AssemblyCache.CreateAssemblyCache();
+            int result = 0; // Initialise return code to OK
 
             if (install)
             { // We are going to install an assembly
-                if (System.IO.File.Exists(filename))
+                try
                 {
-                    //Next line changed so that no install reference is provided which allows easy uninstall from Assembly explorer display
-                    //int result = pCache.InstallAssembly(0, filename, installReference);
-                    int result = pCache.InstallAssembly(0, filename, null);
-                    if (result == 0) Console.WriteLine("Installed OK!");
-                    else Console.WriteLine("Install returned error code: " + result);
+                    if (System.IO.File.Exists(filename))
+                    {
+                        new System.EnterpriseServices.Internal.Publish().GacInstall(filename);
+                        return result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unable to find file: " + filename);
+                        return 1;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    result = 2;
+                    Console.WriteLine("Exception: " + ex.ToString());
                     return result;
                 }
-                else
-                {
-                    Console.WriteLine("Unable to find file: " + filename);
-                    return 1;
-                }
             }
-            else
-            { // We are going to uninstall an assembly
-                IASSEMBLYCACHE_UNINSTALL_DISPOSITION puldisposition = IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_UNKNOWN; // Initialise variable
-                //Next line changed so that no install reference is provided which allows easy uninstall from Assembly explorer display
-                //int result = pCache.UninstallAssembly(0, filename, installReference, out puldisposition);
-                int result = pCache.UninstallAssembly(0, filename, null, out puldisposition);
-                if (result == 0) Console.WriteLine("Uninstalled with no error!");
-                else Console.WriteLine("Uninstall returned error code: " + result);
-
-                switch (puldisposition)
+            else // We are going to uninstall an assembly
+            {
+                try
                 {
-                    case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_ALREADY_UNINSTALLED:
-                        Console.WriteLine("Outcome: Assembly already uninstalled"); break;
-                    case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_DELETE_PENDING:
-                        Console.WriteLine("Outcome: Delete currently pending"); break;
-                    case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_HAS_INSTALL_REFERENCES:
-                        Console.WriteLine("Outcome: Assembly has remaining install references"); break;
-                    case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_REFERENCE_NOT_FOUND:
-                        Console.WriteLine("Outcome: Unable to find assembly - " + filename); break;
-                    case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_STILL_IN_USE:
-                        Console.WriteLine("Outcome: Assembly still in use"); break;
-                    case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_UNINSTALLED:
-                        Console.WriteLine("Outcome: Assembly uninstalled"); break;
-                    default:
-                        Console.WriteLine("Unknown uninstall outcome code: " + puldisposition); break;
+                    if (System.IO.File.Exists(filename))
+                    {
+                        Publish pub = new Publish();
+                        Console.WriteLine("ToString(): " + filename + " " + pub.ToString());
+                        pub.GacRemove(filename);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unable to find file: " + filename);
+                        return 1;
+                    }
                 }
+                catch (Exception ex)
+                {
+                    result = 2;
+                    Console.WriteLine("Exception: " + ex.ToString());
+                    return result;
+                }
+
                 return result;
             }
-           
+
         }
     }
 }
