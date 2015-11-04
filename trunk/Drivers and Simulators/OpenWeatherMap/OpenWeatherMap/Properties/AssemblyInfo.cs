36604822bbd0544ca60ccadc4c8348041e5e4271
chris-rowland <chris-rowland@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2015-11-04 18:00:48 +0000

    First commit of the OpenWeatherMap ObservingConditions driver. Currently built using the platform references, not the project references.

---
+++ trunk/Drivers and Simulators/OpenWeatherMap/OpenWeatherMap/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+// TODO - Add your authorship information here
+[assembly: AssemblyTitle("ASCOM.OpenWeatherMap.ObservingConditions")]
+[assembly: AssemblyDescription("ASCOM ObservingConditions driver for OpenWeatherMap")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The ASCOM Initiative")]
+[assembly: AssemblyProduct("ASCOM ObservingConditions driver for OpenWeatherMap")]
+[assembly: AssemblyCopyright("Copyright © 2015 The ASCOM Initiative")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50c4bf82-d6c8-4314-b0cb-bfe096513615")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//
+// TODO - Set your driver's version here
+[assembly: AssemblyVersion("6.0.*")]
+//[assembly: AssemblyFileVersion("6.0.0.0")]
