37a47164612a8d583fdc1a433079ed7db4310a90
simpsonpwg <simpsonpwg@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2015-12-19 12:44:43 +0000

    Internal - Revert GACInstall to use the Fusion library to remove assemblies because the Framework Publish.Uninstall(File) does not appear to work with all our assemblies. Installers - Fix uninstall so that it removes ASCOM.Utilities.VideoUtils and completely removes all recently introduced ASCOM assemblies from the GAC and NativeImages caches.

--- trunk/GACInstall/Program.cs
+++ trunk/GACInstall/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.EnterpriseServices.Internal;
+using System.Reflection;
+
 //// Artinsoft
 //// Author: Mauricio Rojas orellabac@gmail.com mrojas@artinsoft.com
 //// This program uses the undocumented GAC API to perform a simple install of an assembly
@@ -30,14 +30,17 @@ namespace ASCOM.Internal.GACInstall
             string filename = "";
             bool help = false;
             bool versions = false;
+            bool list = false;
 
             if (args.Length == 0) help = true; // No arguments will print a help message
-            if (args.Length == 1) // One argument could be /H /V or a filename
+            if (args.Length == 1) // One argument could be /H /V or /L a filename
             {
                 if (string.Compare(args[0], "/H", true) == 0) help = true; // Test for help switch
                 if (string.Compare(args[0], "-H", true) == 0) help = true;
                 if (string.Compare(args[0], "/V", true) == 0) versions = true; // Test for help switch
                 if (string.Compare(args[0], "-V", true) == 0) versions = true;
+                if (string.Compare(args[0], "/L", true) == 0) list = true; // Test for list switch
+                if (string.Compare(args[0], "-L", true) == 0) list = true;
                 filename = args[0];
             }
 
@@ -85,7 +88,73 @@ namespace ASCOM.Internal.GACInstall
                 return 0;
             }
 
+            if (list)
+            {
+                // List ASCOM assemblies
+                SortedList<String, String> assemblyNames = new SortedList<String, String>();
+                IAssemblyEnum assemblyEnumerator = AssemblyCache.CreateGACEnum(); // Get an enumerator for the GAC assemblies
+                IAssemblyName iAssemblyName = null;
+
+                while ((AssemblyCache.GetNextAssembly(assemblyEnumerator, out iAssemblyName) == 0))
+                {
+                    try
+                    {
+                        AssemblyName assemblyName = new AssemblyName();
+                        try
+                        {
+                            assemblyName.Name = AssemblyCache.GetName(iAssemblyName);
+                            assemblyName.Version = AssemblyCache.GetVersion(iAssemblyName);
+                            assemblyName.CultureInfo = AssemblyCache.GetCulture(iAssemblyName);
+                            assemblyName.SetPublicKeyToken(AssemblyCache.GetPublicKeyToken(iAssemblyName));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("GetAssemblyName Exception: " + ex.ToString());
+                        }
+
+                        assemblyNames.Add(assemblyName.FullName, assemblyName.Name);
+                    }
+                    catch
+                    {
+                        // Ignore an exceptions here due to duplicate names, these are all MS assemblies
+                    }
+
+                }
+
+                Console.WriteLine("\r\nInstalled ASCOM assemblies: ");
+
+                foreach (KeyValuePair<string, string> assemblyName in assemblyNames)
+                {
+                    //  Process each assembly in turn
+                    try
+                    {
+                        if (((assemblyName.Key.IndexOf("ASCOM") + 1) > 0) || (assemblyName.Key.Contains("565de7938946fba7")))
+                        {
+                            Console.WriteLine(assemblyName.Key);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("AssemblyName.Key Exception: " + ex.ToString());
+                    }
+                }
+                return 0;
+            }
+
+            // Must be install or uninstall
             int result = 0; // Initialise return code to OK
+
+            // Create an FUSION_INSTALL_REFERENCE struct and fill it with data
+            // We use opaque scheme here
+            FUSION_INSTALL_REFERENCE[] installReference = new FUSION_INSTALL_REFERENCE[1];
+            installReference[0].dwFlags = 0;
+            installReference[0].guidScheme = AssemblyCache.FUSION_REFCOUNT_OPAQUE_STRING_GUID;
+            installReference[0].szIdentifier = "GACInstall";
+            installReference[0].szNonCannonicalData = "Installed by Peter Simpson's GACInstall program";
+            installReference[0].cbSize = 40;
+
+            // Get an IAssemblyCache interface
+            IAssemblyCache pCache = AssemblyCache.CreateAssemblyCache();
 
             if (install)
             { // We are going to install an assembly
@@ -93,8 +162,9 @@ namespace ASCOM.Internal.GACInstall
                 {
                     if (System.IO.File.Exists(filename))
                     {
-                        Publish pub = new Publish();
-                        pub.GacInstall(filename);
+                        result = pCache.InstallAssembly(0, filename, null);
+                        if (result == 0) Console.WriteLine("Installed OK!");
+                        else Console.WriteLine("Install returned error code: " + result);
                         return result;
                     }
                     else
@@ -114,23 +184,35 @@ namespace ASCOM.Internal.GACInstall
             {
                 try
                 {
-                    if (System.IO.File.Exists(filename))
-                    {
-                        Publish pub = new Publish();
-                        Console.WriteLine("ToString(): " + filename + " " + pub.ToString());
-                        pub.GacRemove(filename);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Unable to find file: " + filename);
-                        return 1;
+                    { // We are going to uninstall an assembly
+                        IASSEMBLYCACHE_UNINSTALL_DISPOSITION puldisposition = IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_UNKNOWN; // Initialise variable
+                        result = pCache.UninstallAssembly(0, filename, null, out puldisposition);
+                        if (result == 0) Console.WriteLine("Uninstalled with no error!");
+                        else Console.WriteLine("Uninstall returned error code: " + result);
+
+                        switch (puldisposition)
+                        {
+                            case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_ALREADY_UNINSTALLED:
+                                Console.WriteLine("Outcome: Assembly already uninstalled"); break;
+                            case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_DELETE_PENDING:
+                                Console.WriteLine("Outcome: Delete currently pending"); break;
+                            case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_HAS_INSTALL_REFERENCES:
+                                Console.WriteLine("Outcome: Assembly has remaining install references"); break;
+                            case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_REFERENCE_NOT_FOUND:
+                                Console.WriteLine("Outcome: Unable to find assembly - " + filename); break;
+                            case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_STILL_IN_USE:
+                                Console.WriteLine("Outcome: Assembly still in use"); break;
+                            case IASSEMBLYCACHE_UNINSTALL_DISPOSITION.IASSEMBLYCACHE_UNINSTALL_DISPOSITION_UNINSTALLED:
+                                Console.WriteLine("Outcome: Assembly uninstalled"); break;
+                            default:
+                                Console.WriteLine("Unknown uninstall outcome code: " + puldisposition); break;
+                        }
                     }
                 }
                 catch (Exception ex)
                 {
                     result = 2;
                     Console.WriteLine("Exception: " + ex.ToString());
-                    return result;
                 }
 
                 return result;
