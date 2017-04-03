00b8e5d95d18e46e7785b99deb3ef8242ec6ecbc
simpsonpwg <simpsonpwg@60bb46e1-9293-8f4a-b0db-2b7d974c5fa5>
2017-04-03 09:38:31 +0000

    Help - Update help text to clarify units for Telescope.DeclinationRate and the sidereal rate value in the Telescope.DriveRates enumeration.

--- ASCOM.DriverAccess/Telescope.cs
+++ ASCOM.DriverAccess/Telescope.cs
@@ -486,7 +486,7 @@ namespace ASCOM.DriverAccess
         }
 
         /// <summary>
-        /// The declination tracking rate (arcseconds per second, default = 0.0)
+        /// The declination tracking rate (arcseconds per SI second, default = 0.0)
         /// </summary>
         /// <remarks>
         /// This property, together with <see cref="RightAscensionRate" />, provides support for "offset tracking".
@@ -506,6 +506,7 @@ namespace ASCOM.DriverAccess
         /// destination coordinates at the given offset rate.</description></item>
         /// <item><description>This will allow precise slews to a fast-moving target with a slow-slewing telescope.</description></item>
         /// <item><description>When the slew completes, the <see cref="TargetRightAscension" /> and <see cref="TargetDeclination" /> properties should reflect the final (adjusted) destination.</description></item>
+        /// <item><description>The units of this property are arcseconds per SI (atomic) second. Please note that for historic reasons the units of the <see cref="RightAscensionRate" /> property are seconds per sidereal second.</description></item>
         /// </list>
         /// </para>
         /// <para>
@@ -784,7 +785,7 @@ namespace ASCOM.DriverAccess
         /// <item><description>If this property is zero, tracking will be at the selected <see cref="TrackingRate" />.</description></item>
         /// <item><description>If <see cref="CanSetRightAscensionRate" /> is False, this property must always return 0.</description></item> 
         /// To discover whether this feature is supported, test the <see cref="CanSetRightAscensionRate" />property. 
-        /// <item><description>The property value is in in seconds of right ascension per sidereal second.</description></item> 
+        /// <item><description>The units of this property are seconds of right ascension per sidereal second. Please note that for historic reasons the units of the <see cref="DeclinationRate" /> property are arcseconds per SI second.</description></item> 
         /// <item><description>To convert a given rate in (the more common) units of sidereal seconds
         /// per UTC (clock) second, multiply the value by 0.9972695677 
         /// (the number of UTC seconds in a sidereal second) then set the property.
