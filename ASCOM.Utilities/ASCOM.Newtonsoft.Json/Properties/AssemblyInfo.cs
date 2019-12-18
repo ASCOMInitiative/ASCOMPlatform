58b6ea012c41ea9ad03a811a9476929682788d7b
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2019-12-18 17:33:16 +0000

    Give new components the Platform version number scheme

--- ASCOM.Utilities/ASCOM.Newtonsoft.Json/Properties/AssemblyInfo.cs
+++ ASCOM.Utilities/ASCOM.Newtonsoft.Json/Properties/AssemblyInfo.cs
@@ -32,39 +32,19 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-#if NET20
-[assembly: AllowPartiallyTrustedCallers]
-#elif NET35
-[assembly: AllowPartiallyTrustedCallers]
-#elif NET40
-[assembly: AllowPartiallyTrustedCallers]
-#else
-[assembly: AllowPartiallyTrustedCallers]
-#endif
-
-#if !SIGNED
-
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Schema")]
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Tests")]
-#else
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Schema, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f561df277c6c0b497d629032b410cdcf286e537c054724f7ffa0164345f62b3e642029d7a80cc351918955328c4adc8a048823ef90b0cf38ea7db0d729caf2b633c3babe08b0310198c1081995c19029bc675193744eab9d7345b8a67258ec17d112cebdbbb2a281487dceeafb9d83aa930f32103fbe1d2911425bc5744002c7")]
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f561df277c6c0b497d629032b410cdcf286e537c054724f7ffa0164345f62b3e642029d7a80cc351918955328c4adc8a048823ef90b0cf38ea7db0d729caf2b633c3babe08b0310198c1081995c19029bc675193744eab9d7345b8a67258ec17d112cebdbbb2a281487dceeafb9d83aa930f32103fbe1d2911425bc5744002c7")]
-#endif
-
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Dynamic, PublicKey=0024000004800000940000000602000000240000525341310004000001000100cbd8d53b9d7de30f1f1278f636ec462cf9c254991291e66ebb157a885638a517887633b898ccbcf0d5c5ff7be85a6abe9e765d0ac7cd33c68dac67e7e64530e8222101109f154ab14a941c490ac155cd1d4fcba0fabb49016b4ef28593b015cab5937da31172f03f67d09edda404b88a60023f062ae71d0b2e4438b74cc11dc9")]
+[assembly: AssemblyTitle("Json.NET for ASCOM")]
+[assembly: AssemblyDescription("Json.NET is a popular high-performance JSON framework for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ASCOM Initiative")]
+[assembly: AssemblyProduct("Json.NET for ASCOM")]
+[assembly: AssemblyCopyright("Copyright © James Newton-King 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-#if HAVE_COM_ATTRIBUTES
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components. If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("9ca358aa-317b-4925-8ada-4a29e943a363")]
-#endif
+[assembly: Guid("63B632A6-F4AA-424F-B956-4030122C5DA4")]
 
 [assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("6.0.0.0")]
--- ASCOM.Utilities/Alpaca Support/Properties/AssemblyInfo.cs
+++ ASCOM.Utilities/Alpaca Support/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Alpaca Support")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ASCOM Utilities Support")]
+[assembly: AssemblyDescription("C# support assembly for the VB Utilities components")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Alpaca Support")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("ASCOM Utilities Support")]
+[assembly: AssemblyCopyright("Copyright ASCOM © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
