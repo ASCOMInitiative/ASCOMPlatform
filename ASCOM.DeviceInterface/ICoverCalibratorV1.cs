d3af2e159b0bbfef36d40dfc71e2186e1d70473d
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2020-01-19 09:43:36 +0000

    Add common methods to ICoverCalibrator interface

--- ASCOM.DeviceInterface/ICoverCalibratorV1.cs
+++ ASCOM.DeviceInterface/ICoverCalibratorV1.cs
@@ -17,6 +17,198 @@ namespace ASCOM.DeviceInterface
     [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
     public interface ICoverCalibratorV1
     {
+        #region Common Methods
+
+        /// <summary>
+        /// Set True to connect to the device hardware. Set False to disconnect from the device hardware.
+        /// You can also read the property to check whether it is connected. This reports the current hardware state.
+        /// </summary>
+        /// <value><c>true</c> if connected to the hardware; otherwise, <c>false</c>.</value>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks>
+        /// <p style="color:red"><b>Must be implemented</b></p>Do not use a NotConnectedException here, that exception is for use in other methods that require a connection in order to succeed.
+        /// <para>The Connected property sets and reports the state of connection to the device hardware.
+        /// For a hub this means that Connected will be true when the first driver connects and will only be set to false
+        /// when all drivers have disconnected.  A second driver may find that Connected is already true and
+        /// setting Connected to false does not report Connected as false.  This is not an error because the physical state is that the
+        /// hardware connection is still true.</para>
+        /// <para>Multiple calls setting Connected to true or false will not cause an error.</para>
+        /// </remarks>
+        bool Connected { get; set; }
+
+        /// <summary>
+        /// Returns a description of the device, such as manufacturer and modelnumber. Any ASCII characters may be used.
+        /// </summary>
+        /// <value>The description.</value>
+        /// <exception cref="NotConnectedException">If the device is not connected and this information is only available when connected.</exception>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks><p style="color:red"><b>Must be implemented</b></p> </remarks>
+        string Description { get; }
+
+        /// <summary>
+        /// Descriptive and version information about this ASCOM driver.
+        /// </summary>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks>
+        /// <p style="color:red"><b>Must be implemented</b></p> This string may contain line endings and may be hundreds to thousands of characters long.
+        /// It is intended to display detailed information on the ASCOM driver, including version and copyright data.
+        /// See the <see cref="Description" /> property for information on the device itself.
+        /// To get the driver version in a parseable string, use the <see cref="DriverVersion" /> property.
+        /// </remarks>
+        string DriverInfo { get; }
+
+        /// <summary>
+        /// A string containing only the major and minor version of the driver.
+        /// </summary>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks><p style="color:red"><b>Must be implemented</b></p> This must be in the form "n.n".
+        /// It should not to be confused with the <see cref="InterfaceVersion" /> property, which is the version of this specification supported by the
+        /// driver.
+        /// </remarks>
+        string DriverVersion { get; }
+
+        /// <summary>
+        /// The interface version number that this device supports. Should return 2 for this interface version.
+        /// </summary>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks><p style="color:red"><b>Must be implemented</b></p> Clients can detect legacy V1 drivers by trying to read ths property.
+        /// If the driver raises an error, it is a V1 driver. V1 did not specify this property. A driver may also return a value of 1.
+        /// In other words, a raised error or a return value of 1 indicates that the driver is a V1 driver.
+        /// </remarks>
+        short InterfaceVersion { get; }
+
+        /// <summary>
+        /// The short name of the driver, for display purposes
+        /// </summary>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks><p style="color:red"><b>Must be implemented</b></p> </remarks>
+        string Name { get; }
+
+        /// <summary>
+        /// Launches a configuration dialog box for the driver.  The call will not return
+        /// until the user clicks OK or cancel manually.
+        /// </summary>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks><p style="color:red"><b>Must be implemented</b></p> </remarks>
+        void SetupDialog();
+
+        /// <summary>
+        /// Invokes the specified device-specific action.
+        /// </summary>
+        /// <param name="ActionName">
+        /// A well known name agreed by interested parties that represents the action to be carried out.
+        /// </param>
+        /// <param name="ActionParameters">List of required parameters or an <see cref="String.Empty">Empty String</see> if none are required.
+        /// </param>
+        /// <returns>A string response. The meaning of returned strings is set by the driver author.</returns>
+        /// <exception cref="ASCOM.MethodNotImplementedException">Throws this exception if no actions are suported.</exception>
+        /// <exception cref="ASCOM.ActionNotImplementedException">It is intended that the SupportedActions method will inform clients
+        /// of driver capabilities, but the driver must still throw an ASCOM.ActionNotImplemented exception if it is asked to
+        /// perform an action that it does not support.</exception>
+        /// <exception cref="NotConnectedException">If the driver is not connected.</exception>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <example>Suppose filter wheels start to appear with automatic wheel changers; new actions could
+        /// be “FilterWheel:QueryWheels” and “FilterWheel:SelectWheel”. The former returning a
+        /// formatted list of wheel names and the second taking a wheel name and making the change, returning appropriate
+        /// values to indicate success or failure.
+        /// </example>
+        /// <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p>
+        /// This method is intended for use in all current and future device types and to avoid name clashes, management of action names
+        /// is important from day 1. A two-part naming convention will be adopted - <b>DeviceType:UniqueActionName</b> where:
+        /// <list type="bullet">
+        /// <item><description>DeviceType is the same value as would be used by <see cref="ASCOM.Utilities.Chooser.DeviceType"/> e.g. Telescope, Camera, Switch etc.</description></item>
+        /// <item><description>UniqueActionName is a single word, or multiple words joined by underscore characters, that sensibly describes the action to be performed.</description></item>
+        /// </list>
+        /// <para>
+        /// It is recommended that UniqueActionNames should be a maximum of 16 characters for legibility.
+        /// Should the same function and UniqueActionName be supported by more than one type of device, the reserved DeviceType of
+        /// “General” will be used. Action names will be case insensitive, so FilterWheel:SelectWheel, filterwheel:selectwheel
+        /// and FILTERWHEEL:SELECTWHEEL will all refer to the same action.</para>
+        /// <para>The names of all supported actions must be returned in the <see cref="SupportedActions"/> property.</para>
+        /// </remarks>
+        string Action(string ActionName, string ActionParameters);
+
+        /// <summary>
+        /// Returns the list of action names supported by this driver.
+        /// </summary>
+        /// <value>An ArrayList of strings (SafeArray collection) containing the names of supported actions.</value>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks><p style="color:red"><b>Must be implemented</b></p> This method must return an empty arraylist if no actions are supported. Please do not throw a
+        /// <see cref="ASCOM.PropertyNotImplementedException" />.
+        /// <para>This is an aid to client authors and testers who would otherwise have to repeatedly poll the driver to determine its capabilities.
+        /// Returned action names may be in mixed case to enhance presentation but  will be recognised case insensitively in
+        /// the <see cref="Action">Action</see> method.</para>
+        /// <para>An array list collection has been selected as the vehicle for  action names in order to make it easier for clients to
+        /// determine whether a particular action is supported. This is easily done through the Contains method. Since the
+        /// collection is also ennumerable it is easy to use constructs such as For Each ... to operate on members without having to be concerned
+        /// about hom many members are in the collection. </para>
+        /// <para>Collections have been used in the Telescope specification for a number of years and are known to be compatible with COM. Within .NET
+        /// the ArrayList is the correct implementation to use as the .NET Generic methods are not compatible with COM.</para>
+        /// </remarks>
+        ArrayList SupportedActions { get; }
+
+        /// <summary>
+        /// Transmits an arbitrary string to the device and does not wait for a response.
+        /// Optionally, protocol framing characters may be added to the string before transmission.
+        /// </summary>
+        /// <param name="Command">The literal command string to be transmitted.</param>
+        /// <param name="Raw">
+        /// if set to <c>true</c> the string is transmitted 'as-is'.
+        /// If set to <c>false</c> then protocol framing characters may be added prior to transmission.
+        /// </param>
+        /// <exception cref="MethodNotImplementedException">If the method is not implemented</exception>
+        /// <exception cref="NotConnectedException">If the driver is not connected.</exception>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> </remarks>
+        void CommandBlind(string Command, bool Raw = false);
+
+        /// <summary>
+        /// Transmits an arbitrary string to the device and waits for a boolean response.
+        /// Optionally, protocol framing characters may be added to the string before transmission.
+        /// </summary>
+        /// <param name="Command">The literal command string to be transmitted.</param>
+        /// <param name="Raw">
+        /// if set to <c>true</c> the string is transmitted 'as-is'.
+        /// If set to <c>false</c> then protocol framing characters may be added prior to transmission.
+        /// </param>
+        /// <returns>
+        /// Returns the interpreted boolean response received from the device.
+        /// </returns>
+        /// <exception cref="MethodNotImplementedException">If the method is not implemented</exception>
+        /// <exception cref="NotConnectedException">If the driver is not connected.</exception>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> </remarks>
+        bool CommandBool(string Command, bool Raw = false);
+
+        /// <summary>
+        /// Transmits an arbitrary string to the device and waits for a string response.
+        /// Optionally, protocol framing characters may be added to the string before transmission.
+        /// </summary>
+        /// <param name="Command">The literal command string to be transmitted.</param>
+        /// <param name="Raw">
+        /// if set to <c>true</c> the string is transmitted 'as-is'.
+        /// If set to <c>false</c> then protocol framing characters may be added prior to transmission.
+        /// </param>
+        /// <returns>
+        /// Returns the string response received from the device.
+        /// </returns>
+        /// <exception cref="MethodNotImplementedException">If the method is not implemented</exception>
+        /// <exception cref="NotConnectedException">If the driver is not connected.</exception>
+        /// <exception cref="DriverException">Must throw an exception if the call was not successful</exception>
+        /// <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> </remarks>
+        string CommandString(string Command, bool Raw = false);
+
+        /// <summary>
+        /// Dispose the late-bound interface, if needed. Will release it via COM
+        /// if it is a COM object, else if native .NET will just dereference it
+        /// for GC.
+        /// </summary>
+        void Dispose();
+
+        #endregion
+
+        #region Device Specific Methods
+
         /// <summary>
         /// Returns the state of the device cover, if present, otherwise returns "NotPresent"
         /// </summary>
@@ -124,5 +316,7 @@ namespace ASCOM.DeviceInterface
         /// <para>If an error condition arises while turning off the calibrator, <see cref="CalibratorState"/> must be set to <see cref="CalibratorStatus.Error"/> rather than <see cref="CalibratorStatus.Unknown"/>.</para>
         /// </remarks>
         void CalibratorOff();
+
+        #endregion
     }
 }
