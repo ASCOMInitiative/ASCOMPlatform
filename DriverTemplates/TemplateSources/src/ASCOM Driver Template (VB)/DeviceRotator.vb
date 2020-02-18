b780d429ea7c9a42e4cddbfa47dbb248da2aa3fb
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2020-02-18 13:26:31 +0000

    Fixes to VB Rotator driver template

--- DriverTemplates/TemplateSources/src/ASCOM Driver Template (VB)/DeviceRotator.vb
+++ DriverTemplates/TemplateSources/src/ASCOM Driver Template (VB)/DeviceRotator.vb
@@ -8,7 +8,7 @@ Imports ASCOM.Utilities
 Imports ASCOM.Astrometry.AstroUtils
 
 Class DeviceRotator
-    Implements IRotatorV2
+    Implements IRotatorV3
     Private m_util As New Util()
     Private TL As New TraceLogger()
     Private astroUtilities As New AstroUtils()
@@ -18,45 +18,45 @@ Class DeviceRotator
     Private rotatorPosition As Single = 0 ' Synced or mechanical position angle of the rotator depending on the value of CanSync
     Private rotatorMechanicalPosition As Single = 0 ' Mechanical position angle Of the rotator 
 
-    Public ReadOnly Property CanReverse() As Boolean Implements IRotatorV2.CanReverse
+    Public ReadOnly Property CanReverse() As Boolean Implements IRotatorV3.CanReverse
         Get
             TL.LogMessage("CanReverse Get", False.ToString())
             Return False
         End Get
     End Property
 
-    Public Sub Halt() Implements IRotatorV2.Halt
+    Public Sub Halt() Implements IRotatorV3.Halt
         TL.LogMessage("Halt", "Not implemented")
         Throw New ASCOM.MethodNotImplementedException("Halt")
     End Sub
 
-    Public ReadOnly Property IsMoving() As Boolean Implements IRotatorV2.IsMoving
+    Public ReadOnly Property IsMoving() As Boolean Implements IRotatorV3.IsMoving
         Get
             TL.LogMessage("IsMoving Get", False.ToString()) ' This rotator has instantaneous movement
             Return False
         End Get
     End Property
 
-    Public Sub Move(Position As Single) Implements IRotatorV2.Move
+    Public Sub Move(Position As Single) Implements IRotatorV3.Move
         TL.LogMessage("Move", Position.ToString()) ' Move by this amount
         rotatorPosition += Position
         rotatorPosition = astroUtilities.Range(rotatorPosition, 0.0, True, 360.0, False) ' Ensure value is in the range 0.0..359.9999...
     End Sub
 
-    Public Sub MoveAbsolute(Position As Single) Implements IRotatorV2.MoveAbsolute
+    Public Sub MoveAbsolute(Position As Single) Implements IRotatorV3.MoveAbsolute
         TL.LogMessage("MoveAbsolute", Position.ToString()) ' Move to this position
         rotatorPosition = Position
         rotatorPosition = astroUtilities.Range(rotatorPosition, 0.0, True, 360.0, False) ' Ensure value is in the range 0.0..359.9999...
     End Sub
 
-    Public ReadOnly Property Position() As Single Implements IRotatorV2.Position
+    Public ReadOnly Property Position() As Single Implements IRotatorV3.Position
         Get
             TL.LogMessage("Position Get", rotatorPosition.ToString()) ' This rotator has instantaneous movement
             Return rotatorPosition
         End Get
     End Property
 
-    Public Property Reverse() As Boolean Implements IRotatorV2.Reverse
+    Public Property Reverse() As Boolean Implements IRotatorV3.Reverse
         Get
             TL.LogMessage("Reverse Get", "Not implemented")
             Throw New ASCOM.PropertyNotImplementedException("Reverse", False)
@@ -67,14 +67,14 @@ Class DeviceRotator
         End Set
     End Property
 
-    Public ReadOnly Property StepSize() As Single Implements IRotatorV2.StepSize
+    Public ReadOnly Property StepSize() As Single Implements IRotatorV3.StepSize
         Get
             TL.LogMessage("StepSize Get", "Not implemented")
             Throw New ASCOM.PropertyNotImplementedException("StepSize", False)
         End Get
     End Property
 
-    Public ReadOnly Property TargetPosition() As Single Implements IRotatorV2.TargetPosition
+    Public ReadOnly Property TargetPosition() As Single Implements IRotatorV3.TargetPosition
         Get
             TL.LogMessage("TargetPosition Get", rotatorPosition.ToString()) ' This rotator has instantaneous movement
             Return rotatorPosition
@@ -83,34 +83,34 @@ Class DeviceRotator
 
     ' IRotatorV3 methods
 
-    Public ReadOnly Property CanSync As Boolean
+    Public ReadOnly Property CanSync As Boolean Implements IRotatorV3.CanSync
         Get
-            TL.LogMessage("CanSync Get", False.ToString());
-            Return False;
+            TL.LogMessage("CanSync Get", False.ToString())
+            Return False
         End Get
     End Property
 
-    Public ReadOnly Property MechanicalPosition As Single
+    Public ReadOnly Property MechanicalPosition As Single Implements IRotatorV3.MechanicalPosition
         Get
             TL.LogMessage("MechanicalPosition Get", rotatorMechanicalPosition.ToString())
             Return rotatorMechanicalPosition
         End Get
     End Property
 
-    Public Sub MoveMechanical(Position As Single)
+    Public Sub MoveMechanical(Position As Single) Implements IRotatorV3.MoveMechanical
         TL.LogMessage("MoveMechanical", Position.ToString()) ' Move To this position
 
         ' TODO Implement correct sync behaviour. i.e. If the rotator has been synced the mechanical And rotator positions won't be the same
         rotatorMechanicalPosition = astroUtilities.Range(Position, 0.0, True, 360.0, False) ' Ensure value Is In the range 0.0..359.9999...
-        rotatorPosition = astroUtilities.Range(Position, 0.0, True, 360.0, False) ' Ensure value Is In the range 0.0..359.9999...
+        rotatorPosition = astroUtilities.Range(Position, 0.0, True, 360.0, False) ' Ensure value is In the range 0.0..359.9999...
     End Sub
 
-    Public Sub Sync(Position As Single)
+    Public Sub Sync(Position As Single) Implements IRotatorV3.Sync
 
         TL.LogMessage("Sync", Position.ToString()) ' Sync To this position
 
         ' TODO Implement correct sync behaviour. i.e. the rotator mechanical And rotator positions may Not be the same
-        rotatorPosition = astroUtilities.Range(Position, 0.0, True, 360.0, False); // Ensure value Is In the range 0.0..359.9999...
+        rotatorPosition = astroUtilities.Range(Position, 0.0, True, 360.0, False) ' Ensure value is In the range 0.0..359.9999...
     End Sub
 
 #End Region
