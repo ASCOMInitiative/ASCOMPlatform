ec95c7bb8315899007796c469d1934e2364922a8
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2021-09-17 12:37:52 +0100

    Fix default namespace for local server projects.

--- DriverTemplates/TemplateSources/src/ASCOM LocalServer Template CS/Properties/Resources.Designer.cs
+++ DriverTemplates/TemplateSources/src/ASCOM LocalServer Template CS/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ASCOM.LocalServer.Properties {
+namespace ASCOM.LocalServer.Server.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ASCOM.LocalServer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ASCOM.LocalServer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ASCOM.LocalServer.Server.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
