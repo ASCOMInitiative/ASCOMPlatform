1cf4fd1a03e8a52e7684a99071be8a4737a784d1
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-06-16 10:25:50 +0100

    Add comments to IStateValue interface

--- ASCOM.DeviceInterface/IStateValue.cs
+++ ASCOM.DeviceInterface/IStateValue.cs
@@ -7,7 +7,7 @@ using System.Text;
 namespace ASCOM.DeviceInterface
 {
     /// <summary>
-    /// 
+    /// Interface definition for DeviceState objects
     /// </summary>
     [Guid("5300D26E-1C7D-4541-B2DA-8AF5F57E183D")]
     [ComVisible(true)]
@@ -15,12 +15,12 @@ namespace ASCOM.DeviceInterface
     public interface IStateValue
     {
         /// <summary>
-        /// 
+        /// Property name with casing that must match the casing in the relevant interface definition
         /// </summary>
         string Name { get; }
 
         /// <summary>
-        /// 
+        /// Property value
         /// </summary>
         object Value { get; }
     }
