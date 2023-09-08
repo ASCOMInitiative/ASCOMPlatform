c2b67f8e9343f1c39002252fb1e9af219174944d
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-09-08 17:44:13 +0100

    Add missing file.

---
+++ Releases/ASCOM 6/Uninstaller/MigrationSupport/Properties/AssemblyInfo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly: AssemblyTitle("Migration Support")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("ASCOM Initiative")]
+[assembly: AssemblyProduct("Migration Support")]
+[assembly: AssemblyCopyright("Copyright © ASCOM Initiative 2022")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("43234d78-8dfc-44b5-9d39-e18d568748c1")]
+
+// Version information for an assembly consists of the following four values:
+// 
+// Major Version
+// Minor Version 
+// Build Number
+// Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("6.6.0.0")]
+
+// Permissions for particular assemblies to access internal "Friend" variables and methods of this assembly
+[assembly: InternalsVisibleTo("UninstallAscom, PublicKey= " + "0024000004800000940000000602000000240000525341310004000001000100afa2def19e73b7" + "2a7cf149888c93ec828db75e4d481b5c652aa43417a77689ce9151853ce10278c44bff4aa559b9" + "0a4f534cef9eaa6cb8b0f6a5f0719f9ad18795416367e9f485712075c54aea9b4e9aa990326e2f" + "929a990a5c61b679564343ff21547e096db1a90e50419c12c390641686270105620f51fd7d94ef" + "54beeee8")]
+
+
+
+
+
+[assembly: InternalsVisibleTo("FinaliseInstall, PublicKey= " + "0024000004800000940000000602000000240000525341310004000001000100afa2def19e73b7" + "2a7cf149888c93ec828db75e4d481b5c652aa43417a77689ce9151853ce10278c44bff4aa559b9" + "0a4f534cef9eaa6cb8b0f6a5f0719f9ad18795416367e9f485712075c54aea9b4e9aa990326e2f" + "929a990a5c61b679564343ff21547e096db1a90e50419c12c390641686270105620f51fd7d94ef" + "54beeee8")]
+
+
+
+
+
+[assembly: InternalsVisibleTo("InstallTemplates, PublicKey= " + "0024000004800000940000000602000000240000525341310004000001000100afa2def19e73b7" + "2a7cf149888c93ec828db75e4d481b5c652aa43417a77689ce9151853ce10278c44bff4aa559b9" + "0a4f534cef9eaa6cb8b0f6a5f0719f9ad18795416367e9f485712075c54aea9b4e9aa990326e2f" + "929a990a5c61b679564343ff21547e096db1a90e50419c12c390641686270105620f51fd7d94ef" + "54beeee8")]
+
+
+
+
+
