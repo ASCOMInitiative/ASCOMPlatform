79ecdce185610bb6eda172ef93e31dd46b7e8065
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-11-11 11:33:01 +0000

    Fix issue in NOVAS 3.1 C code where the ephemeris file may not have been closed on DLL unload.

--- NOVAS/NOVAS-31/NOVAS3/DllMain.c
+++ NOVAS/NOVAS-31/NOVAS3/DllMain.c
@@ -1,36 +1,37 @@
 #ifndef _NOVAS_
 #include "novas.h"
+#include "eph_manager.h"
 #endif
-#include <windows.h>
 
+#include <windows.h>
 #include <stdio.h>
 #include <time.h>
 
 BOOL WINAPI DllMain(
-    HINSTANCE hinstDLL,  // handle to DLL module
-    DWORD fdwReason,     // reason for calling function
-    LPVOID lpReserved)  // reserved
+	HINSTANCE hinstDLL,  // handle to DLL module
+	DWORD fdwReason,     // reason for calling function
+	LPVOID lpReserved)  // reserved
 {
-    // Perform actions based on the reason for calling.
-    switch (fdwReason)
-    {
-    case DLL_PROCESS_ATTACH:
-        // Initialize once for each new process.
-        // Return FALSE to fail DLL load.
-        break;
+	// Perform actions based on the reason for calling.
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		// Initialize once for each new process.
+		// Return FALSE to fail DLL load.
+		break;
 
-    case DLL_THREAD_ATTACH:
-        // Do thread-specific initialization.
-        break;
+	case DLL_THREAD_ATTACH:
+		// Do thread-specific initialization.
+		break;
 
-    case DLL_THREAD_DETACH:
-        // Do thread-specific clean-up.
-        break;
+	case DLL_THREAD_DETACH:
+		// Do thread-specific clean-up.
+		break;
 
-    case DLL_PROCESS_DETACH:
-        // Perform any necessary clean-up.
-        ephem_close();
-        break;
-    }
-    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+	case DLL_PROCESS_DETACH:
+		// Perform any necessary clean-up.
+		ephem_close();
+		break;
+	}
+	return TRUE;  // Successful DLL_PROCESS_ATTACH.
 }
