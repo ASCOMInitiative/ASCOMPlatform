29a3825221e74fddb5417cc36c1f26f42a2218f8
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-11-14 12:07:50 +0000

    Fix DeviceInterfaces assembly that did not have the correct assembly version number post turning into a package.

--- ASCOM.DeviceInterface/Properties/AssemblyInfo.cs
+++ ASCOM.DeviceInterface/Properties/AssemblyInfo.cs
@@ -1,6 +1,14 @@
 ﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
+
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("5849AF17-3590-42ff-900E-7544D45C9825")]
 
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyCompany("ASCOM Initiative")]
+[assembly: AssemblyCopyright("Copyright © ASCOM Initiative 2023")]
+[assembly: AssemblyDescription("ASCOM device interfaces for Platform 7")]
+[assembly: AssemblyProduct("ASCOM.DeviceInterfaces")]
+[assembly: AssemblyTitle(" ASCOM Device Interfaces")]
