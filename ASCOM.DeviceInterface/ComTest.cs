59bf9323d6874e24e4361b860870598d25f3af10
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-12-03 11:52:18 +0000

    Add basic COM class for test.

---
+++ ASCOM.DeviceInterface/ComTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.OLE.Interop;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ASCOM.DeviceInterface
+{
+    /// <summary>
+    /// COM visible ArrayList class for use by .NET 5, 6, 7, 8 and later
+    /// </summary>
+    [Serializable]
+    [Guid("F71A68C5-4130-48E6-90CD-0CD2D977D4D1")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComVisible(true)]
+    public class ComTest : IComTest
+    {
+        Dictionary<string,string> list = new Dictionary<string, string> { { "Default1", "Value1" }, { "Default2", "Value2" } };
+
+        /// <summary>
+        /// Create a ComTest instance
+        /// </summary>
+        public ComTest()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int NumberOfItems() => list.Count;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public void Add(string name, string value)
+        {
+            list.Add(name, value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetValues()
+        {
+            string retVal = "";
+            foreach (KeyValuePair<string, string> item in list)
+            {
+                retVal += $"{item.Key} = {item.Value}\r\n";
+            }
+
+            return retVal;
+        }
+    }
+}
---
+++ ASCOM.DeviceInterface/IComTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ASCOM.DeviceInterface
+{
+    /// <summary>
+    /// ComArrayList interface
+    /// </summary>
+    [Guid("6720258E-E5B1-4160-BE34-95484A496E3B")]
+    [ComVisible(true)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface IComTest
+    {
+        /// <summary>
+        /// Number of items in the ArrayList
+        /// </summary>
+        /// <returns></returns>
+        int NumberOfItems();
+
+        /// <summary>
+        /// Get stored values
+        /// </summary>
+        /// <returns></returns>
+        string GetValues();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        void Add(string name, string value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator GetEnumerator();
+    }
+}
