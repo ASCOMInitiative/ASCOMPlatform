be42f2a6e130772de4a90dc66156234f2419357e
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-12-03 09:37:05 +0000

    Make ComArrayList serialisable.

--- ASCOM.DeviceInterface/ComArrayList.cs
+++ ASCOM.DeviceInterface/ComArrayList.cs
@@ -10,6 +10,7 @@ namespace ASCOM.DeviceInterface
     /// <summary>
     /// COM visible ArrayList class for use by .NET 5, 6, 7, 8 and later
     /// </summary>
+    [Serializable]
     [Guid("E67780D1-7C92-442C-8DCA-FCD06718427D")]
     [ClassInterface(ClassInterfaceType.None)]
     [ComVisible(true)]
@@ -18,7 +19,7 @@ namespace ASCOM.DeviceInterface
         /// <summary>
         /// Create a ComArrayList instance
         /// </summary>
-        public ComArrayList()
+        public ComArrayList() : base()
         {
         }
 
