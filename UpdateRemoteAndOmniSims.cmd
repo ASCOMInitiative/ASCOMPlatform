20bdfbe4042101f993620ef7e339b5f4f0833128
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2024-07-02 13:56:21 +0100

    Revise local script to populate the Platform code base with the latest OmniSim and ASCOM Remote code bases.

--- UpdateRemoteAndOmniSims.cmd
+++ UpdateRemoteAndOmniSims.cmd
@@ -1,2 +1,12 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-msbuild UpdateRemoteAndOmniSims.msbuild > buildlog.txt
+rem msbuild UpdateRemoteAndOmniSims.msbuild > buildlog.txt
+
+xcopy /s/y J:\Ascom.Alpaca.Simulators J:\ASCOMPlatform\OmniSimulators
+
+cd \ASCOMPlatform\OmniSimulators\ASCOM.Alpaca.Simulators
+
+dotnet publish -c Release -r win-x86 --self-contained true /p:PublishTrimmed=false -p DefineConstants=ASCOM_COM -p:Platform=x86 -o ..\publish\x86
+
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=false -p DefineConstants=ASCOM_COM -o ..\publish\x64 
+
+pause
