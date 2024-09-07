2636e038605672be1774239601be9eba14f92c59
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2024-09-07 14:34:10 +0100

    Dynamic Clients - Ensure that the local server windows form is hidden when running. Part of #76.

--- ASCOM.Utilities/AlpacaDynamicClients/Dynamic Client Local Server/LocalServerForm.Designer.cs
+++ ASCOM.Utilities/AlpacaDynamicClients/Dynamic Client Local Server/LocalServerForm.Designer.cs
@@ -47,8 +47,11 @@ namespace ASCOM.DynamicClients
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(233, 52);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "LocalServerForm";
+            this.ShowInTaskbar = false;
             this.Text = "Dynamic Driver Local Server";
             this.ResumeLayout(false);
 
