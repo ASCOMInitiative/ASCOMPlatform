a628e9c7cba83d66c5670b3bdf681e923f53ca32
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2025-06-30 12:20:40 +0100

    JustAHub  - Convert to SDK project format.

--- Drivers and Simulators/JustAHub/Properties/AssemblyInfo.cs
+++ Drivers and Simulators/JustAHub/Properties/AssemblyInfo.cs
@@ -1,34 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-// TODO - Add your authorship information here
-[assembly: AssemblyTitle("ASCOM JustAHub")]
-[assembly: AssemblyDescription("ASCOM Local Server for JustAHub")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The ASCOM Initiative")]
-[assembly: AssemblyProduct("ASCOM JustAHub")]
-[assembly: AssemblyCopyright("Copyright © The ASCOM Initiative 2025")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components. Do not set this to TRUE because it will stop the driver from registering correctly. 
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("7.0.0.0")]
-[assembly: AssemblyFileVersion("7.0.0.0")]
 [assembly: Guid("4FE42B85-A500-425F-9B6A-22F5BE7BA69E")]
