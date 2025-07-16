f789ef8b46d2ed59b92fef857be9c81394c586c3
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2025-07-16 12:00:35 +0100

    Device Hub - Implement persistence for new configuration values. WIP

--- Drivers and Simulators/ASCOM Device Hub/DeviceHub/Business Object Classes/Dome Classes/DomeLayoutSettings.cs
+++ Drivers and Simulators/ASCOM Device Hub/DeviceHub/Business Object Classes/Dome Classes/DomeLayoutSettings.cs
@@ -3,114 +3,314 @@ using System.Windows.Media.Media3D;
 
 namespace ASCOM.DeviceHub
 {
-	public class DomeLayoutSettings : DevicePropertiesBase, ICloneable
-	{
-		#region Constructors
+    public class DomeLayoutSettings : DevicePropertiesBase, ICloneable
+    {
+        #region Constructors
 
-		public DomeLayoutSettings()
-		{}
+        public DomeLayoutSettings()
+        { }
 
-		public DomeLayoutSettings( DomeLayoutSettings other )
-		{
-			this._domeScopeOffset = other._domeScopeOffset;
-			this._domeRadius = other._domeRadius;
-			this._gemAxisOffset = other._gemAxisOffset;
-			this._azimuthAccuracy = other._azimuthAccuracy;
-			this._slaveInterval = other._slaveInterval;
-		}
+        public DomeLayoutSettings(DomeLayoutSettings other)
+        {
+            this._domeScopeOffset = other._domeScopeOffset;
+            this._domeRadius = other._domeRadius;
+            this._azimuthAccuracy = other._azimuthAccuracy;
+            this._slaveInterval = other._slaveInterval;
 
-		#endregion Constructors
+            // Copy the offsets for the GEM axis
+            this._gemAxisOffset = other._gemAxisOffset;
+            this._gemAxisOffset2 = other._gemAxisOffset2;
+            this._gemAxisOffset3 = other._gemAxisOffset3;
+            this._gemAxisOffset4 = other._gemAxisOffset4;
+            this._gemAxisOffset5 = other._gemAxisOffset5;
 
-		#region Change Notification Properties
+            // Copy the optical offsets
+            this._opticalOffset = other._opticalOffset;
+            this._opticalOffset2 = other._opticalOffset2;
+            this._opticalOffset3 = other._opticalOffset3;
+            this._opticalOffset4 = other._opticalOffset4;
+            this._opticalOffset5 = other._opticalOffset5;
 
-		private Point3D _domeScopeOffset;
+            // Copy the telescope names
+            this._telescopeName = other._telescopeName;
+            this._telescopeName2 = other._telescopeName2;
+            this._telescopeName3 = other._telescopeName3;
+            this._telescopeName4 = other._telescopeName4;
+            this._telescopeName5 = other._telescopeName5;
+        }
 
-		public Point3D DomeScopeOffset
-		{
-			get { return _domeScopeOffset; }
-			set
-			{
-				if ( value != _domeScopeOffset )
-				{
-					_domeScopeOffset = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+        #endregion Constructors
 
-		private int _domeRadius;
+        #region Change Notification Properties
 
-		public int DomeRadius
-		{
-			get { return _domeRadius; }
-			set
-			{
-				if ( value != _domeRadius )
-				{
-					_domeRadius = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+        private Point3D _domeScopeOffset;
+        private int _domeRadius;
+        private int _slaveInterval;
+        private int _azimuthAccuracy;
 
-		private int _gemAxisOffset;
+        private int _gemAxisOffset;
+        private int _gemAxisOffset2;
+        private int _gemAxisOffset3;
+        private int _gemAxisOffset4;
+        private int _gemAxisOffset5;
 
-		public int GemAxisOffset
-		{
-			get { return _gemAxisOffset; }
-			set
-			{
-				if ( value != _gemAxisOffset )
-				{
-					_gemAxisOffset = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+        private int _opticalOffset;
+        private int _opticalOffset2;
+        private int _opticalOffset3;
+        private int _opticalOffset4;
+        private int _opticalOffset5;
 
-		private int _azimuthAccuracy;
+        private string _telescopeName;
+        private string _telescopeName2;
+        private string _telescopeName3;
+        private string _telescopeName4;
+        private string _telescopeName5;
 
-		public int AzimuthAccuracy
-		{
-			get { return _azimuthAccuracy; }
-			set
-			{
-				if ( value != _azimuthAccuracy )
-				{
-					_azimuthAccuracy = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+        public Point3D DomeScopeOffset
+        {
+            get { return _domeScopeOffset; }
+            set
+            {
+                if (value != _domeScopeOffset)
+                {
+                    _domeScopeOffset = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-		private int _slaveInterval;
+        public int DomeRadius
+        {
+            get { return _domeRadius; }
+            set
+            {
+                if (value != _domeRadius)
+                {
+                    _domeRadius = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-		public int SlaveInterval
-		{
-			get { return _slaveInterval; }
-			set
-			{
-				if ( value != _slaveInterval )
-				{
-					_slaveInterval = value;
-					OnPropertyChanged();
-				}
-			}
-		}
+        public int GemAxisOffset
+        {
+            get { return _gemAxisOffset; }
+            set
+            {
+                if (value != _gemAxisOffset)
+                {
+                    _gemAxisOffset = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-		private int _opticalOffset;
+        public int GemAxisOffset2
+        {
+            get { return _gemAxisOffset2; }
+            set
+            {
+                if (value != _gemAxisOffset2)
+                {
+                    _gemAxisOffset2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-		public int OpticalOffset
-		{
-			get { return _opticalOffset; }
-			set
-			{
-				if ( value != _opticalOffset )
-				{
-					_opticalOffset = value;
-					OnPropertyChanged();
-				}
-			}
+        public int GemAxisOffset3
+        {
+            get { return _gemAxisOffset3; }
+            set
+            {
+                if (value != _gemAxisOffset3)
+                {
+                    _gemAxisOffset3 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int GemAxisOffset4
+        {
+            get { return _gemAxisOffset4; }
+            set
+            {
+                if (value != _gemAxisOffset4)
+                {
+                    _gemAxisOffset4 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int GemAxisOffset5
+        {
+            get { return _gemAxisOffset5; }
+            set
+            {
+                if (value != _gemAxisOffset5)
+                {
+                    _gemAxisOffset5 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int AzimuthAccuracy
+        {
+            get { return _azimuthAccuracy; }
+            set
+            {
+                if (value != _azimuthAccuracy)
+                {
+                    _azimuthAccuracy = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int SlaveInterval
+        {
+            get { return _slaveInterval; }
+            set
+            {
+                if (value != _slaveInterval)
+                {
+                    _slaveInterval = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int OpticalOffset
+        {
+            get { return _opticalOffset; }
+            set
+            {
+                if (value != _opticalOffset)
+                {
+                    _opticalOffset = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int OpticalOffset2
+        {
+            get { return _opticalOffset2; }
+            set
+            {
+                if (value != _opticalOffset2)
+                {
+                    _opticalOffset2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int OpticalOffset3
+        {
+            get { return _opticalOffset3; }
+            set
+            {
+                if (value != _opticalOffset3)
+                {
+                    _opticalOffset3 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int OpticalOffset4
+        {
+            get { return _opticalOffset4; }
+            set
+            {
+                if (value != _opticalOffset4)
+                {
+                    _opticalOffset4 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int OpticalOffset5
+        {
+            get { return _opticalOffset5; }
+            set
+            {
+                if (value != _opticalOffset5)
+                {
+                    _opticalOffset5 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string TelescopeName
+        {
+            get { return _telescopeName; }
+            set
+            {
+                if (value != _telescopeName)
+                {
+                    _telescopeName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string TelescopeName2
+        {
+            get { return _telescopeName2; }
+            set
+            {
+                if (value != _telescopeName2)
+                {
+                    _telescopeName2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string TelescopeName3
+        {
+            get { return _telescopeName3; }
+            set
+            {
+                if (value != _telescopeName3)
+                {
+                    _telescopeName3 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string TelescopeName4
+        {
+            get { return _telescopeName4; }
+            set
+            {
+                if (value != _telescopeName4)
+                {
+                    _telescopeName4 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string TelescopeName5
+        {
+            get { return _telescopeName5; }
+            set
+            {
+                if (value != _telescopeName5)
+                {
+                    _telescopeName5 = value;
+                    OnPropertyChanged();
+                }
+            }
         }
 
         #endregion Change Notification Properties
@@ -118,15 +318,15 @@ namespace ASCOM.DeviceHub
         #region ICloneable Implementation
 
         object ICloneable.Clone()
-		{
-			return new DomeLayoutSettings( this );
-		}
+        {
+            return new DomeLayoutSettings(this);
+        }
 
-		public DomeLayoutSettings Clone()
-		{
-			return new DomeLayoutSettings( this );
-		}
+        public DomeLayoutSettings Clone()
+        {
+            return new DomeLayoutSettings(this);
+        }
 
-		#endregion ICloneable Implementation
-	}
+        #endregion ICloneable Implementation
+    }
 }
--- Drivers and Simulators/ASCOM Device Hub/DeviceHub/Business Object Classes/Dome Classes/DomeSettings.cs
+++ Drivers and Simulators/ASCOM Device Hub/DeviceHub/Business Object Classes/Dome Classes/DomeSettings.cs
@@ -16,10 +16,29 @@ namespace ASCOM.DeviceHub
         private const string _offsetDefault = "0.0";
         private const string _radiusProfileName = "Dome Radius";
         private const string _radiusDefault = "2000";
+
         private const string _gemAxisOffsetProfileName = "GEM Axis Offset";
+        private const string _gemAxisOffset2ProfileName = "GEM Axis Offset 2";
+        private const string _gemAxisOffset3ProfileName = "GEM Axis Offset 3";
+        private const string _gemAxisOffset4ProfileName = "GEM Axis Offset 4";
+        private const string _gemAxisOffset5ProfileName = "GEM Axis Offset 5";
         private const string _gemAxisOffsetDefault = "0";
+
         private const string _opticalOffsetProfileName = "Optical Offset";
+        private const string _opticalOffset2ProfileName = "Optical Offset 2";
+        private const string _opticalOffset3ProfileName = "Optical Offset 3";
+        private const string _opticalOffset4ProfileName = "Optical Offset 4";
+        private const string _opticalOffset5ProfileName = "Optical Offset 5";
         private const string _opticalOffsetDefault = "0";
+
+        private const string _telescopeNameProfileName = "Telescope Name";
+        private const string _telescopeName2ProfileName = "Telescope Name 2";
+        private const string _telescopeName3ProfileName = "Telescope Name 3";
+        private const string _telescopeName4ProfileName = "Telescope Name 4";
+        private const string _telescopeName5ProfileName = "Telescope Name 5";
+        private const string _telescope1NameDefault = "Main Telescope";
+        private const string _telescopeNameDefault = "";
+
         private const string _azimuthAccuracyProfileName = "Azimuth Accuracy";
         private const string _azimuthAccuracyDefault = "2";
         private const string _azimuthAdjustmentProfileName = "Azimuth Adjustment";
@@ -44,6 +63,23 @@ namespace ASCOM.DeviceHub
             double layoutZOffset;
             int domeRadius;
             int gemAxisOffset;
+
+            int gemAxisOffset2;
+            int gemAxisOffset3;
+            int gemAxisOffset4;
+            int gemAxisOffset5;
+
+            int opticalOffset2;
+            int opticalOffset3;
+            int opticalOffset4;
+            int opticalOffset5;
+
+            string telescopeName;
+            string telescopeName2;
+            string telescopeName3;
+            string telescopeName4;
+            string telescopeName5;
+
             int opticalOffset;
             int azimuthAccuracy;
             double azimuthAdjustment;
@@ -61,8 +97,6 @@ namespace ASCOM.DeviceHub
                 layoutYOffset = Convert.ToDouble(profile.GetValue(DriverID, _yOffsetProfileName, String.Empty, _offsetDefault), CultureInfo.InvariantCulture);
                 layoutZOffset = Convert.ToDouble(profile.GetValue(DriverID, _zOffsetProfileName, String.Empty, _offsetDefault), CultureInfo.InvariantCulture);
                 domeRadius = Convert.ToInt32(profile.GetValue(DriverID, _radiusProfileName, String.Empty, _radiusDefault));
-                gemAxisOffset = Convert.ToInt32(profile.GetValue(DriverID, _gemAxisOffsetProfileName, String.Empty, _gemAxisOffsetDefault));
-                opticalOffset = Convert.ToInt32(profile.GetValue(DriverID, _opticalOffsetProfileName, String.Empty, _opticalOffsetDefault));
                 azimuthAccuracy = Convert.ToInt32(profile.GetValue(DriverID, _azimuthAccuracyProfileName, String.Empty, _azimuthAccuracyDefault));
                 azimuthAdjustment = Convert.ToDouble(profile.GetValue(DriverID, _azimuthAdjustmentProfileName, String.Empty, _azimuthAdjustmentDefault), CultureInfo.InvariantCulture);
                 slaveInterval = Convert.ToInt32(profile.GetValue(DriverID, _slaveIntervalProfileName, String.Empty, _slaveIntervalDefault));
@@ -70,6 +104,24 @@ namespace ASCOM.DeviceHub
                 usePOTHCalculation = Convert.ToBoolean(profile.GetValue(DriverID, _usePOTHSlavingCalculationProfileName, String.Empty, _usePOTHSlavingCalculationDefault));
                 useRevisedCalculation = Convert.ToBoolean(profile.GetValue(DriverID, _useRevisedSlavingCalculationProfileName, String.Empty, _useRevisedSlavingCalculationDefault));
                 fastUpdatePeriod = Convert.ToDouble(profile.GetValue(DriverID, _fastUpdateProfileName, String.Empty, _fastUpdateDefault), CultureInfo.InvariantCulture);
+
+                gemAxisOffset = Convert.ToInt32(profile.GetValue(DriverID, _gemAxisOffsetProfileName, String.Empty, _gemAxisOffsetDefault));
+                gemAxisOffset2 = Convert.ToInt32(profile.GetValue(DriverID, _gemAxisOffset2ProfileName, String.Empty, _gemAxisOffsetDefault));
+                gemAxisOffset3 = Convert.ToInt32(profile.GetValue(DriverID, _gemAxisOffset3ProfileName, String.Empty, _gemAxisOffsetDefault));
+                gemAxisOffset4 = Convert.ToInt32(profile.GetValue(DriverID, _gemAxisOffset4ProfileName, String.Empty, _gemAxisOffsetDefault));
+                gemAxisOffset5 = Convert.ToInt32(profile.GetValue(DriverID, _gemAxisOffset5ProfileName, String.Empty, _gemAxisOffsetDefault));
+
+                opticalOffset = Convert.ToInt32(profile.GetValue(DriverID, _opticalOffsetProfileName, String.Empty, _opticalOffsetDefault));
+                opticalOffset2 = Convert.ToInt32(profile.GetValue(DriverID, _opticalOffset2ProfileName, String.Empty, _opticalOffsetDefault));
+                opticalOffset3 = Convert.ToInt32(profile.GetValue(DriverID, _opticalOffset3ProfileName, String.Empty, _opticalOffsetDefault));
+                opticalOffset4 = Convert.ToInt32(profile.GetValue(DriverID, _opticalOffset4ProfileName, String.Empty, _opticalOffsetDefault));
+                opticalOffset5 = Convert.ToInt32(profile.GetValue(DriverID, _opticalOffset5ProfileName, String.Empty, _opticalOffsetDefault));
+
+                telescopeName = profile.GetValue(DriverID, _telescopeNameProfileName, String.Empty, _telescope1NameDefault);
+                telescopeName2 = profile.GetValue(DriverID, _telescopeName2ProfileName, String.Empty, _telescopeNameDefault);
+                telescopeName3 = profile.GetValue(DriverID, _telescopeName3ProfileName, String.Empty, _telescopeNameDefault);
+                telescopeName4 = profile.GetValue(DriverID, _telescopeName4ProfileName, String.Empty, _telescopeNameDefault);
+                telescopeName5 = profile.GetValue(DriverID, _telescopeName5ProfileName, String.Empty, _telescopeNameDefault);
             }
 
             // Prevent the user from circumventing the valid fast update by setting the value in the profile store directly.
@@ -80,10 +132,26 @@ namespace ASCOM.DeviceHub
             {
                 DomeScopeOffset = new System.Windows.Media.Media3D.Point3D(layoutXOffset, layoutYOffset, layoutZOffset),
                 DomeRadius = domeRadius,
-                GemAxisOffset = gemAxisOffset,
                 AzimuthAccuracy = azimuthAccuracy,
                 SlaveInterval = slaveInterval,
-                OpticalOffset = opticalOffset
+
+                GemAxisOffset = gemAxisOffset,
+                GemAxisOffset2 = gemAxisOffset2,
+                GemAxisOffset3 = gemAxisOffset3,
+                GemAxisOffset4 = gemAxisOffset4,
+                GemAxisOffset5 = gemAxisOffset5,
+
+                OpticalOffset = opticalOffset,
+                OpticalOffset2 = opticalOffset2,
+                OpticalOffset3 = opticalOffset3,
+                OpticalOffset4 = opticalOffset4,
+                OpticalOffset5 = opticalOffset5,
+
+                TelescopeName = telescopeName,
+                TelescopeName2 = telescopeName2,
+                TelescopeName3 = telescopeName3,
+                TelescopeName4 = telescopeName4,
+                TelescopeName5 = telescopeName5,
             };
 
             DomeSettings settings = new DomeSettings
@@ -121,8 +189,6 @@ namespace ASCOM.DeviceHub
                 profile.WriteValue(DriverID, _yOffsetProfileName, DomeLayout.DomeScopeOffset.Y.ToString(CultureInfo.InvariantCulture));
                 profile.WriteValue(DriverID, _zOffsetProfileName, DomeLayout.DomeScopeOffset.Z.ToString(CultureInfo.InvariantCulture));
                 profile.WriteValue(DriverID, _radiusProfileName, DomeLayout.DomeRadius.ToString());
-                profile.WriteValue(DriverID, _gemAxisOffsetProfileName, DomeLayout.GemAxisOffset.ToString());
-                profile.WriteValue(DriverID, _opticalOffsetProfileName, DomeLayout.OpticalOffset.ToString());
                 profile.WriteValue(DriverID, _azimuthAccuracyProfileName, DomeLayout.AzimuthAccuracy.ToString());
                 profile.WriteValue(DriverID, _azimuthAdjustmentProfileName, AzimuthAdjustment.ToString(CultureInfo.InvariantCulture));
                 profile.WriteValue(DriverID, _slaveIntervalProfileName, DomeLayout.SlaveInterval.ToString());
@@ -130,6 +196,24 @@ namespace ASCOM.DeviceHub
                 profile.WriteValue(DriverID, _usePOTHSlavingCalculationProfileName, UsePOTHDomeSlaveCalculation.ToString());
                 profile.WriteValue(DriverID, _useRevisedSlavingCalculationProfileName, UseRevisedDomeSlaveCalculation.ToString());
                 profile.WriteValue(DriverID, _fastUpdateProfileName, FastUpdatePeriod.ToString(CultureInfo.InvariantCulture));
+
+                profile.WriteValue(DriverID, _gemAxisOffsetProfileName, DomeLayout.GemAxisOffset.ToString());
+                profile.WriteValue(DriverID, _gemAxisOffset2ProfileName, DomeLayout.GemAxisOffset2.ToString());
+                profile.WriteValue(DriverID, _gemAxisOffset3ProfileName, DomeLayout.GemAxisOffset3.ToString());
+                profile.WriteValue(DriverID, _gemAxisOffset4ProfileName, DomeLayout.GemAxisOffset4.ToString());
+                profile.WriteValue(DriverID, _gemAxisOffset5ProfileName, DomeLayout.GemAxisOffset5.ToString());
+
+                profile.WriteValue(DriverID, _opticalOffsetProfileName, DomeLayout.OpticalOffset.ToString());
+                profile.WriteValue(DriverID, _opticalOffset2ProfileName, DomeLayout.OpticalOffset2.ToString());
+                profile.WriteValue(DriverID, _opticalOffset3ProfileName, DomeLayout.OpticalOffset3.ToString());
+                profile.WriteValue(DriverID, _opticalOffset4ProfileName, DomeLayout.OpticalOffset4.ToString());
+                profile.WriteValue(DriverID, _opticalOffset5ProfileName, DomeLayout.OpticalOffset5.ToString());
+
+                profile.WriteValue(DriverID, _telescopeNameProfileName, DomeLayout.TelescopeName);
+                profile.WriteValue(DriverID, _telescopeName2ProfileName, DomeLayout.TelescopeName2);
+                profile.WriteValue(DriverID, _telescopeName3ProfileName, DomeLayout.TelescopeName3);
+                profile.WriteValue(DriverID, _telescopeName4ProfileName, DomeLayout.TelescopeName4);
+                profile.WriteValue(DriverID, _telescopeName5ProfileName, DomeLayout.TelescopeName5);
             }
         }
     }
