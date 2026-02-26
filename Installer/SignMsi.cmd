136f202f5c31480b42a07711678f61bedc105aa3
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-02-26 12:20:37 +0000

    Installer - Fix build issue.

--- Installer/SignMsi.cmd
+++ Installer/SignMsi.cmd
@@ -3,4 +3,4 @@ if not defined VSCMD_VER (
   call "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
 )
 
-signtool sign /v /tr http://timestamp.acs.microsoft.com/ /td sha256 /fd sha256 /n "Peter Simpson" "%1\Release\Single\AscomPlatformInstaller.msi"
+signtool sign /v /tr http://timestamp.acs.microsoft.com/ /td sha256 /fd sha256 /n "Peter Simpson" "%1\Release\Single\*.msi"
