6329424e7c817d280f97051c51e709bdf55a912e
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-02-27 09:53:46 +0000

    Installer - Also code sign the MSI file in the data folder.

--- Installer/SignMsi.cmd
+++ Installer/SignMsi.cmd
@@ -3,4 +3,8 @@ if not defined VSCMD_VER (
   call "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
 )
 
+REM Sign the MSI in the top level build folder
 signtool sign /v /tr http://timestamp.acs.microsoft.com/ /td sha256 /fd sha256 /n "Peter Simpson" "%1\Release\Single\*.msi"
+
+REM Sign the MSI in the data folder under the top level build folder
+signtool sign /v /tr http://timestamp.acs.microsoft.com/ /td sha256 /fd sha256 /n "Peter Simpson" "%1\Release\Single\data\*.msi"
